1255a6adb36ff4f3cf4df5d4b5ee8bf7c8fbb6d4
go1dfish <me@go1dfish.me>
2017-09-20 16:12:55 -0400

    Use connect-api-mocks

--- preact-config.js
+++ preact-config.js
@@ -1,13 +1,27 @@
+var apiMocker = require("connect-api-mocker");
+
+const USE_MOCKS = true;
+
 export default config => {
-  config.devServer.proxy = [
-    {
-      path: "/api/**",
-      target: "https://localhost:4443",
-      secure: false,
-      pathRewrite(path) {
-        // common: remove first path segment: (/api/**)
-        return "/" + path.replace(/^\/[^/]+\//, "");
+  if (USE_MOCKS) {
+    config.devServer.setup = function(app) {
+      app.use(function(req, res, next) {
+        res.header("X-Csrf-Token", "itsafake");
+        next();
+      });
+      app.use(apiMocker("/api", "mocks/api"));
+    };
+  } else {
+    config.devServer.proxy = [
+      {
+        path: "/api/**",
+        target: "https://localhost:4443",
+        secure: false,
+        pathRewrite(path) {
+          // common: remove first path segment: (/api/**)
+          return "/" + path.replace(/^\/[^/]+\//, "");
+        }
       }
-    }
-  ];
+    ];
+  }
 };
