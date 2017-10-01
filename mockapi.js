b82f72ec73d37f024d296d07c373581a952fa14a
go1dfish <me@go1dfish.me>
2017-10-01 12:43:15 -0400

    Mock API fixes

--- mockapi.js
+++ mockapi.js
@@ -3,10 +3,16 @@ var https = require("https");
 var connect = require("connect");
 var apiMocker = require("connect-api-mocker");
 var app = connect();
-var restMock = apiMocker("/", "mocks/api");
 
 pem.createCertificate({days:365, selfSigned:true}, function(err, keys){
-  app.use(restMock);
+  app.use(function(req,res,next){
+    setTimeout(next,Math.random() * 2000); // Random delay for more realistic testing
+  });
+  app.use(function(req, res, next) {
+    res.setHeader("X-Csrf-Token", "itsafake");
+    next();
+  });
+  app.use(apiMocker("/", "mocks/api"));
   console.log("Mock politiawww running");
   https.createServer({
     key: keys.serviceKey,
