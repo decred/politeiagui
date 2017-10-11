8955430f658075e3b56f71062a5b4de546184777
Sebastien Bramille <sebastien.bramille@gmail.com>
2017-10-11 18:08:09 +0100

    Add reduxdevtools

--- src/configureStore.js
+++ src/configureStore.js
@@ -1,17 +1,25 @@
-import { createStore, applyMiddleware } from "redux";
+import { createStore, applyMiddleware, compose } from "redux";
 import thunkMiddleware from "redux-thunk";
 import { createLogger } from "redux-logger";
 import rootReducer from "./reducers";
 
 const loggerMiddleware = createLogger();
-const configureStore = preloadedState =>
-  createStore(
+const configureStore = preloadedState => {
+  const composeEnhancers =
+    typeof window === "object" &&
+    window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ ?
+      window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__({
+        // Specify extension’s options like name, actionsBlacklist, actionsCreators, serialize...
+      }) : compose;
+
+  return createStore(
     rootReducer,
     preloadedState,
-    applyMiddleware(
+    composeEnhancers(applyMiddleware(
       thunkMiddleware,
       loggerMiddleware
-    )
+    ))
   );
+};
 
 export default configureStore;
