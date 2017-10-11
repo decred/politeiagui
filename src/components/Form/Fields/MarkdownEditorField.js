dbe36076a27a0bc48ecf9ebc087405dca12fe7fd
Sebastien Bramille <sebastien.bramille@gmail.com>
2017-10-11 20:51:25 +0100

    Add custom markdown editor

--- src/components/Form/Fields/MarkdownEditorField.js
+++ src/components/Form/Fields/MarkdownEditorField.js
@@ -1,24 +1,24 @@
 import React from "react";
 import Markdown from "../../MarkdownRenderer";
+import MDEditor from "../../markdown";
 
-const MarkdownEditorField = ({ placeholder, input, rows, cols, touched, error, disabled }) => (
-  <div className="markdown-editor">
-    <div className="editor">
-    <textarea
-      {...input}
-      {...{
-      placeholder,
-      rows,
-      cols,
-      // onInput: this.onChange,
-      // onChange: this.onChange
-    }} />
+const MarkdownEditorField = ({ placeholder, input, rows, cols, touched, error, disabled }) => {
+  return (
+    <div className="markdown-editor">
+      <div className="editor">
+        <MDEditor
+          placeholder={placeholder}
+          rows={rows}
+          cols={cols}
+          {...input}
+        />
+      </div>
+      {touched && error && !disabled && <span className="error">{error}</span>}
+      <div className="preview">
+        <Markdown value={input.value} />
+      </div>
     </div>
-    {touched && error && !disabled && <span className="error">{error}</span>}
-    <div className="preview">
-      <Markdown value={input.value} />
-    </div>
-  </div>
-);
+  );
+}
 
 export default MarkdownEditorField;
