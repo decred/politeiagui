86482b9c0cb21c5929503935532e1264a1433cc4
Sebastien Bramille <sebastien.bramille@gmail.com>
2017-10-12 10:45:56 +0100

    Split login and signup in 2 components

--- src/components/Signup/index.js
+++ src/components/Signup/index.js
@@ -2,6 +2,7 @@ import React, { Component } from "react";
 import { autobind } from "core-decorators";
 import { withRouter } from "react-router";
 import LoginForm from "./LoginForm";
+import SignupForm from "./SignupForm";
 import CurrentUser from "./CurrentUser";
 import loginConnector from "../../connectors/login";
 
@@ -10,11 +11,22 @@ class Login extends Component {
     this.props.onResetNewUser();
   }
 
+  componentWillReceiveProps(nextProps) {
+    if (nextProps.newUserResponse) {
+      nextProps.history.push("/user/signup/next");
+    }
+  }
+
   render() {
     return (
       <div className="login-form">
         {this.props.loggedInAs ? (
           <CurrentUser />
+        ) : this.props.signup || this.props.isShowingSignup ? (
+          <SignupForm {...{
+            onSignup: this.onSignup,
+            hideCancel: this.props.signup
+          }} />
         ) : (
           <LoginForm {...{
             onLogin: this.onLogin
@@ -22,6 +34,13 @@ class Login extends Component {
         )}
       </div>
     );
+  }
+
+  onSignup() {
+    const { password, passwordVerify } = this.state;
+    if (!password || password !== passwordVerify || !this.props.email) return;
+    this.props.onSignup(this.state.password);
+    this.setState({ password: "", passwordVerify: "" });
   }
 
   onLogin(props) {
