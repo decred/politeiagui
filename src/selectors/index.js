d0477c068b449489a85e5e054de6683aff30dde3
go1dfish <me@go1dfish.me>
2017-10-16 00:55:22 -0700

    Remove unused code

--- src/selectors/index.js
+++ src/selectors/index.js
@@ -1,5 +1,4 @@
 export * from "./api";
-export * from "./app";
 
 export const selectorMap = fns => (...args) => Object.keys(fns)
   .reduce((res, key) => ({ ...res, [key]: fns[key](...args) }), {});
