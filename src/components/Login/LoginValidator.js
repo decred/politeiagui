11256026b68aeaa4fa172d01b4e97aafa9714021
Sebastien Bramille <sebastien.bramille@gmail.com>
2017-10-18 13:30:09 +0100

    Verify email address syntax

--- src/components/Login/LoginValidator.js
+++ src/components/Login/LoginValidator.js
@@ -4,6 +4,10 @@ const validate = values => {
     errors.global = "All fields are required";
   }
 
+  if (values.email && !/^[A-Z0-9._%+-]+@[A-Z0-9.-]+\.[A-Z]{2,4}$/i.test(values.email)) {
+    errors.global = "Invalid email address";
+  }
+
   return errors;
 };
 
--- src/components/Signup/SignupValidator.js
+++ src/components/Signup/SignupValidator.js
@@ -8,6 +8,10 @@ const validate = values => {
     errors.global = "Passwords do not match";
   }
 
+  if (values.email && !/^[A-Z0-9._%+-]+@[A-Z0-9.-]+\.[A-Z]{2,4}$/i.test(values.email)) {
+    errors.global = "Invalid email address";
+  }
+
   return errors;
 };
 
