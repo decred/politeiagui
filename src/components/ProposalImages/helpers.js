0467bfc69c8b0eb78194338b42a67ec8b8f2ee69
Sebastien Bramille <sebastien.bramille@gmail.com>
2017-10-19 21:20:57 +0100

    Refactorize ProposalImages & add the isFileValid helper

---
+++ src/components/ProposalImages/helpers.js
@@ -0,0 +1,11 @@
+export function isFileValid(file, policy) {
+  if (file.size < policy.maximagesize) {
+    return false;
+  }
+
+  if (policy.validmimetypes.indexOf(file.mime) !== -1) {
+    return false;
+  }
+
+  return true;
+}
---
+++ src/components/ProposalImages/index.js
@@ -0,0 +1,35 @@
+import React, { Component } from "react";
+import { cloneDeep } from "lodash";
+import { isFileValid } from "./validator";
+
+class ProposalImages extends Component {
+  constructor(props) {
+    super(props);
+
+    this.onRemove = this.onRemove.bind(this);
+  }
+
+  onRemove(idx) {
+    const files = cloneDeep(this.props.files);
+    files.splice(idx, 1);
+    this.props.onChange(files);
+  }
+
+  render() {
+    const { files, policy } = this.props;
+
+    return (
+      <div>
+        {(files || []).map(({ name, mime, digest, payload, size }, idx) => (
+          <div key={digest || idx}>
+            <h5>{name}{isFileValid({ size, mime }, policy) ? null :  <span className="error">&nbsp;Errored</span>}</h5>
+            <img alt={name} src={`data:${mime};base64,${payload}`} />
+            <span onClick={() => this.onRemove(idx)}>REMOVE</span>
+          </div>
+        ))}
+      </div>
+    );
+  }
+}
+
+export default ProposalImages;
