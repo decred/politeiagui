c8627b3e81d5504c9e6efc7e2d1b7f2234e44ccb
go1dfish <me@go1dfish.me>
2017-10-19 16:04:53 -0400

    Disable service workers for now

--- src/index.js
+++ src/index.js
@@ -3,7 +3,9 @@ import ReactDOM from "react-dom";
 import "./style/index.css";
 import "font-awesome/css/font-awesome.min.css";
 import App from "./App";
-import registerServiceWorker from "./registerServiceWorker";
+//import registerServiceWorker from "./registerServiceWorker";
+import { unregister } from "./registerServiceWorker";
 
 ReactDOM.render(<App />, document.getElementById("root"));
-registerServiceWorker();
+
+unregister();
