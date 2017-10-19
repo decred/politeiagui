98b282c69bd2c076041b85606564c1ed722325b4
Sean Durkin <sndurkin@gmail.com>
2017-10-18 21:33:51 -0400

    Add censor/publish actions to the unvetted list.

--- src/components/ProposalUnvetted/ListItem.js
+++ src/components/ProposalUnvetted/ListItem.js
@@ -8,11 +8,19 @@ const UnvettedListItem = ({
   censorshiprecord: {
     token,
   },
+  onSubmitStatusProposal,
 }) => (
   <tr>
     <td><Link to={`/proposals/${token}/status`}>{name}</Link></td>
     <td>{getProposalStatus(status)}</td>
-    <td><Link to={`/proposals/${token}/status`}>{token}</Link></td>
+    <td><Link to={`/proposals/${token}/status`}>{token.substring(0, 7) + "..."}</Link></td>
+    {status == 2 ? (
+      <td>
+        <button onClick={() => onSubmitStatusProposal(token, 3)}>Censored</button>
+        <button onClick={() => onSubmitStatusProposal(token, 4)}>Publish</button>
+      </td>
+    ) : <td></td>}
+
   </tr>
 );
 
