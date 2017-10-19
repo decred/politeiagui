0b0de4ed4077d5764b6cffc81f0662848124749e
Sean Durkin <sndurkin@gmail.com>
2017-10-19 00:35:38 -0400

    Add status to proposal details page.

--- src/components/ProposalDetailPage.js
+++ src/components/ProposalDetailPage.js
@@ -4,6 +4,7 @@ import ProposalImages from "./ProposalImages";
 import ErrorPage from "./ErrorPage";
 import Markdown from "./MarkdownRenderer";
 import proposalConnector from "../connectors/proposal";
+import { getProposalStatus } from "../helpers";
 
 class ProposalDetailPage extends Component {
   componentDidMount() {
@@ -22,8 +23,13 @@ class ProposalDetailPage extends Component {
     return isLoading ? <LoadingPage /> : error ? <ErrorPage {...{ error }} /> : (
       <div className="page proposal-detail-page">
         <h2>{proposal.name}</h2>
-        <div>
-          Created {(new Date(proposal.timestamp * 1000)).toString()}
+        <div className="proposal-meta-data">
+          <div className="proposal-meta-data-name">Created:</div>
+          <div className="proposal-meta-data-value">{(new Date(proposal.timestamp * 1000)).toString()}</div>
+        </div>
+        <div className="proposal-meta-data">
+          <div className="proposal-meta-data-name">Status:</div>
+          <div className="proposal-meta-data-value">{getProposalStatus(proposal.status)}</div>
         </div>
         <hr />
         <Markdown value={proposal.files && proposal.files.length > 0 ? atob(proposal.files[0].payload) : ""} />
