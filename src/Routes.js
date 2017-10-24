9f86ea69df3c161340782a1bcd4450b45524195e
Sebastien Bramille <sebastien.bramille@gmail.com>
2017-10-24 15:40:33 +0100

    Add password reset route

--- src/Routes.js
+++ src/Routes.js
@@ -7,6 +7,8 @@ import SignupNext from "./components/SignupNextStepPage";
 import Signup from "./components/SignupPage";
 import ForgottenPassword from "./components/ForgottenPasswordPage";
 import ForgottenPasswordSuccess from "./components/ForgottenPassword/SuccessPage";
+import PasswordReset from "./components/PasswordResetPage";
+import PasswordResetSuccess from "./components/PasswordReset/SuccessPage";
 import Verify from "./components/Verify";
 import VerifySuccess from "./components/Verify/indexSuccess";
 import VerifyFailure from "./components/Verify/indexFailure";
@@ -34,6 +36,8 @@ class Routes extends Component {
         <Route path="/user/signup" component={Signup} />
         <Route exact path="/user/forgotten/password" component={ForgottenPassword} />
         <Route exact path="/user/forgotten/password/next" component={ForgottenPasswordSuccess} />
+        <Route exact path="/user/password/reset" component={PasswordReset} />
+        <Route exact path="/user/password/reset/next" component={PasswordResetSuccess} />
         <Route path="/user/verify/success" component={VerifySuccess} />
         <Route path="/user/verify/failure" component={VerifyFailure} />
         <Route path="/user/verify" component={Verify} exact={true} />
