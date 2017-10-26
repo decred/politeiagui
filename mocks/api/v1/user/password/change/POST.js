87f905c95c621b23896c1f63ce72427312443833
Sebastien Bramille <sebastien.bramille@gmail.com>
2017-10-26 17:12:41 +0100

    Update change password mock

---
+++ mocks/api/v1/user/password/change/POST.js
@@ -0,0 +1,20 @@
+module.exports = function (request, response) {
+  let body = [];
+  request
+    .on("data", (chunk) => {
+      body.push(chunk);
+    })
+    .on("end", () => {
+      body = Buffer.concat(body).toString();
+      // At this point, we have the headers, method, url and body, and can now
+      // do whatever we need to in order to respond to this request.
+      const bodyParsed = JSON.parse(body);
+      response.writeHead(200);
+
+      if (bodyParsed.currentpassword === "error") {
+        return response.end(JSON.stringify({ "errorcode": 2 }));
+      }
+
+      return response.end(JSON.stringify({ "errorcode": 1 }));
+    });
+};
