5c6a7df0b239f834bc12d4f868a5077723130e51
Sebastien Bramille <sebastien.bramille@gmail.com>
2017-11-05 12:05:07 +0000

    Add nightwatch configuration

---
+++ test/e2e/nightwatch.globals.js
@@ -0,0 +1,28 @@
+module.exports = {
+
+   // Abord all on test fail
+   abortOnAssertionFailure: true,
+
+   // Duration between two checks
+   waitForConditionPollInterval: 300,
+
+   // Timeout duration
+   waitForConditionTimeout: 1000,
+
+   /*
+    * Define if the test failed when many HTML elements are found when
+    * we expect only one
+    */
+   throwOnMultipleElementsReturned: false,
+
+   // Before/After Hooks of all tests
+   before: next => next(),
+   after: next => next(),
+
+   // Before/After Hooks of test suites
+   beforeEach: (browser, next) => next(),
+   afterEach: (browser, next) => next(),
+
+   // To customize output report
+   reporter: (results, next) => next()
+};
