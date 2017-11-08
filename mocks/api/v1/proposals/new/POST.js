5fb4e17f1f24221f9065194be1870f4c8d39c13a
Sean Durkin <sndurkin@gmail.com>
2017-11-08 16:17:28 -0500

    Add mock fixes from go1dfish.

--- mocks/api/v1/proposals/new/POST.js
+++ mocks/api/v1/proposals/new/POST.js
@@ -16,7 +16,7 @@ module.exports = function (request, response) {
       }
 
       return response.end(JSON.stringify({ censorshiprecord: {
-        "token": "337fc4762dac6bbe11d3d0130f33a09978004b190e6ebbbde9312ac63f223527",
+        "token": "6284c5f8fba5665373b8e6651ebc8747b289fed242d2f880f64a284496bb4ca8",
         "merkle": "0dd10219cd79342198085cbe6f737bd54efe119b24c84cbc053023ed6b7da4c8",
         "signature": "fcc92e26b8f38b90c2887259d88ce614654f32ecd76ade1438a0def40d360e461d995c796f16a17108fad226793fd4f52ff013428eda3b39cd504ed5f1811d0d"
       } }));
