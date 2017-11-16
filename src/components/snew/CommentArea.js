12d838cc70508a3cd41a1bc401e36abdb1279b13
Tiago Alves Dulce <tiagoalvesdulce@outlook.com>
2017-11-16 20:05:44 -0200

    Hide CommentArea for unvetted proposals

--- src/components/snew/CommentArea.js
+++ src/components/snew/CommentArea.js
@@ -2,14 +2,14 @@ import React from "react";
 import { CommentArea as CommentAreaBase } from "snew-classic-ui";
 import connector from "../../connectors/proposal";
 
-const CommentArea = ({ comments, loggedIn, ...props }) => (
-  <CommentAreaBase {...{
-    ...props,
-    num_comments: comments.length,
-    locked: !loggedIn,
-    currentSort: "new",
-    sortOptions: ["new"]
-  }} />
+const CommentArea = ({ comments, loggedIn, proposal, ...props }) => (
+  !loggedIn || proposal.status === 2 || proposal.status === 3 ? null :
+    <CommentAreaBase {...{
+      ...props,
+      num_comments: comments.length,
+      currentSort: "new",
+      sortOptions: ["new"]
+    }} />
 );
 
 export default connector(CommentArea);
