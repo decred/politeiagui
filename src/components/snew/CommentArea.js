f76b26b42f476000e2795eb51f0ba28ac35bc865
Tiago Alves Dulce <tiagoalvesdulce@outlook.com>
2017-11-17 12:29:39 -0200

    Hide comment form on unvetted proposals (#105)
    * Create unvetted proposal example
    
    * Hide CommentArea for unvetted proposals
    
    * Use status constants
    
    * Lock CommentArea when user is not logged
    
    * Fix name of unreviewed example proposal

--- src/components/snew/CommentArea.js
+++ src/components/snew/CommentArea.js
@@ -1,15 +1,18 @@
 import React from "react";
 import { CommentArea as CommentAreaBase } from "snew-classic-ui";
 import connector from "../../connectors/proposal";
+import { PROPOSAL_STATUS_CENSORED, PROPOSAL_STATUS_UNREVIEWED } from "../../constants";
 
-const CommentArea = ({ comments, loggedIn, ...props }) => (
-  <CommentAreaBase {...{
-    ...props,
-    num_comments: comments.length,
-    locked: !loggedIn,
-    currentSort: "new",
-    sortOptions: ["new"]
-  }} />
+
+const CommentArea = ({ comments, loggedIn, proposal, ...props }) => (
+  proposal.status === PROPOSAL_STATUS_UNREVIEWED || proposal.status === PROPOSAL_STATUS_CENSORED ? null :
+    <CommentAreaBase {...{
+      ...props,
+      locked: !loggedIn,
+      num_comments: comments.length,
+      currentSort: "new",
+      sortOptions: ["new"]
+    }} />
 );
 
 export default connector(CommentArea);
