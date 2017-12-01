512041924d5d585f0ec50b58b5f757c2f60f5dc1
go1dfish <me@go1dfish.me>
2017-11-30 17:58:00 -0800

    Sign verification token as string instead of hex array

--- src/lib/api.js
+++ src/lib/api.js
@@ -89,7 +89,7 @@ export const newUser = (csrf, email, password) => pki.myPubKeyHex().then(publick
 
 export const verifyNewUser = searchQuery => {
   const { email, verificationtoken } = qs.parse(searchQuery);
-  return pki.signHex(hexToArray(verificationtoken))
+  return pki.signStringHex(verificationtoken)
     .then(signature => GET("/v1/user/verify?" + qs.stringify({ email, verificationtoken, signature })))
     .then(getResponse);
 };
