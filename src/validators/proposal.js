f232e353ea94a5abca774a67cb2e86a8ba2353be
Tiago Alves Dulce <tiagoalvesdulce@outlook.com>
2017-12-04 13:23:02 -0200

    Add characters validation to the proposal name field (#123)
    * Add supportedcharacters to the mocks
    
    * Add arrayToRegex and proposalNameValidator functions
    
    * Add proposal name characters validation
    
    * Remove log
    
    * Merge error messages for the proposal name.

--- src/validators/proposal.js
+++ src/validators/proposal.js
@@ -1,18 +1,20 @@
 import { forEach } from "lodash";
 import { SubmissionError } from "redux-form";
 import { isFileValid } from "../components/ProposalImages/helpers";
-import { isRequiredValidator } from "./util";
+import { isRequiredValidator, proposalNameValidator } from "./util";
 
 const validate = (values, dispatch, props) => {
   if (!isRequiredValidator(values.name) || !isRequiredValidator(values.description)) {
-    throw new SubmissionError({ _error: "All fields are required" });
+    throw new SubmissionError({ _error: "You must provide both a proposal name and description." });
   }
 
-  if (props.policy.minnamelength && (values.name.length < props.policy.minnamelength)) {
-    throw new SubmissionError({ _error: `Name must be at least ${props.policy.minnamelength} characters` });
-  }
-  if (props.policy.maxnamelength && (values.name.length > props.policy.maxnamelength)) {
-    throw new SubmissionError({ _error: `Name must be less than ${props.policy.maxnamelength} characters` });
+  if ((props.policy.minnamelength && (values.name.length < props.policy.minnamelength)) ||
+     (props.policy.maxnamelength && (values.name.length > props.policy.maxnamelength)) ||
+     (props.policy.supportedcharacters && !proposalNameValidator(values.name, props.policy.supportedcharacters))) {
+    throw new SubmissionError({
+      _error: `The proposal name must be between ${props.policy.minnamelength} and ${props.policy.maxnamelength} characters long ` +
+        `and only contain the following characters: ${props.policy.supportedcharacters.join(" ")}`
+    });
   }
 
   if (values.files) {
