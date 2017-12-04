3eb563885605d8b95122de9ede2185ce9c0b89ad
Tiago Alves Dulce <tiagoalvesdulce@outlook.com>
2017-12-04 17:58:16 -0200

    Add comment length validation

--- src/validators/reply.js
+++ src/validators/reply.js
@@ -1,9 +1,15 @@
 import { SubmissionError } from "redux-form";
 import { isRequiredValidator } from "./util";
 
-const validate = values => {
+const validate = (values, policy) => {
   if (!isRequiredValidator(values.comment)) {
     throw new SubmissionError({ _error: "A comment body is required" });
+  }
+
+  if (policy.maxcommentlength && (values.comment.length > policy.maxcommentlength)) {
+    throw new SubmissionError({
+      _error: `The comment must be less than ${policy.maxcommentlength} characters long`
+    });
   }
 };
 
