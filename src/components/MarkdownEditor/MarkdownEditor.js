5d21da49dc8509070255e7e801f67cd75255273c
Fernando Abolafio <fernando@fredbots.com>
2017-12-05 20:04:06 -0200

    Set default markdown parser (snew/Markdown.js)

--- src/components/MarkdownEditor/MarkdownEditor.js
+++ src/components/MarkdownEditor/MarkdownEditor.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import PropTypes from "prop-types";
-import { ReactMdeToolbar, ReactMdeTextArea, ReactMdeCommands, ReactMdePreview } from 'react-mde';
+import { ReactMdeToolbar, ReactMdeTextArea, ReactMdeCommands } from 'react-mde';
 import {
     getSelection,
 } from "react-mde/lib/js/helpers/ReactMdeSelectionHelper";
@@ -8,6 +8,7 @@ import 'react-mde/lib/styles/css/react-mde.css';
 import 'react-mde/lib/styles/css/react-mde-toolbar.css';
 import 'react-mde/lib/styles/css/react-mde-textarea.css';
 import 'react-mde/lib/styles/css/react-mde-preview.css';
+import MarkdownPreview from './MarkdownPreview';
 
 class MarkdownEditor extends React.Component {
   textArea: HTMLTextAreaElement;
@@ -44,10 +45,7 @@ class MarkdownEditor extends React.Component {
   }
 
   render() {
-    const {
-      value,
-      showdownOptions
-    } = this.props;
+    const { value } = this.props;
     return (
       <div className="react-mde">
         <ReactMdeToolbar
@@ -59,11 +57,7 @@ class MarkdownEditor extends React.Component {
             value={value}
             textAreaRef={(c) => this.textArea = c}
         />
-        <ReactMdePreview
-                markdown={value.text}
-                previewRef={(c) => this.preview = c}
-                showdownOptions={showdownOptions}
-            />
+        <MarkdownPreview body={value.text} />
       </div>
     );
   }
@@ -71,7 +65,6 @@ class MarkdownEditor extends React.Component {
 
 MarkdownEditor.propTypes = {
   value: PropTypes.object.isRequired,
-  showdownOptions: PropTypes.object,
 };
 
 export default MarkdownEditor;
