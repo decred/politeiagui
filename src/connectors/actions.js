35ac23c73dad22bce7cacb550c222cb8ceecb88f
Sean Durkin <sndurkin@gmail.com>
2017-12-08 00:24:38 -0500

    Fix bug with error flow for setting proposal status.

--- src/connectors/actions.js
+++ src/connectors/actions.js
@@ -7,6 +7,7 @@ const actions = connect(
     loggedInAs: sel.loggedInAs,
     email: sel.email,
     isAdmin: sel.isAdmin,
+    setStatusProposalToken: sel.setStatusProposalToken,
     setStatusProposalError: sel.setStatusProposalError,
   }),
   {
