5e9fc79f5e233db96cfb42920a9fc3a33ea06a37
Fernando Abolafio <fernando@fredbots.com>
2017-12-12 11:34:35 -0200

    Replace markdown by react-markdown

--- src/components/snew/Markdown.js
+++ src/components/snew/Markdown.js
@@ -1,10 +1,11 @@
 import React from "react";
 import Snudownd from "snuownd";
+import ReactMarkdown from 'react-markdown';
 const parser = Snudownd.getParser();
 
 const MarkdownRenderer = ({ body, className }) => (
   <div className={className}>
-    <div className="md" dangerouslySetInnerHTML={{__html: parser.render(body || "")}} />
+    <ReactMarkdown className="md" source={body}/>
   </div>
 );
 
