5b6a7a99c83319733076bfe8b52bffbc938455ff
Fernando Abolafio <fernandohsabolafio@gmail.com>
2017-12-13 17:51:17 -0200

    improve markdown  (#145)
    * Replace markdown by react-markdown
    
    * Remove compile warnings for this PR and clear snuownd deps
    
    * Custom renderers for image and link tags
    
    * Remove console log

--- src/components/snew/Markdown.js
+++ src/components/snew/Markdown.js
@@ -1,10 +1,22 @@
 import React from "react";
-import Snudownd from "snuownd";
-const parser = Snudownd.getParser();
+import ReactMarkdown from 'react-markdown';
+
+const customRenderers = {
+  image: ({src, alt}) => {
+    return <a rel="nofollow" href={src}>{alt}</a>
+  },
+  link: ({href, children}) => {
+    return <a rel="nofollow" href={href}>{children[0]}</a>;
+  }
+}
 
 const MarkdownRenderer = ({ body, className }) => (
   <div className={className}>
-    <div className="md" dangerouslySetInnerHTML={{__html: parser.render(body || "")}} />
+    <ReactMarkdown 
+      className="md" 
+      renderers={customRenderers}
+      source={body}  
+    />
   </div>
 );
 
