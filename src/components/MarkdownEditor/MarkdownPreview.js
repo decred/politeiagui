66ec56c54baf15d270cb948c050f411b694d8f67
Tiago Alves Dulce <tiagoalvesdulce@outlook.com>
2017-12-17 13:41:30 -0200

    Increase submit page roundfield width to 900px

--- src/components/MarkdownEditor/MarkdownPreview.js
+++ src/components/MarkdownEditor/MarkdownPreview.js
@@ -1,13 +1,6 @@
-<<<<<<< HEAD
 import React from "react";
 import PropTypes from "prop-types";
-import MarkdownHelp from "./MarkdownHelp";
 import Markdown from "../snew/Markdown";
-=======
-import React from 'react';
-import PropTypes from 'prop-types';
-import Markdown from '../snew/Markdown';
->>>>>>> 1c51800... Move text preview next to the editor
 
 const MarkdownPreview = ({ body }) => (
   <div className="mde-preview">
