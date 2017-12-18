91e9a72d0fd0103229cc1cbccca782aedb149ba6
Tiago Alves Dulce <tiagoalvesdulce@outlook.com>
2017-12-18 14:32:35 -0200

    Wait 1 second to show loading icon on page transitions

--- src/components/snew/PageLoadingIcon.js
+++ src/components/snew/PageLoadingIcon.js
@@ -1,11 +1,36 @@
 import React from "react";
 import LoadingIcon from "./LoadingIcon";
 
-const PageLoadingIcon = ({ hidden }) => (
-  <LoadingIcon
-    hidden={hidden}
-    width={200}
-    style={{ margin: "300px auto 0 auto" }} />
-);
-
+class PageLoadingIcon extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      show: this.props.hidden || false
+    };
+  }
+  componentDidMount() {
+    if (!this.props.hidden) {
+      this.loadingIconDelay = setTimeout(() => {
+        this.setState({ show: true });
+        this.loadingIconDelay = 0;
+      }, 1000);
+    }
+  }
+  componentWillUnmount() {
+    if (this.loadingIconDelay) {
+      clearTimeout(this.loadingIconDelay);
+      this.loadingIconDelay = 0;
+    }
+  }
+  render () {
+    const { show } = this.state;
+    console.log(show);
+    return (
+      <LoadingIcon
+        hidden={!show}
+        width={200}
+        style={{ margin: "300px auto 0 auto" }} />
+    );
+  }
+}
 export default PageLoadingIcon;
