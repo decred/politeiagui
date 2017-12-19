43a2e891a8ee77c801c3001c5566d2e29f70c371
Sean Durkin <sndurkin@gmail.com>
2017-12-19 15:29:33 -0500

    Provide some extra information for email verification messages. (#161)

--- src/components/ForgottenPassword/SuccessPage.js
+++ src/components/ForgottenPassword/SuccessPage.js
@@ -1,9 +1,29 @@
 import React from "react";
+import forgottenPasswordNext from "../../connectors/forgottenPasswordNext";
 
-const SuccessPage = () => (
+const SuccessPage = ({ email }) => (
   <div className="page forgotten-password-next-step-page">
-    <p>Please check your inbox to reset your password.</p>
+    <h3>Please check your inbox to reset your password.</h3>
+    <p>
+      Note that for privacy reasons, Politeia does not disclose whether
+      an email address has been registered. If you don't receive
+      an email:
+    </p>
+    <ul>
+      {email ? (
+        <li>
+          Check that <span className="email-address">{email}</span> is the
+          correct address.
+        </li>
+      ) : null}
+      <li>Check your spam folder!</li>
+    </ul>
+    <p>
+      If you're sure you should have received an email, join the <code>#support</code> channel
+      on <a href="https://docs.decred.org/support-directory/#join-us-on-slack">our
+      Slack</a> to get assistance from Politeia administrators.
+    </p>
   </div>
 );
 
-export default SuccessPage;
+export default forgottenPasswordNext(SuccessPage);
--- src/components/SignupNextStepPage.js
+++ src/components/SignupNextStepPage.js
@@ -1,9 +1,34 @@
 import React from "react";
+import signupNext from "../connectors/signupNext";
 
-const SignupNextStepPage = () => (
+const SignupNextStepPage = ({ email }) => (
   <div className="page signup-next-step-page">
-    <p>Please check your inbox to verify your registration.</p>
+    <h3>Please check your inbox to verify your registration.</h3>
+    <p>
+      Note that for privacy reasons, Politeia does not disclose whether
+      an email address has already been registered. If you don't receive
+      an email:
+    </p>
+    <ul>
+      {email ? (
+        <li>
+          Check that <span className="email-address">{email}</span> is the
+          correct address.
+        </li>
+      ) : null}
+      <li>Check your spam folder!</li>
+      <li>
+        Make sure you don't already have an account on Politeia with this
+        email address. If you do, you should <a href="/password">reset your
+        account</a> instead.
+      </li>
+    </ul>
+    <p>
+      If you're sure you should have received an email, join the <code>#support</code> channel
+      on <a href="https://docs.decred.org/support-directory/#join-us-on-slack">our
+      Slack</a> to get assistance from Politeia administrators.
+    </p>
   </div>
 );
 
-export default SignupNextStepPage;
+export default signupNext(SignupNextStepPage);
