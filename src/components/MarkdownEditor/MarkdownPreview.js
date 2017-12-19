5aacac829928f209b559f0f12d4145505742b384
Fernando Abolafio <fernandohsabolafio@gmail.com>
2017-12-19 17:53:04 -0200

    Inform users that 2 newlines are needed to create new paragraphs (#150) (#154)

--- src/components/MarkdownEditor/MarkdownPreview.js
+++ src/components/MarkdownEditor/MarkdownPreview.js
@@ -1,9 +1,9 @@
-import React from 'react';
-import PropTypes from 'prop-types';
-import MarkdownHelp from './MarkdownHelp';
-import Markdown from '../snew/Markdown';
+import React from "react";
+import PropTypes from "prop-types";
+import MarkdownHelp from "./MarkdownHelp";
+import Markdown from "../snew/Markdown";
 
-const MarkdownPreview = ({body}) => (
+const MarkdownPreview = ({ body }) => (
   <div className="mde-preview">
     <Markdown body={body} className="mde-preview-content"/>
     <div className="mde-help">
--- src/components/Message.js
+++ src/components/Message.js
@@ -4,16 +4,24 @@ const Message = ({
   type,
   header,
   body
-}) => (
-  <div className={"message-ct message-" + type}>
-    <div className="message-icon">{type === "success" ? "✔" : "✖"}</div>
-    <div className="message-text">
-      <div className="message-header">{header}</div>
-      <div className="message-body">
-        {(body instanceof Error) ? body.message : body}
-      </div>
+}) => {
+  const mapTypeToIcon = {
+    success: "✔",
+    error: "✖",
+    info: "ℹ︎"
+  }
+  const icon = mapTypeToIcon[type] ? mapTypeToIcon[type] : mapTypeToIcon.error;
+  return (
+    <div className={"message-ct message-" + type}>
+      <div className="message-icon">{icon}</div>
+        <div className="message-text">
+          <div className="message-header">{header}</div>
+            <div className="message-body">
+              {(body instanceof Error) ? body.message : body}
+            </div>
+        </div>
     </div>
-  </div>
-);
+  )
+};
 
 export default Message;
