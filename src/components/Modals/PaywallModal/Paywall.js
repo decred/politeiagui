9db2e497dda95b934f1a70340ea266161af7500c
vctt94 <victor.duarte.oliveira@gmail.com>
2017-12-26 17:03:59 -0200

    editing style

--- src/components/Modals/PaywallModal/Paywall.js
+++ src/components/Modals/PaywallModal/Paywall.js
@@ -6,8 +6,17 @@ const Modal = ({
   onSubmit,
   isHidden
 }) => (
-  <div hidden={isHidden}>
-    this is the paywall modal
+  <div className="paywall-modal-content">
+    <div className="modal-paywall-header">
+      <h2 className="modal-paywall-header-title">Paywall</h2>
+      <a href="#" className="modal-paywall-header-close" aria-hidden="true">×</a>
+    </div>
+    <div className="modal-paywall-content">
+      <p>This is the paywall</p>
+    </div>
+    <div className="modal-paywall-footer">
+      <a href="#" class="btn">Pay!</a>
+    </div>
   </div>
 );
 
