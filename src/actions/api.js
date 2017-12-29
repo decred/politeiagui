6fd0df2341edda30683da1d4f72ca09898aec6dc
Tiago Alves Dulce <tiagoalvesdulce@outlook.com>
2017-12-29 19:39:16 -0200

    Always call / on init

--- src/actions/api.js
+++ src/actions/api.js
@@ -8,11 +8,23 @@ export const onSetEmail = act.SET_EMAIL;
 
 export const onInit = () => dispatch => {
   dispatch(act.REQUEST_ME());
-  return api.me()
-    .then(response => dispatch(act.RECEIVE_ME(response)))
+  return api
+    .me()
+    .then(response => {
+      dispatch(act.RECEIVE_ME(response));
+      dispatch(act.REQUEST_INIT_SESSION());
+      return api
+        .apiInfo()
+        .then(response => dispatch(act.RECEIVE_INIT_SESSION(response)))
+        .catch(error => {
+          dispatch(act.RECEIVE_INIT_SESSION(null, error));
+          throw error;
+        });
+    })
     .catch(() => {
       dispatch(act.REQUEST_INIT_SESSION());
-      return api.apiInfo()
+      return api
+        .apiInfo()
         .then(response => dispatch(act.RECEIVE_INIT_SESSION(response)))
         .catch(error => {
           dispatch(act.RECEIVE_INIT_SESSION(null, error));
@@ -25,16 +37,16 @@ export const onRouteChange = () => dispatch => {
   dispatch(act.CLEAN_ERRORS());
 };
 
-export const onGetPolicy = () =>
-  dispatch => {
-    dispatch(act.REQUEST_POLICY());
-    return api.policy()
-      .then(response => dispatch(act.RECEIVE_POLICY(response)))
-      .catch(error => {
-        dispatch(act.RECEIVE_POLICY(null, error));
-        throw error;
-      });
-  };
+export const onGetPolicy = () => dispatch => {
+  dispatch(act.REQUEST_POLICY());
+  return api
+    .policy()
+    .then(response => dispatch(act.RECEIVE_POLICY(response)))
+    .catch(error => {
+      dispatch(act.RECEIVE_POLICY(null, error));
+      throw error;
+    });
+};
 
 export const withCsrf = fn => (dispatch, getState) => {
   const csrf = sel.csrf(getState());
@@ -57,9 +69,10 @@ export const onCreateNewUser = ({ email, password }) =>
 
 export const onResetNewUser = act.RESET_NEW_USER;
 
-export const onVerifyNewUser = (searchQuery) => (dispatch) => {
+export const onVerifyNewUser = searchQuery => dispatch => {
   dispatch(act.REQUEST_VERIFY_NEW_USER(searchQuery));
-  return api.verifyNewUser(searchQuery)
+  return api
+    .verifyNewUser(searchQuery)
     .then(res => dispatch(act.RECEIVE_VERIFY_NEW_USER(res)))
     .catch(err => {
       dispatch(act.RECEIVE_VERIFY_NEW_USER(null, err));
@@ -67,7 +80,7 @@ export const onVerifyNewUser = (searchQuery) => (dispatch) => {
     });
 };
 
-export const onSignup = (props) => (dispatch) => dispatch(onCreateNewUser(props));
+export const onSignup = props => dispatch => dispatch(onCreateNewUser(props));
 
 export const onLogin = ({ email, password }) =>
   withCsrf((dispatch, getState, csrf) => {
@@ -94,11 +107,11 @@ export const onLogout = () =>
       .catch(error => dispatch(act.RECEIVE_LOGOUT(null, error)));
   });
 
-export const onChangePassword = ( password, newPassword ) =>
+export const onChangePassword = (password, newPassword) =>
   withCsrf((dispatch, getState, csrf) => {
     dispatch(act.REQUEST_CHANGE_PASSWORD());
     return api
-      .changePassword(csrf,  password, newPassword)
+      .changePassword(csrf, password, newPassword)
       .then(response => dispatch(act.RECEIVE_CHANGE_PASSWORD(response)))
       .catch(error => {
         dispatch(act.RECEIVE_CHANGE_PASSWORD(null, error));
@@ -106,7 +119,7 @@ export const onChangePassword = ( password, newPassword ) =>
       });
   });
 
-export const onFetchVetted = () => (dispatch) => {
+export const onFetchVetted = () => dispatch => {
   dispatch(act.REQUEST_VETTED());
   return api
     .vetted()
@@ -114,7 +127,7 @@ export const onFetchVetted = () => (dispatch) => {
     .catch(error => dispatch(act.RECEIVE_VETTED(null, error)));
 };
 
-export const onFetchUnvetted = () => (dispatch) => {
+export const onFetchUnvetted = () => dispatch => {
   dispatch(act.REQUEST_UNVETTED());
   return api
     .unvetted()
@@ -122,7 +135,7 @@ export const onFetchUnvetted = () => (dispatch) => {
     .catch(error => dispatch(act.RECEIVE_UNVETTED(null, error)));
 };
 
-export const onFetchProposal = (token) => (dispatch) => {
+export const onFetchProposal = token => dispatch => {
   dispatch(act.REQUEST_PROPOSAL(token));
   return api
     .proposal(token)
@@ -130,7 +143,7 @@ export const onFetchProposal = (token) => (dispatch) => {
     .catch(error => dispatch(act.RECEIVE_PROPOSAL(null, error)));
 };
 
-export const onFetchProposalComments = (token) => (dispatch) => {
+export const onFetchProposalComments = token => dispatch => {
   dispatch(act.REQUEST_PROPOSAL_COMMENTS(token));
   return api
     .proposalComments(token)
@@ -160,7 +173,7 @@ export const onSubmitComment = (loggedInAs, token, comment, parentid) =>
       });
   });
 
-const statusName = key => ({3: "censor", 4: "publish"}[key]);
+const statusName = key => ({ 3: "censor", 4: "publish" }[key]);
 
 export const onSubmitStatusProposal = (loggedInAs, token, status) =>
   window.confirm(`Are you sure you want to ${statusName(status)} this proposal?`)
@@ -173,26 +186,37 @@ export const onSubmitStatusProposal = (loggedInAs, token, status) =>
     })
     : {type: "NOOP"};
 
-export const redirectedFrom = location => dispatch => dispatch(act.REDIRECTED_FROM(location));
-export const resetRedirectedFrom = () => dispatch => dispatch(act.RESET_REDIRECTED_FROM());
+export const redirectedFrom = location => dispatch =>
+  dispatch(act.REDIRECTED_FROM(location));
+export const resetRedirectedFrom = () => dispatch =>
+  dispatch(act.RESET_REDIRECTED_FROM());
 
 export const onForgottenPasswordRequest = ({ email }) =>
   withCsrf((dispatch, getState, csrf) => {
     dispatch(act.REQUEST_FORGOTTEN_PASSWORD_REQUEST({ email }));
     return api
       .forgottenPasswordRequest(csrf, email)
-      .then(response => dispatch(act.RECEIVE_FORGOTTEN_PASSWORD_REQUEST(response)))
+      .then(response =>
+        dispatch(act.RECEIVE_FORGOTTEN_PASSWORD_REQUEST(response))
+      )
       .catch(error => {
         dispatch(act.RECEIVE_FORGOTTEN_PASSWORD_REQUEST(null, error));
         throw error;
       });
   });
 
-export const resetForgottenPassword = () => dispatch => dispatch(act.RESET_FORGOTTEN_PASSWORD_REQUEST());
+export const resetForgottenPassword = () => dispatch =>
+  dispatch(act.RESET_FORGOTTEN_PASSWORD_REQUEST());
 
-export const onPasswordResetRequest = ({ email, verificationtoken, password }) =>
+export const onPasswordResetRequest = ({
+  email,
+  verificationtoken,
+  password
+}) =>
   withCsrf((dispatch, getState, csrf) => {
-    dispatch(act.REQUEST_PASSWORD_RESET_REQUEST({ email, verificationtoken, password }));
+    dispatch(
+      act.REQUEST_PASSWORD_RESET_REQUEST({ email, verificationtoken, password })
+    );
     return api
       .passwordResetRequest(csrf, email, verificationtoken, password)
       .then(response => dispatch(act.RECEIVE_PASSWORD_RESET_REQUEST(response)))
@@ -202,4 +226,5 @@ export const onPasswordResetRequest = ({ email, verificationtoken, password }) =
       });
   });
 
-export const resetPasswordReset = () => dispatch => dispatch(act.RESET_PASSWORD_RESET_REQUEST);
+export const resetPasswordReset = () => dispatch =>
+  dispatch(act.RESET_PASSWORD_RESET_REQUEST);
