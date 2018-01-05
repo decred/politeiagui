f8add12dfbd78cb39931c8cd9d3379e37b53e086
vctt94 <victor.duarte.oliveira@gmail.com>
2018-01-05 17:24:12 -0200

    adding timeout when response is null

--- src/actions/external_api.js
+++ src/actions/external_api.js
@@ -2,35 +2,39 @@ import * as external_api from "../lib/external_api";
 import act from "./methods";
 
 const CONFIRMATIONS_REQUIRED = 2;
-
+const TIME_IN_SECONDS = 10;
 export const getPaymentsByAddress = (address, amount) => dispatch => {
   dispatch(act.REQUEST_VERIFY_PAYWALL_PAYMENT());
   return external_api.getPaymentsByAddress(address)
     .then(response => {
       if(response === null) {
         dispatch(act.GRANT_SUBMIT_PROPOSAL_ACCESS(false));
-        return;
-      }
-      dispatch(act.RECEIVE_VERIFY_PAYWALL_PAYMENT(response));
-      const txid = checkForPayment(response, address, amount);
-      if (txid) {
-        dispatch(act.GET_PAYWALL_TXID({txid}));
-        dispatch(act.GRANT_SUBMIT_PROPOSAL_ACCESS(true));
-      }
-      else {
-        dispatch(act.GRANT_SUBMIT_PROPOSAL_ACCESS(false));
-        setTimeout(()=> dispatch(getPaymentsByAddress(address, amount)), 10*1000);
+        setTimeout(()=> dispatch(getPaymentsByAddress(address, amount)), TIME_IN_SECONDS*1000);
+      } else {
+        dispatch(act.RECEIVE_VERIFY_PAYWALL_PAYMENT(response));
+        const txid = checkForPayment(response, address, amount);
+        if (txid) {
+          dispatch(act.GET_PAYWALL_TXID({txid}));
+          dispatch(act.GRANT_SUBMIT_PROPOSAL_ACCESS(true));
+          return;
+        }
+        else {
+          dispatch(act.GRANT_SUBMIT_PROPOSAL_ACCESS(false));
+          setTimeout(()=> dispatch(getPaymentsByAddress(address, amount)), TIME_IN_SECONDS*1000);
+        }
       }
     })
     .catch(error => {
       dispatch(act.GRANT_SUBMIT_PROPOSAL_ACCESS(false));
       dispatch(act.RECEIVE_VERIFY_PAYWALL_PAYMENT(null, error));
-      setTimeout(()=> dispatch(getPaymentsByAddress(address, amount)), 10*1000);
+      setTimeout(()=> dispatch(getPaymentsByAddress(address, amount)), TIME_IN_SECONDS*1000);
       throw error;
     });
 };
 
 const checkForPayment = (poll, addressToMatch, amount) => {
+  if (!poll)
+    return;
   let txid;
   poll.forEach((transaction) => {
     txid = checkTransaction(transaction, addressToMatch, amount);
