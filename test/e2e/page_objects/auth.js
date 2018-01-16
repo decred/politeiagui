8d83369f55e1a360403c21ad978ec07b03b52759
Sebastien Bramille <sebastien.bramille@gmail.com>
2018-01-16 18:26:38 +0000

    Fix test

--- test/e2e/page_objects/auth.js
+++ test/e2e/page_objects/auth.js
@@ -81,7 +81,7 @@ const Commands = {
       .clearValue("@signupInputPasswordVerify")
       .setValue("@signupInputPasswordVerify", "test")
       .click("@signupSubmitButton")
-      .waitForElementVisible("@loggedIn", 3000);
+      .waitForElementVisible("@signupSuccess", 3000);
   },
 };
 
