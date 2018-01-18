4f01e9d7ac478b90e9e9286b7444a0980fdf832f
Fernando Abolafio <fernandohsabolafio@gmail.com>
2018-01-18 18:03:39 -0200

    Improve logout message (#176) (#176)

--- src/components/LogoutPage.js
+++ src/components/LogoutPage.js
@@ -16,7 +16,13 @@ class LogoutPage extends Component {
       <ErrorPage {...{ error }} />
     ) : (
       <article className="page logout-page">
-        <h2>Successfully Logged Out</h2>
+        <h3>You are now logged out. Thanks for stopping by, and have a great day.</h3>
+        <p>Here are some links that might be useful for you:</p>
+        <ul>
+          <li><a href="/user/signup">Log back in</a></li>
+          <li><a href="https://www.decred.org/">decred.org</a></li>
+          <li><a href="https://github.com/decred/politeia">Politeia</a> and <a href="https://github.com/decred/politeiagui">PoliteiaGUI</a> on Github</li>
+        </ul>
       </article>
     );
   }
