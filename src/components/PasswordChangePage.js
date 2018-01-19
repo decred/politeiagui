433aade4d0085dae996a2e1c6db7cbf10a9e7bef
Tiago Alves Dulce <tiagoalvesdulce@outlook.com>
2018-01-19 12:44:22 -0200

    Add PasswordChangePage component

---
+++ src/components/PasswordChangePage.js
@@ -0,0 +1,13 @@
+import React from "react";
+import PasswordChange from "./PasswordChange";
+
+const PasswordChangePage = () => (
+  <div className="content" role="main">
+    <div className="page user-profile-page">
+      <h1>Change Password</h1>
+      <PasswordChange />
+    </div>
+  </div>
+);
+
+export default PasswordChangePage;
