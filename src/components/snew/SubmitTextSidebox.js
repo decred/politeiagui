c92c08616de8434e144f6f18d9d4b90c94623ed3
vctt94 <victor.duarte.oliveira@gmail.com>
2018-01-20 12:59:42 -0200

    Warning if paywall not paid

--- src/components/snew/SubmitTextSidebox.js
+++ src/components/snew/SubmitTextSidebox.js
@@ -1,26 +1,29 @@
 import React from "react";
 import connector from "../../connectors/actions";
+import WarningPaywallNotPaid from "./WarningPaywallNotPaid";
 
 const SubmitTextSidebox = ({
-  Link, loggedInAs
-}) => loggedInAs ? (
-  <div className="spacer">
-    <div className="sidebox submit submit-text">
-      <div className="morelink">
-        <Link
-          className="login-required access-required"
-          data-event-action="submit"
-          data-event-detail="self"
-          data-type="subreddit"
-          href="/proposals/new"
-        >
-          Submit a new Proposal
-        </Link>
-        <div className="nub" />
+  Link, loggedInAs, grantAccess
+}) => loggedInAs ?
+  grantAccess ? (
+    <div className="spacer">
+      <div className="sidebox submit submit-text">
+        <div className="morelink">
+          <Link
+            className="login-required access-required"
+            data-event-action="submit"
+            data-event-detail="self"
+            data-type="subreddit"
+            href="/proposals/new"
+          >
+            Submit a new Proposal
+          </Link>
+          <div className="nub" />
+        </div>
       </div>
     </div>
-  </div>
-) : null;
+  ) : <WarningPaywallNotPaid />
+  : null;
 
 export default connector(SubmitTextSidebox);
 
---
+++ src/components/snew/WarningPaywallNotPaid.js
@@ -0,0 +1,18 @@
+import React from "react";
+
+const WarningPaywallNotPaid = () => (<div className="spacer">
+  <div className="titlebox">
+    <form
+      action="#"
+      className="usertext warn-on-unload"
+      id="form-t4_5rve"
+    >
+      <p>
+        You can not send Proposals before paying The Paywall
+      </p>
+    </form>
+    <div className="clear" />
+  </div>
+</div>);
+
+export default WarningPaywallNotPaid;
