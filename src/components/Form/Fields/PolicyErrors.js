a0d75952f63709b85c9307132a6ff3fda9b024e7
Tiago Alves Dulce <tiagoalvesdulce@outlook.com>
2018-01-30 16:59:37 -0200

    Add jsx-quotes prefer-double rule

--- src/components/Form/Fields/PolicyErrors.js
+++ src/components/Form/Fields/PolicyErrors.js
@@ -4,7 +4,7 @@ import Message from "../../Message";
 const PolicyErrors = ({errors}) => (
   <div>
     {
-      errors.map((error, idx) => <Message key={idx} body={error} type='error' />)
+      errors.map((error, idx) => <Message key={idx} body={error} type="error" />)
     }
   </div>
 );
