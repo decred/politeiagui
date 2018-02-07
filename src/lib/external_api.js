58bebee3088d04146c635b71aaafe3eb5b4441bb
vctt94 <victor.duarte.oliveira@gmail.com>
2018-02-07 17:03:26 -0200

    form request working

--- src/lib/external_api.js
+++ src/lib/external_api.js
@@ -1,18 +1,28 @@
 const dcrdataURL = (network, address) => `https://${network}.dcrdata.org/api/address/${address}/raw`;
 const FAUCET_URL = "https://faucet.decred.org/requestfaucet";
 
-const POST = (path, json, method = "POST") => fetch(FAUCET_URL, {
-  mode: "cors",
-  headers: {
-    "Accept": "application/json",
-  },
-  method,
-  body: JSON.stringify(json)
-});
+const POST = (path, params, method = "POST") => {
+  var formBody = [];
+  for (var key in params) {
+    var encodedKey = encodeURIComponent(key);
+    var encodedValue = encodeURIComponent(params[key]);
+    formBody.push(encodedKey + "=" + encodedValue);
+  }
+  formBody = formBody.join("&");
+
+  return fetch(path, {
+    mode: "cors",
+    headers: {
+      "Content-Type": "application/x-www-form-urlencoded; charset=UTF-8",
+    },
+    method,
+    body: formBody
+  });
+};
 
 const getRawAddresses = (network, address) => fetch(dcrdataURL(network, address)).then(r => {
   // work around when transactions are not paid and dcrdata api returns Unprocessable Entity
-  if(r.statusText === "Unprocessable Entity") {
+  if (r.statusText === "Unprocessable Entity") {
     return null;
   }
   return r.json();
