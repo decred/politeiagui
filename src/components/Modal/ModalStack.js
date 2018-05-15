49b7f15ac58083242fd6c524e043861c789ed1b6
Tiago Alves Dulce <tiagoalvesdulce@outlook.com>
2018-05-15 08:35:48 -0300

    fix two scrolls

--- src/components/Modal/ModalStack.js
+++ src/components/Modal/ModalStack.js
@@ -27,8 +27,8 @@ class ModalStack extends React.Component {
     }
     if(modalChanged) this.setState({ modals }, () => {
       if (modals.length !== 0)
-        document.querySelector("html").style.overflow = "hidden";
-      else document.querySelector("html").style.overflow = "scroll";
+        document.querySelector("html").style.overflowY = "hidden";
+      else document.querySelector("html").style.overflowY = "scroll";
     });
   }
   renderModalContent = (modalData) => (
