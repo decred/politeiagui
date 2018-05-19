b8a1396ea7da6e8a06e95ecfdf823c4291e2584a
Fernando Abolafio <fernandohsabolafio@gmail.com>
2018-05-19 11:47:27 -0300

    Test lib local_storage.js

--- src/lib/tests/local_storage.test.js
+++ src/lib/tests/local_storage.test.js
@@ -1,3 +1,30 @@
+import * as ls from "../local_storage";
 describe("save state chunks to local storage (lib/local_storage.js", () => {
+  const mockState = {
+    api: {
+      me: {
+        response: {
+          email: "foo@bar.com",
+          username: "foobar"
+        }
+      },
+      changeUsername: {
+        response: {
+          username: "foobar"
+        }
+      }
+    }
+  };
+  const getFromLS = (key) => localStorage.getItem(key);
+  test("save state to local storage", () => {
+    ls.handleSaveStateToLocalStorage(mockState);
+    expect(getFromLS("state")).toBeTruthy();
+  });
 
+  test("clear state from local storage", () => {
+    ls.handleSaveStateToLocalStorage(mockState);
+    expect(getFromLS("state")).toBeTruthy();
+    ls.clearStateLocalStorage();
+    expect(getFromLS("state")).toBeFalsy();
+  });
 });
