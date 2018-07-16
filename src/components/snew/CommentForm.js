7b9c83d1b859c213b1fadf5db19bd32db77f5ae5
Fernando Abolafio <fernandohsabolafio@gmail.com>
2018-07-16 15:47:14 -0300

    Encapsulate markdown help into a component and remove superscript row

--- src/components/snew/CommentForm.js
+++ src/components/snew/CommentForm.js
@@ -4,6 +4,7 @@ import Message from "../Message";
 import connector from "../../connectors/reply";
 import MarkdownEditorField from "../Form/Fields/MarkdownEditorField";
 import { PROPOSAL_VOTING_NOT_STARTED } from "../../constants";
+import MarkdownHelp from "../MarkdownHelp";
 
 const CommentForm = ({
   Link = LinkComponent,
@@ -95,98 +96,7 @@ const CommentForm = ({
           </div>
         )}
         {isShowingMarkdownHelp && (
-          <div className="markhelp">
-            <p>
-            we use a slightly-customized version of{" "}
-              <a href="http://daringfireball.net/projects/markdown/syntax">
-              Markdown
-              </a>{" "}
-            for formatting. See below for some basics.
-            </p>
-            <table className="md">
-              <tbody>
-                <tr
-                  style={{
-                    backgroundColor: "#ffff99",
-                    textAlign: "center"
-                  }}
-                >
-                  <td>
-                    <em>you type:</em>
-                  </td>
-                  <td>
-                    <em>you see:</em>
-                  </td>
-                </tr>
-                <tr>
-                  <td>*italics*</td>
-                  <td>
-                    <em>italics</em>
-                  </td>
-                </tr>
-                <tr>
-                  <td>**bold**</td>
-                  <td>
-                    <b>bold</b>
-                  </td>
-                </tr>
-                <tr>
-                  <td>[decred!](https://decred.org)</td>
-                  <td>
-                    <a href="https://decred.org">decred!</a>
-                  </td>
-                </tr>
-                <tr>
-                  <td>
-                  * item 1<br />
-                  * item 2<br />
-                  * item 3
-                  </td>
-                  <td>
-                    <ul>
-                      <li>item 1</li>
-                      <li>item 2</li>
-                      <li>item 3</li>
-                    </ul>
-                  </td>
-                </tr>
-                <tr>
-                  <td>> quoted text</td>
-                  <td>
-                    <blockquote>quoted text</blockquote>
-                  </td>
-                </tr>
-                <tr>
-                  <td>
-                  Lines starting with four spaces<br />
-                  are treated like code:<br />
-                    <br />
-                    <span className="spaces">    </span>if 1 * 2 != 3:<br />
-                    <span className="spaces">        </span>print "hello, world!"<br />
-                  </td>
-                  <td>
-                  Lines starting with four spaces<br />
-                  are treated like code:<br />
-                    <pre>
-                    if 1 * 2 != 3:<br />    print "hello, world!"{"\n"}
-                    </pre>
-                  </td>
-                </tr>
-                <tr>
-                  <td>~~strikethrough~~</td>
-                  <td>
-                    <strike>strikethrough</strike>
-                  </td>
-                </tr>
-                <tr>
-                  <td>super^script</td>
-                  <td>
-                  super<sup>script</sup>
-                  </td>
-                </tr>
-              </tbody>
-            </table>
-          </div>
+          <MarkdownHelp />
         )}
       </div>
     </form> : null
--- src/components/snew/SubmitPage.js
+++ src/components/snew/SubmitPage.js
@@ -9,6 +9,7 @@ import MultipleItemsBodyMessage from "../MultipleItemsBodyMessage";
 import ProposalInfo from "../ProposalInfo";
 import { isArray, isUndefined, concat, cloneDeep } from "lodash";
 import { Field } from "redux-form";
+import MarkdownHelp from "../MarkdownHelp";
 
 const normalizer = (value, previousValue) => {
   let files = [];
@@ -117,99 +118,7 @@ class SubmitPage extends React.Component {
               </div>
               <div className="spacer">
                 <div className="roundfield">
-                  <div className="markhelp" >
-                    <p>
-                      We use a slightly-customized version of{" "}
-                      <a href="http://daringfireball.net/projects/markdown/syntax">
-                        Markdown
-                      </a>{" "}
-                      for formatting. See below for some basics
-                    </p>
-                    <table className="md">
-                      <tbody>
-                        <tr
-                          style={{
-                            backgroundColor: "#ffff99",
-                            textAlign: "center"
-                          }}
-                        >
-                          <td>
-                            <em>you type:</em>
-                          </td>
-                          <td>
-                            <em>you see:</em>
-                          </td>
-                        </tr>
-                        <tr>
-                          <td>*italics*</td>
-                          <td>
-                            <em>italics</em>
-                          </td>
-                        </tr>
-                        <tr>
-                          <td>**bold**</td>
-                          <td>
-                            <b>bold</b>
-                          </td>
-                        </tr>
-                        <tr>
-                          <td>[decred!](https://decred.org)</td>
-                          <td>
-                            <a href="https://decred.org">decred!</a>
-                          </td>
-                        </tr>
-                        <tr>
-                          <td>
-                            * item 1<br />
-                            * item 2<br />
-                            * item 3
-                          </td>
-                          <td>
-                            <ul>
-                              <li>item 1</li>
-                              <li>item 2</li>
-                              <li>item 3</li>
-                            </ul>
-                          </td>
-                        </tr>
-                        <tr>
-                          <td>> quoted text</td>
-                          <td>
-                            <blockquote>quoted text</blockquote>
-                          </td>
-                        </tr>
-                        <tr>
-                          <td>
-                            Lines starting with four spaces<br />
-                            are treated like code:<br />
-                            <br />
-                            <span className="spaces">    </span>if 1 * 2 != 3:<br />
-                            <span className="spaces">        </span>print
-                            "hello, world!"<br />
-                          </td>
-                          <td>
-                            Lines starting with four spaces<br />
-                            are treated like code:<br />
-                            <pre>
-                              if 1 * 2 != 3:<br />    print "hello, world!"{"\n"}
-                            </pre>
-                          </td>
-                        </tr>
-                        <tr>
-                          <td>~~strikethrough~~</td>
-                          <td>
-                            <strike>strikethrough</strike>
-                          </td>
-                        </tr>
-                        <tr>
-                          <td>super^script</td>
-                          <td>
-                            super<sup>script</sup>
-                          </td>
-                        </tr>
-                      </tbody>
-                    </table>
-                  </div>
+                  <MarkdownHelp />
                 </div>
               </div>
             </div>
