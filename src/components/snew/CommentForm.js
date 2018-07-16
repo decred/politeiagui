1167573b1eb543b2aaaac29c34f1c3df8413b83f
Fernando Abolafio <fernandohsabolafio@gmail.com>
2018-07-16 11:42:16 -0300

    Optimize comments typing speed (#459)
    * Make comments component (ThingComment) state full in order to isolate the comment’s state
    
    * Remove log
    
    * link snew-classic-ui
    
    * Upgrade yarn.lock
    
    * Remove commented code

--- src/components/snew/CommentForm.js
+++ src/components/snew/CommentForm.js
@@ -1,37 +1,29 @@
 import React from "react";
 import LinkComponent from "./Link";
-import { Field } from "redux-form";
 import Message from "../Message";
-import ErrorField from "../Form/Fields/ErrorField";
 import connector from "../../connectors/reply";
 import MarkdownEditorField from "../Form/Fields/MarkdownEditorField";
 import { PROPOSAL_VOTING_NOT_STARTED } from "../../constants";
 
 const CommentForm = ({
   Link = LinkComponent,
-  body,
   isPostingComment,
   isShowingMarkdownHelp,
   error,
   thingId,
-  defaultBody,
-  onChangeBody,
   onSave,
-  handleSubmit,
   onToggleMarkdownHelp,
-  onSetReplyParent,
   loggedInAsEmail,
   userCanExecuteActions,
   getVoteStatus,
   token,
-  showContentPolicy=false
+  showContentPolicy=false,
+  value,
+  onChange,
+  onClose
 }) => (
   loggedInAsEmail ?
-    <form className="usertext cloneable warn-on-unload"  onSubmit={handleSubmit(onSave)}>
-      <Field
-        name="global"
-        component={props => <ErrorField title="Cannot create comment" {...props} />}
-      />
+    <form className="usertext cloneable warn-on-unload"  onSubmit={onSave}>
       {error ? (
         <Message
           type="error"
@@ -45,19 +37,12 @@ const CommentForm = ({
           getVoteStatus(token) &&
           getVoteStatus(token).status === PROPOSAL_VOTING_NOT_STARTED && (
           <div className="md">
-            <Field
-              component={MarkdownEditorField}
+            <MarkdownEditorField
+              input={{
+                value: value,
+                onChange: onChange
+              }}
               toggledStyle
-              name="comment"
-              autoFocus={!!thingId}
-              type="text"
-              cols={1}
-              data-event-action="comment"
-              data-type="link"
-              rows={1}
-              defaultValue={defaultBody}
-              body={body}
-              onChange={onChangeBody && ((e) => onChangeBody(e.target.value))}
             />
           </div>
         )}
@@ -98,9 +83,9 @@ const CommentForm = ({
               >
               save
               </button>
-              {(thingId && (<button
+              {(onClose && (<button
                 className={`togglebutton access-required${!userCanExecuteActions ? " not-active disabled" : ""}`}
-                onClick={() => onSetReplyParent()}
+                onClick={() => onClose()}
                 type="button"
                 disabled={!userCanExecuteActions}
               >
--- src/components/snew/NestedListing.js
+++ src/components/snew/NestedListing.js
@@ -6,8 +6,7 @@ const NestedListing = ({ replyTo, ...props }) => (
   <BaseListing {...{
     ...props,
     replyTo,
-    name: props.name || 0,
-    showReplyForm: (replyTo === props.name) || (!props.name && !replyTo && !props.locked)
+    name: props.name || 0
   }} />
 );
 
