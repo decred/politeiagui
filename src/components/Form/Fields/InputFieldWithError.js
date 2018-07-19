11eeec76fe71b919e8c2b7e448f68ba77966708f
Ramon Recuero <ramon@oly.ai>
2018-07-19 09:36:49 -0700

    Passes placeholder to input wrapper

--- src/components/Form/Fields/InputFieldWithError.js
+++ src/components/Form/Fields/InputFieldWithError.js
@@ -3,12 +3,14 @@ import React from "react";
 const InputFieldWithError = ({
   input,
   label,
+  placeholder,
+  tabIndex,
   type,
   meta: { touched, error, warning }
 }) => (
   <div className="input-with-error">
     <label>{label}</label>
-    <input {...input} placeholder={label} type={type} />
+    <input {...input} tabIndex={tabIndex} placeholder={placeholder} type={type} />
     <div className="input-subline">
       {touched &&
         ((error && <div className="input-error">{error}</div>) ||
