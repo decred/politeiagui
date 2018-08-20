440d02c5601b7d49bf20bf3e9011e9e20717f8f5
Tiago Alves Dulce <tiagoalvesdulce@outlook.com>
2018-08-20 19:59:19 -0300

    Add external link warning (#518)
    * Add external link warning
    
    * Add warning to image links
    
    * Improve message body

--- src/components/snew/Markdown/helpers.js
+++ src/components/snew/Markdown/helpers.js
@@ -1,5 +1,6 @@
 import React from "react";
 import xssFilters from "xss-filters";
+import * as modalTypes from "../../Modal/modalTypes";
 
 export const traverseChildren = (el, cb) => {
   const filterChildren = (c) => React.Children.map(c,
@@ -39,12 +40,44 @@ export const handleFilterXss = (el) => {
   return React.cloneElement(el, newProps);
 };
 
-export const customRenderers = (filterXss) => ({
+const verifyExternalLink = (e, link, confirmWithModal) => {
+  e.preventDefault();
+  const tmpLink = document.createElement("a");
+  tmpLink.href = link;
+  const externalLink = (tmpLink.hostname !== window.top.location.hostname);
+  // if this is an external link, show confirmation dialog
+  if (externalLink) {
+    confirmWithModal(modalTypes.CONFIRM_ACTION, {
+      title: "External Link Warning",
+      message: (
+        <React.Fragment>
+          <p style={{ marginBottom: "10px" }}>
+            This link will take you to an external website!
+          </p>
+          <p>
+            <strong>External link: {link}</strong>
+          </p>
+          <p style={{ marginTop: "10px" }}>
+            Are you sure you want to proceed?
+          </p>
+        </React.Fragment>
+      )
+    }).then(confirm => {
+      if (confirm) {
+        window.location.href = link;
+      }
+    });
+  } else {
+    window.location.href = link;
+  }
+};
+
+export const customRenderers = (filterXss, confirmWithModal) => ({
   image: ({ src, alt }) => {
-    return <a rel="nofollow" href={src}>{alt}</a>;
+    return <a rel="nofollow" onClick={(e) => verifyExternalLink(e, src, confirmWithModal)} href={src}>{alt}</a>;
   },
   link: ({ href, children }) => {
-    return <a rel="nofollow" href={href}>{children[0]}</a>;
+    return <a rel="nofollow" onClick={(e) => verifyExternalLink(e, href, confirmWithModal)} href={href}>{children[0]}</a>;
   },
   root: (el) => {
     if(filterXss) {
@@ -54,6 +87,6 @@ export const customRenderers = (filterXss) => ({
       children,
       ...props
     } = el;
-    return <div {...props} >{children}</div>;
+    return <div {...props}>{children}</div>;
   }
 });
