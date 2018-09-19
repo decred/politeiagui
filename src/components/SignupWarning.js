b564fbfbc6d0cf8aa569044037f10af7ae85da17
Seth <seth.benton@gmail.com>
2018-09-19 10:55:08 -0700

    Updates signup warning text

--- src/components/SignupWarning.js
+++ src/components/SignupWarning.js
@@ -8,16 +8,14 @@ const SignupWarning = () => {
         proposals and leaving comments.
       </p>
       <p>
-        Politeia will send you a link to verify your email address, and{" "}
-        <span style={{ fontWeight: "bold" }}>you must open this link in this
-        same browser</span>, because your new identity will be used
-        to help verify it.
-      </p>
-      <p>
-        After you verify your email, you will be able to log in and copy your
-        identity to other browsers, but for this signup process, you must use
-        the same browser and ensure that you aren't using a private/incognito
-        window and that you don't clear your cookies.
+        Politeia will send you a link to verify your email address.
+        <span style={{ fontWeight: "bold" }}> You must open this link in the same browser.
+        </span> After verifying your email, Politeia will create your Politeia “identity”,
+        which consists of a public/private cryptographic key pair and browser cookie.
+        This is necessary to verify your identity and allow submission of proposals,
+        commenting, voting, and other Politeia functions. After completing the signup
+        process, you can export your identity (public/private keys) to another browser
+        at any time.
       </p>
     </div>
   );
