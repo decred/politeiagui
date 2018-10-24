17df04d057deaa3d603ba8cfc919fde126a7403b
Fernando Abolafio <fernandohsabolafio@gmail.com>
2018-10-24 15:22:02 -0300

    Update email validator

--- src/validators/util.js
+++ src/validators/util.js
@@ -1,7 +1,7 @@
 import { SubmissionError } from "redux-form";
 
 export function emailValidator(email) {
-  return /^[A-Z0-9._%+-]+@[A-Z0-9.-]+\.[A-Z]{2,4}$/i.test(email);
+  return /^[A-Z0-9._%+-]+@[A-Z0-9.-]+\.[A-Z]{2,24}$/i.test(email);
 }
 
 export function arrayToRegex(arr) {
