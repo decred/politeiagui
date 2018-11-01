0847056c310dd2a992ba7eadbbd45c6eacf833e5
Fernando Abolafio <fernandohsabolafio@gmail.com>
2018-11-01 16:12:36 -0300

    Enable closing modal by pressing “Esc”

--- src/components/Modal/ModalContentWrapper.js
+++ src/components/Modal/ModalContentWrapper.js
@@ -1,4 +1,5 @@
 import React from "react";
+import PropTypes from "prop-types";
 
 const CancelButton = ({ onClick, text }) => (
   <button
@@ -19,32 +20,63 @@ const SubmitButton = ({ onClick, text, disabled }) => (
   </button>
 );
 
-const ModalContentWrapper = ({
-  onClose,
-  onCancel,
-  onSubmit,
-  submitDisabled = false,
-  title,
-  submitText = "OK",
-  cancelText = "Cancel",
-  children,
-  style
-}) => {
-  return (
-    <div className="modal-content" style={{ minWidth: "700px", ...style }}>
-      <div className="modal-content-header">
-        <h2 style={{ fontSize: "18px", textTransform: "uppercase" }} >{title}</h2>
-        <div style={{ display: "flex", justifyContent: "flex-end", flex: 1 }}>
-          {onClose && <span style={{ fontSize: "18px", cursor: "pointer" }} onClick={onClose}>✖</span>}
+class ModalContentWrapper extends React.Component {
+  handleKeyUp = (e) => {
+    if (e.keyCode === 27 && !this.props.disableCloseOnEsc) {
+      if(this.props.onClose) {
+        this.props.onClose();
+      } else if(this.props.onCancel) {
+        this.props.onCancel();
+      }
+    }
+  }
+  componentWillMount(){
+    window.addEventListener("keyup", this.handleKeyUp);
+  }
+  componentWillUnmount() {
+    window.removeEventListener("keyup", this.handleKeyUp);
+  }
+  render() {
+    const {
+      onClose,
+      onCancel,
+      onSubmit,
+      submitDisabled = false,
+      title,
+      submitText = "OK",
+      cancelText = "Cancel",
+      children,
+      style
+    } = this.props;
+
+    return (
+      <div className="modal-content" style={{ minWidth: "700px", ...style }}>
+        <div className="modal-content-header">
+          <h2 style={{ fontSize: "18px", textTransform: "uppercase" }} >{title}</h2>
+          <div style={{ display: "flex", justifyContent: "flex-end", flex: 1 }}>
+            {onClose && <span style={{ fontSize: "18px", cursor: "pointer" }} onClick={onClose}>✖</span>}
+          </div>
         </div>
+        {children}
+        {(onCancel || onSubmit) && <div className="modal-content-actions">
+          {onCancel && <CancelButton onClick={onCancel} text={cancelText} />}
+          {onSubmit && <SubmitButton onClick={onSubmit} text={submitText} disabled={submitDisabled} />}
+        </div>}
       </div>
-      {children}
-      {(onCancel || onSubmit) && <div className="modal-content-actions">
-        {onCancel && <CancelButton onClick={onCancel} text={cancelText} />}
-        {onSubmit && <SubmitButton onClick={onSubmit} text={submitText} disabled={submitDisabled} />}
-      </div>}
-    </div>
-  );
+    );
+  }
+}
+
+ModalContentWrapper.propTypes = {
+  onClose: PropTypes.func,
+  onCancel: PropTypes.func,
+  onSubmit: PropTypes.func,
+  submitDisabled: PropTypes.bool,
+  title: PropTypes.oneOfType([ PropTypes.string, PropTypes.element ]),
+  submitText: PropTypes.string,
+  cancelText: PropTypes.string,
+  style: PropTypes.object,
+  disableCloseOnEsc: PropTypes.bool
 };
 
 export default ModalContentWrapper;
