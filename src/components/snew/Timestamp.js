2a693c2a0ede1c02011bfc2ef098c1a64162bd59
Tiago Alves Dulce <tiagoalvesdulce@outlook.com>
2018-11-07 10:45:43 -0300

    Use DateTooltip from snew-classic-ui (#863)
    * Use DateTooltip from snew-classic-ui
    
    * upgrade dependencies

--- src/components/snew/Timestamp.js
+++
@@ -1,6 +0,0 @@
-import React from "react";
-import TimeAgo from "timeago-react";
-
-const Timestamp = ({ created_utc }) => (created_utc && <TimeAgo datetime={created_utc * 1000} />) || null;
-export default Timestamp;
-
