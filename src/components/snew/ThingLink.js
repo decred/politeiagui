b5a9cb18a7c68f35f9cff788043d41f24762e19e
Kevin <42082827+camus-code@users.noreply.github.com>
2019-01-07 16:29:25 -0600

    Add  diff view to abandoned proposals (#965)

--- src/components/snew/ThingLink.js
+++ src/components/snew/ThingLink.js
@@ -216,7 +216,7 @@ class ThingLinkComp extends React.Component {
                 <span className="font-12 warning-color">edited</span>
               ) : null}
             </Link>{" "}
-            {expanded && displayVersion && version > 1 ? (
+            {expanded && (displayVersion || isAbandoned) && version > 1 ? (
               <VersionPicker
                 onSelectVersion={selVersion => {
                   openModal(modalTypes.PROPOSAL_VERSION_DIFF, {
