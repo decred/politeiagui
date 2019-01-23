436746e0828180580bfaab3513bff526e4845866
camus-code <bitboard@protonmail.com>
2019-01-23 11:08:25 -0600

    Move event listeners from modalContentWrapper to Modal
    Fix button behavior on purchase credit modal
    
    Pass in event listener prop-types to modal
    
    make event listeners controllable
    
    extend testing coverage for modal options

--- src/components/Modal/Modal.js
+++ src/components/Modal/Modal.js
@@ -1,14 +1,49 @@
 import React from "react";
+import PropTypes from "prop-types";
+import OnClickListener from "../OnClickListener";
+import OnKeyListener from "../OnKeyListener";
 
-const Modal = props => {
-  return (
-    <div className="modal">
-      <div className="modal-overlay" />
-      <div className="modal-container" style={props.style}>
-        {props.children}
+const ESC_KEY = 27;
+
+class Modal extends React.Component {
+  render() {
+    const {
+      onClose,
+      onCancel,
+      disableCloseOnEsc,
+      disableCloseOnClick,
+      children,
+      style
+    } = this.props;
+    return (
+      <div>
+        <OnKeyListener
+          keyCode={ESC_KEY}
+          onKeyPress={onClose || onCancel}
+          disabled={disableCloseOnEsc}
+        />
+        <OnClickListener
+          onClick={onClose || onCancel}
+          disabled={disableCloseOnClick}
+        />
+        <div className="modal">
+          <div className="modal-overlay" />
+          <div className="modal-container" style={style}>
+            {children}
+          </div>
+        </div>
       </div>
-    </div>
-  );
+    );
+  }
+}
+
+Modal.propTypes = {
+  onClose: PropTypes.func,
+  onCancel: PropTypes.func,
+  disableCloseOnClick: PropTypes.bool,
+  disableCloseOnEsc: PropTypes.bool,
+  children: PropTypes.object,
+  style: PropTypes.object
 };
 
 export default Modal;
