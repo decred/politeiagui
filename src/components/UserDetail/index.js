ea06f162de09f6c7d4811227641e2d586e78d549
camus-code <bitboard@protonmail.com>
2019-04-16 19:56:44 -0500

    add cms check to functions

--- src/components/UserDetail/index.js
+++ src/components/UserDetail/index.js
@@ -72,8 +72,8 @@ class UserDetail extends Component {
 
   componentDidMount() {
     this.props.onFetchData(this.props.userId);
-    this.props.onFetchUserProposals(this.props.userId);
-    this.props.onFetchProposalsVoteStatus();
+    !this.props.isCMS && this.props.onFetchUserProposals(this.props.userId);
+    !this.props.isCMS && this.props.onFetchProposalsVoteStatus();
   }
 
   onTabChange(tabId) {
