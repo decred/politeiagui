2a014a379f19463e79bf2e62b01a78353363fd82
camus-code <bitboard@protonmail.com>
2019-04-30 15:32:11 -0500

    update copy for cms

--- src/components/snew/PrivacyPolicy.js
+++ src/components/snew/PrivacyPolicy.js
@@ -61,20 +61,15 @@ class PrivacyPolicy extends React.Component {
                 <h3>Privacy Policy</h3>
                 <ul>
                   <li>
-                    The <Link href="/">proposals.decred.org</Link> database
-                    stores your account email address, username, cryptographic
-                    identity public key(s), IP addresses and payment transaction
-                    details - and associates this data with all of your
-                    proposals, comments and up/down votes.
+                    The <Link href="/">cms.decred.org</Link> database stores
+                    your account email address, username, cryptographic identity
+                    public key(s), IP addresses and invoice transaction details
+                    - and associates this data with all of your invoices,
+                    comments and up/down votes.
                   </li>
                   <li>
                     Your email address will be kept private and will not be
                     shared with any third parties.
-                  </li>
-                  <li>
-                    In the interests of transparency, data associating your
-                    public key with content contributions will be published
-                    openly in the Decred-proposals repository here:
                   </li>
                 </ul>
               </React.Fragment>
