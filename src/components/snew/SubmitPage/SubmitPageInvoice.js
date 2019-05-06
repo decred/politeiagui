3a0f4491a22b453d06e24bf14a4c6809d58366de
Alex Yocom-Piatt <ay-p@decred.org>
2019-05-06 06:49:09 -0400

    [cms] Add submit invoice instructions to page (#1156)
    * [cms] Add submit invoice instructions to page
    
    * Update styling to avoid overlap

--- src/components/snew/SubmitPage/SubmitPageInvoice.js
+++ src/components/snew/SubmitPage/SubmitPageInvoice.js
@@ -5,6 +5,7 @@ import SelectField from "../../Form/Fields/SelectField";
 import { FilesField, normalizer } from "../../Form/Fields/FilesField";
 import InputFieldWithError from "../../Form/Fields/InputFieldWithError";
 import ButtonWithLoadingIcon from "../ButtonWithLoadingIcon";
+import Markdown from "../Markdown";
 import Message from "../../Message";
 import { Field } from "redux-form";
 import InvoiceDatasheetField from "../../Form/Fields/InvoiceDatasheetField";
@@ -17,6 +18,25 @@ import {
 
 const YEAR_OPTIONS = [2018, 2019];
 const MONTH_OPTIONS = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12];
+
+const invoiceInstructions = `
+***Contractor Name:*** This is whatever name you identify yourself with the DHG, typically something beyond a mere handle or nick.
+
+***Contractor Location:*** This is the country you are currently located, or primarily residing.
+
+***Contractor Contact:*** Contact information in case an administrator would need to reach out to discuss something, typically an email address or chat nick.
+
+***Contractor Rate:*** This is the previously agreed upon rate you will be performing work.
+
+***Payment Address:*** This is the DCR address where you would like to receive payment.  
+
+***Line Items:***
+  * Type: Currently can be 1 (Labor), 2 (Expense), or 3 (Misc)
+  * Domain: The broad category of work performed/expenses spent (for example, Development, Marketing, Community etc).
+  * Subdomain: The specific project or program of which the work or expenses are related (for example, Decrediton, dcrd, NYC Event).
+  * Description: A thorough description of the work or expenses.
+  * Labor: The number of hours of work performed.
+  * Expenses: The cost of the line item (in USD).`;
 
 const InvoiceSubmit = props => {
   const {
@@ -137,37 +157,58 @@ const InvoiceSubmit = props => {
                       </span>
                     </DynamicDataDisplay>
                   </div>
-                  <div className="usertext">
-                    <Field
-                      name="name"
-                      label="Contractor name"
-                      type="text"
-                      component={InputFieldWithError}
-                    />
-                    <Field
-                      name="location"
-                      label="Contractor location"
-                      type="text"
-                      component={InputFieldWithError}
-                    />
-                    <Field
-                      name="contact"
-                      label="Contractor contact"
-                      type="text"
-                      component={InputFieldWithError}
-                    />
-                    <Field
-                      name="rate"
-                      label="Contractor rate (USD)"
-                      type="number"
-                      component={InputFieldWithError}
-                    />
-                    <Field
-                      name="address"
-                      label="Payment address"
-                      type="text"
-                      component={InputFieldWithError}
-                    />
+                  <div
+                    className="usertext"
+                    style={{
+                      display: "flex",
+                      justifyContent: "space-between"
+                    }}
+                  >
+                    <div
+                      style={{
+                        paddingRight: "20px"
+                      }}
+                    >
+                      <Field
+                        name="name"
+                        label="Contractor name"
+                        type="text"
+                        component={InputFieldWithError}
+                      />
+                      <Field
+                        name="location"
+                        label="Contractor location"
+                        type="text"
+                        component={InputFieldWithError}
+                      />
+                      <Field
+                        name="contact"
+                        label="Contractor contact"
+                        type="text"
+                        component={InputFieldWithError}
+                      />
+                      <Field
+                        name="rate"
+                        label="Contractor rate (USD)"
+                        type="number"
+                        component={InputFieldWithError}
+                      />
+                      <Field
+                        name="address"
+                        label="Payment address"
+                        type="text"
+                        component={InputFieldWithError}
+                      />
+                    </div>
+                    <div>
+                      <Markdown
+                        body={invoiceInstructions}
+                        filterXss={false}
+                        confirmWithModal={null}
+                        displayExternalLikWarning={false}
+                        {...props}
+                      />
+                    </div>
                   </div>
                   <div className="usertext">
                     <Field
