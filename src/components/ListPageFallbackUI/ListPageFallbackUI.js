a69a7a3f40b5d49bd0096ba657ae79d8d86cac17
Kevin <42082827+camus-code@users.noreply.github.com>
2019-05-10 04:29:18 -0500

    Improve fallback UI for proposals (#1181)

--- src/components/ListPageFallbackUI/ListPageFallbackUI.js
+++ src/components/ListPageFallbackUI/ListPageFallbackUI.js
@@ -1,8 +1,21 @@
 import React from "react";
+import { Tabs, Tab } from "../Tabs";
+import LoadingIcon from "../snew/LoadingIcon";
 
 const ListPageFallbackUI = () => (
-  <div className="fallback-list-page-content">
-    <h1 className="fallback-list-page-content-title">Loading...</h1>
+  <div className="content">
+    <h1 className="content-title">Public Proposals</h1>
+    <Tabs>
+      <Tab title="In Discusssion" count={""} />
+      <Tab title="Voting" count={""} />
+      <Tab title="Approved" count={""} />
+      <Tab title="Rejected" count={""} />
+      <Tab title="Abandoned" count={""} />
+    </Tabs>
+    <LoadingIcon
+      width={200}
+      style={{ paddingTop: "100px", margin: "0 auto" }}
+    />
   </div>
 );
 
