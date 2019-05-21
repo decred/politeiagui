788cb2d1f04586602e0ba95d304e0d889392e720
Alex Yocom-Piatt <ay-p@decred.org>
2019-05-21 17:07:11 -0500

    [cms] Add new fields for payouts

--- src/components/GeneratePayouts/PayoutsTable.js
+++ src/components/GeneratePayouts/PayoutsTable.js
@@ -1,6 +1,9 @@
 import React from "react";
 import PropTypes from "prop-types";
 
+const usdConvert = 100;
+const dcrConvert = 100000000;
+
 const PayoutRow = ({
   year,
   month,
@@ -8,17 +11,21 @@ const PayoutRow = ({
   contractorrate,
   labortotal,
   expensetotal,
-  address
+  address,
+  exchangerate,
+  dcrtotal
 }) => {
   return (
     <tr>
       <td>{year}</td>
       <td>{month}</td>
       <td>{contractorname}</td>
-      <td>{contractorrate}</td>
-      <td>{labortotal}</td>
-      <td>{expensetotal}</td>
-      <td>{expensetotal + labortotal}</td>
+      <td>{contractorrate / usdConvert}</td>
+      <td>{labortotal / usdConvert}</td>
+      <td>{expensetotal / usdConvert}</td>
+      <td>{(expensetotal + labortotal) / usdConvert}</td>
+      <td>{exchangerate / usdConvert}</td>
+      <td>{dcrtotal / dcrConvert}</td>
       <td>{address}</td>
     </tr>
   );
@@ -36,6 +43,8 @@ const PayoutsTable = ({ payouts }) => {
           <th>Labor total (USD)</th>
           <th>Expense total (USD)</th>
           <th>Combined total (USD)</th>
+          <th>Exchange Rate (USD)</th>
+          <th>Total Payment (DCR)</th>
           <th>Address</th>
         </tr>
         {payouts.map((payout, idx) => (
