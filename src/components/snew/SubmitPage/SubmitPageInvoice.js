1aac480892e686f4016d9d2c144dfa4872eabd36
Fernando Abolafio <fernandohsabolafio@gmail.com>
2019-05-22 19:03:00 +0200

    add policy info into the help text

--- src/components/snew/SubmitPage/SubmitPageInvoice.js
+++ src/components/snew/SubmitPage/SubmitPageInvoice.js
@@ -19,12 +19,36 @@ import {
 const YEAR_OPTIONS = [2018, 2019];
 const MONTH_OPTIONS = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12];
 
-const invoiceInstructions = `
-***Contractor Name:*** This is whatever name you identify yourself with the DHG, typically something beyond a mere handle or nick.
+const concatSupportedChars = supportedChars =>
+  supportedChars.reduce((str, v) => str + v, "");
+
+const invoiceInstructions = ({
+  cmscontactsupportedchars,
+  cmsnamelocationsupportedchars,
+  maxlocationlength,
+  minlocationlength,
+  mincontactlength,
+  maxcontactlength,
+  minnamelength,
+  maxnamelength,
+  minlineitemcollength,
+  maxlineitemcollength,
+  invoicefieldsupportedchars
+}) => `
+***Contractor Name:*** This is whatever name you identify yourself with the DHG, typically something beyond a mere handle or nick. 
+ - Allowed chars: ${concatSupportedChars(cmsnamelocationsupportedchars)}
+ - Min length: ${minnamelength}
+ - Max length: ${maxnamelength}
 
 ***Contractor Location:*** This is the country you are currently located, or primarily residing.
+- Allowed chars: ${concatSupportedChars(cmsnamelocationsupportedchars)}
+- Min length: ${minlocationlength}
+- Max length: ${maxlocationlength}
 
 ***Contractor Contact:*** Contact information in case an administrator would need to reach out to discuss something, typically an email address or chat nick.
+- Allowed chars: ${concatSupportedChars(cmscontactsupportedchars)}
+- Min length: ${mincontactlength}
+- Max length: ${maxcontactlength}
 
 ***Contractor Rate:*** This is the previously agreed upon rate you will be performing work.
 
@@ -36,7 +60,13 @@ const invoiceInstructions = `
   * Subdomain: The specific project or program of which the work or expenses are related (for example, Decrediton, dcrd, NYC Event).
   * Description: A thorough description of the work or expenses.
   * Labor: The number of hours of work performed.
-  * Expenses: The cost of the line item (in USD).`;
+  * Expenses: The cost of the line item (in USD).
+  
+Line items policy for domain, subdomain and description:
+- Allowed chars: ${concatSupportedChars(invoicefieldsupportedchars)}
+- Min length: ${minlineitemcollength}
+- Max length: ${maxlineitemcollength}
+`;
 
 const InvoiceSubmit = props => {
   const {
@@ -204,13 +234,15 @@ const InvoiceSubmit = props => {
                       />
                     </div>
                     <div>
-                      <Markdown
-                        body={invoiceInstructions}
-                        filterXss={false}
-                        confirmWithModal={null}
-                        displayExternalLikWarning={false}
-                        {...props}
-                      />
+                      {policy && (
+                        <Markdown
+                          body={invoiceInstructions(policy)}
+                          filterXss={false}
+                          confirmWithModal={null}
+                          displayExternalLikWarning={false}
+                          {...props}
+                        />
+                      )}
                     </div>
                   </div>
                   <div className="usertext">
