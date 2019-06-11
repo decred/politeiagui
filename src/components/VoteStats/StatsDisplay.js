4d4394fae9f539cdb61416c86ab380d354b8606b
Fernando Abolafio <fernandohsabolafio@gmail.com>
2019-06-11 14:52:37 +0200

    Improve quorum tooltip (#1262)
    * Tweak tooltip message
    
    * better padding for tooltip

--- src/components/VoteStats/StatsDisplay.js
+++ src/components/VoteStats/StatsDisplay.js
@@ -203,10 +203,7 @@ class Stats extends React.Component {
     const isPreVoting =
       status === PROPOSAL_VOTING_NOT_AUTHORIZED ||
       status === PROPOSAL_VOTING_AUTHORIZED;
-    const currentQuorumPercentage = getPercentage(
-      totalVotes,
-      numOfEligibleVotes
-    );
+
     const quorumInVotes = Math.trunc(
       (numOfEligibleVotes * quorumPercentage) / 100
     );
@@ -248,12 +245,13 @@ class Stats extends React.Component {
               Quorum:
               <Tooltip
                 tipStyle={{
-                  left: "80px",
-                  maxWidth: "100px",
-                  padding: "6px",
-                  textAlign: "center"
+                  left: "105px",
+                  top: "-2px",
+                  padding: "3px",
+                  textAlign: "center",
+                  minWidth: "300px"
                 }}
-                text={`${currentQuorumPercentage}/${quorumPercentage} %`}
+                text={`Quorum minimum is ${quorumPercentage}% of ${numOfEligibleVotes} elegible votes`}
                 position="right"
               >
                 <span
