4164ac3ca356b7cd90dc0187135f497f19e5bca9
Victor Guedes <victorgcramos@gmail.com>
2019-08-07 17:03:46 -0300

    fix loading icon to match design

--- src/containers/User/Detail/Proposals/Proposals.jsx
+++ src/containers/User/Detail/Proposals/Proposals.jsx
@@ -1,8 +1,8 @@
 import React from "react";
+import { Spinner } from "pi-ui";
 import { useUserProposals } from "./hooks";
 import Proposal from "src/componentsv2/Proposal";
 import HelpMessage from "src/componentsv2/HelpMessage";
-import LoadingWithMessage from "src/componentsv2/LoadingWithMessage";
 
 const Proposals = props => {
   const { proposals, loading } = useUserProposals(props);
@@ -14,7 +14,9 @@ const Proposals = props => {
   return !loading ? emptyProposalList ?
       <>{proposals.map(proposal => renderProposal(proposal))}</> :
       <HelpMessage>This user has not created any proposals yet</HelpMessage> :
-      <LoadingWithMessage message={"Loading"} spinnerProps={{ invert: true }}/>;
+      <HelpMessage>
+        <Spinner invert/>
+      </HelpMessage>;
 };
 
 export default Proposals;
