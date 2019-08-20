83a9a72198659442e9c8bcac976ce4868782f610
thi4go <thiagofigueiredo777@gmail.com>
2019-08-20 10:54:54 -0300

    Adressing review comments

--- src/componentsv2/ModalMDGuide/ModalMDGuide.jsx
+++ src/componentsv2/ModalMDGuide/ModalMDGuide.jsx
@@ -1,5 +1,6 @@
 import React from "react";
 import { Modal, P } from "pi-ui";
+import styles from "./ModalMDGuide.module.css";
 
 const ModalMDGuide = ({ title = "Formatting Help", onClose, ...props }) => {
   return (
@@ -19,36 +20,36 @@ const MDGuide = () => (
   <div>
     <P>We use a slightly customized version from of Markdown for formatting. See below for some basics.</P>
 
-    <div style={styles.table}>
+    <div className={styles.table}>
 
-      <div style={styles.tableHeader}>
+      <div className={styles.tableHeader}>
         <div><b>You type</b></div>
         <div><b>You see</b></div>
       </div>
 
-      <div style={styles.tableRow}>
+      <div className={styles.tableRow}>
         <div>*italics*</div>
         <div><i>italics</i></div>
       </div>
 
-      <div style={styles.tableRow}>
+      <div className={styles.tableRow}>
         <div>**bold**</div>
         <div><b>bold</b></div>
       </div>
 
-      <div style={styles.tableRow}>
+      <div className={styles.tableRow}>
         <div>~~strikethrough~~</div>
         <div><s>strikethrough</s></div>
       </div>
 
-      <div style={styles.tableRowEven}>
+      <div className={styles.tableRowEven}>
         <div>[decred!](https://decred.org)</div>
         <div >
           <a href="https://decred.org">decred!</a>
         </div>
       </div>
 
-      <div style={styles.tableRow}>
+      <div className={styles.tableRow}>
         <div>*item1 <br/> *item2 <br/> *item3 </div>
         <div>
           <ul>
@@ -59,21 +60,21 @@ const MDGuide = () => (
         </div>
       </div>
 
-      <div style={styles.tableRow}>
+      <div className={styles.tableRow}>
         <div>> quoted text</div>
         <div>
-          <blockquote style={styles.quotedText}>quoted text</blockquote>
+          <blockquote className={styles.quotedText}>quoted text</blockquote>
         </div>
       </div>
 
-      <div style={styles.tableRowEven}>
+      <div className={styles.tableRowEven}>
         <div>
           Lines starting with four spaces <br/>are treated like code: <br /> <br />
           <span className="spaces">    </span>if 1 * 3 != 3: <br />
           <span className="spaces">        </span>return false
         </div>
         <div >
-          <pre style={styles.codeBlock}>
+          <pre className={styles.codeBlock}>
           if 1 * 2 != 3:
           <br />
             return false
@@ -83,47 +84,5 @@ const MDGuide = () => (
     </div>
   </div> 
 );
-
-const styles = {
-  table: {
-    display: 'flex', 
-    flexDirection: 'column'
-  },
-  tableHeader: {
-    display: 'flex', 
-    justifyContent: 'space-around',
-    backgroundColor: '#F3F5F6',
-    marginTop: '10px',
-    paddingTop: '15px',
-    paddingBottom: '15px',
-  },
-  tableRow: {
-    display: 'flex', 
-    justifyContent: 'space-around',
-    paddingTop: '15px',
-    paddingBottom: '15px',
-    borderBottom: '1px solid #F3F5F6'
-  },
-  tableRowEven: {
-    display: 'flex', 
-    justifyContent: 'space-evenly',
-    paddingTop: '15px',
-    paddingBottom: '15px',
-    borderBottom: '1px solid #F3F5F6'
-  },
-  quotedText: {
-    borderLeft: '.25em solid #dfe2e5',
-    color: '#6a737d',
-    padding: '0 1em'
-  },
-  codeBlock: {
-    padding: '8px',
-    overflow: 'auto',
-    border: '1px solid #eeeed2',
-    backgroundColor: '#fcfcf7',
-    borderRadius: '2px',
-    marginTop: '25px'
-  }
-}
 
 export default ModalMDGuide;
--- src/componentsv2/ProposalForm/ProposalForm.jsx
+++ src/componentsv2/ProposalForm/ProposalForm.jsx
@@ -1,4 +1,4 @@
-import React, { useState } from "react";
+import React from "react";
 import PropTypes from "prop-types";
 import { withRouter } from "react-router-dom";
 import { Formik } from "formik";
