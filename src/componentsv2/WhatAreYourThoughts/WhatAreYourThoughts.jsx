e49792a5edb27dc2940fdac834d612681a668b78
Victor Guedes <victorgcramos@gmail.com>
2019-09-11 15:06:38 -0300

    Improving of 'What are your thoughts' block for not signed in users

--- src/componentsv2/WhatAreYourThoughts/WhatAreYourThoughts.jsx
+++ src/componentsv2/WhatAreYourThoughts/WhatAreYourThoughts.jsx
@@ -5,10 +5,10 @@ import styles from "./WhatAreYourThoughts.module.css";
 const WhatAreYourThoughts = ({ onLoginClick, onSignupClick }) => {
   return (
     <div className={styles.wrapper}>
-      <Text color="gray">What are your thoughts? Log in or Sign up</Text>
+      <Text color="gray">What are your thoughts?</Text>
       <div className={styles.buttonsWrapper}>
         {onLoginClick && (
-          <Button kind="secondary" onClick={onLoginClick}>
+          <Button kind="secondary" style={{ border: "solid 1px" }} onClick={onLoginClick}>
             Log in
           </Button>
         )}
