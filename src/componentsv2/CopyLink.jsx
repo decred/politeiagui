aab8f1d42409428fdb7731357145d0662e569f86
Fernando Abolafio <fernandohsabolafio@gmail.com>
2019-09-12 14:46:24 +0200

    add hover effect for github and link icons (#1398)

--- src/componentsv2/CopyLink.jsx
+++ src/componentsv2/CopyLink.jsx
@@ -1,19 +1,28 @@
 import React from "react";
+import { getThemeProperty, useHover, useTheme } from "pi-ui";
 import PropTypes from "prop-types";
 import IconButton from "src/componentsv2/IconButton";
 import CopyToClipboard from "src/componentsv2/CopyToClipboard";
 
-const CopyLink = ({ url, className }) => (
-  <CopyToClipboard value={url} tooltipText="Copy link">
-    {({ onCopyToClipboard }) => (
-      <IconButton
-        className={className}
-        type="link"
-        onClick={onCopyToClipboard}
-      />
-    )}
-  </CopyToClipboard>
-);
+const CopyLink = ({ url, className }) => {
+  const theme = useTheme();
+  const hoverColor = getThemeProperty(theme, "color-gray");
+  const [ref, isHovered] = useHover();
+  const iconColor = isHovered ? hoverColor : undefined;
+  return (
+    <CopyToClipboard value={url} tooltipText="Copy link">
+      {({ onCopyToClipboard }) => (
+        <IconButton
+          ref={ref}
+          iconColor={iconColor}
+          className={className}
+          type="link"
+          onClick={onCopyToClipboard}
+        />
+      )}
+    </CopyToClipboard>
+  );
+};
 
 CopyLink.propTypes = {
   url: PropTypes.string
