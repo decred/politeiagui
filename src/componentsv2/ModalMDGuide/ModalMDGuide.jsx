b466ebd6ef5aca6b402e6d1e0fa6c6ee66b68b8e
Fernando Abolafio <fernandohsabolafio@gmail.com>
2019-10-03 17:35:05 +0200

    tweak format table to match specs

--- src/componentsv2/ModalMDGuide/ModalMDGuide.jsx
+++ src/componentsv2/ModalMDGuide/ModalMDGuide.jsx
@@ -1,6 +1,67 @@
 import React from "react";
-import { Modal, P } from "pi-ui";
+import { Modal, P, Table } from "pi-ui";
 import styles from "./ModalMDGuide.module.css";
+
+const MDGuideTable = () => {
+  const buildRow = (label, content) => ({
+    "You type": label,
+    "You see": content
+  });
+  const tableContent = [
+    buildRow("*italics*", <i>italics</i>),
+    buildRow("**bold**", <b>bold</b>),
+    buildRow("~~strikethrough~~", <s>strikethrough</s>),
+    buildRow(
+      "[decred!](https://decred.org)",
+      <a href="https://decred.org">decred!</a>
+    ),
+    buildRow(
+      <div>
+        *item1 <br /> *item2 <br /> *item3{" "}
+      </div>,
+      <ul>
+        <li>item1</li>
+        <li>item2</li>
+        <li>item3</li>
+      </ul>
+    ),
+    buildRow(
+      "> quoted text",
+      <div className={styles.blockWrapper}>
+        <blockquote className={styles.quotedText}>quoted text</blockquote>
+      </div>
+    ),
+    buildRow(
+      <div>
+        Lines starting with four spaces <br />
+        are treated like code: <br /> <br />
+        <span className="spaces">    </span>if 1 * 3 != 3: <br />
+        <span className="spaces">        </span>return false
+      </div>,
+      <div className={styles.blockWrapper}>
+        <pre className={styles.codeBlock}>
+          if 1 * 2 != 3:
+          <br />
+          return false
+        </pre>
+      </div>
+    )
+  ];
+  return (
+    <>
+      <P>
+        We use a slightly customized version from of Markdown for formatting.
+        See below for some basics.
+      </P>
+      <Table
+        data={tableContent}
+        wrapperClassName={styles.table}
+        bodyCellClassName={styles.tableBodyCell}
+        headers={["You type", "You see"]}
+      />
+    </>
+  );
+};
 
 const ModalMDGuide = ({ title = "Formatting Help", onClose, ...props }) => {
   return (
@@ -11,78 +72,9 @@ const ModalMDGuide = ({ title = "Formatting Help", onClose, ...props }) => {
       contentStyle={{ width: "100%" }}
       titleStyle={{ paddingRight: "4rem" }}
     >
-      <MDGuide />
+      <MDGuideTable />
     </Modal>
   );
 };
-
-const MDGuide = () => (
-  <div>
-    <P>We use a slightly customized version from of Markdown for formatting. See below for some basics.</P>
-
-    <div className={styles.table}>
-
-      <div className={styles.tableHeader}>
-        <div><b>You type</b></div>
-        <div><b>You see</b></div>
-      </div>
-
-      <div className={styles.tableRow}>
-        <div>*italics*</div>
-        <div><i>italics</i></div>
-      </div>
-
-      <div className={styles.tableRow}>
-        <div>**bold**</div>
-        <div><b>bold</b></div>
-      </div>
-
-      <div className={styles.tableRow}>
-        <div>~~strikethrough~~</div>
-        <div><s>strikethrough</s></div>
-      </div>
-
-      <div className={styles.tableRow}>
-        <div>[decred!](https://decred.org)</div>
-        <div style={{ paddingRight: "40px"}}>
-          <a href="https://decred.org">decred!</a>
-        </div>
-      </div>
-
-      <div className={styles.tableRow}>
-        <div>*item1 <br/> *item2 <br/> *item3 </div>
-        <div>
-          <ul>
-            <li>item1</li>
-            <li>item2</li>
-            <li>item3</li>
-          </ul>
-        </div>
-      </div>
-
-      <div className={styles.tableRow}>
-        <div>> quoted text</div>
-        <div>
-          <blockquote className={styles.quotedText}>quoted text</blockquote>
-        </div>
-      </div>
-
-      <div className={styles.tableRow}>
-        <div>
-          Lines starting with four spaces <br/>are treated like code: <br /> <br />
-          <span className="spaces">    </span>if 1 * 3 != 3: <br />
-          <span className="spaces">        </span>return false
-        </div>
-        <div style={{ paddingRight: "30px"}}>
-          <pre className={styles.codeBlock}>
-          if 1 * 2 != 3:
-          <br />
-            return false
-          </pre>
-        </div>
-      </div>
-    </div>
-  </div> 
-);
 
 export default ModalMDGuide;
