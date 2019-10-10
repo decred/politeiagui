0673adeb25ad7729f3106c4a8b63822d10582589
Fernando Abolafio <fernandohsabolafio@gmail.com>
2019-10-10 16:05:40 +0200

    Remove the huge container for the 'go back' link (#1482)

--- src/pages/Proposals/Detail.jsx
+++ src/pages/Proposals/Detail.jsx
@@ -4,7 +4,7 @@ import ProposalDetail from "src/containers/Proposal/Detail";
 
 const PublicList = () => {
   return (
-    <MultipleContentPage topBannerHeight={82}>
+    <MultipleContentPage topBannerHeight={0}>
       {props => {
         return <ProposalDetail {...props} />;
       }}
