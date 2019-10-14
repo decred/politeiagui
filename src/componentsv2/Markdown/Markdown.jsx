e2ccb74bc43d404146f6da210cd619c8ebbf703c
Victor Guedes <victorgcramos@gmail.com>
2019-10-14 10:55:37 -0300

    fix travis build

--- src/componentsv2/Markdown/Markdown.jsx
+++ src/componentsv2/Markdown/Markdown.jsx
@@ -1,9 +1,6 @@
 import React from "react";
-// import { classNames } from "pi-ui";
 import PropTypes from "prop-types";
-// import ReactMarkdown from "react-markdown";
 import MarkdownToJsx from "markdown-to-jsx";
-// import { customRenderers, htmlParserRules } from "./helpers";
 import { rootHandler } from "./helpers";
 import "./styles.css";
 
@@ -11,18 +8,10 @@ const MarkdownRenderer = ({
   body,
   className,
   filterXss = true,
-  // escapeHtml = false,
   ...props
 }) => {
   return (
     <div className={className} {...props}>
-      {/* <ReactMarkdown
-        className="markdown-body"
-        escapeHtml={escapeHtml}
-        astPlugins={[htmlParserRules]}
-        renderers={customRenderers(filterXss)}
-        source={body}
-      /> */}
       <div className="markdown-body">
         <MarkdownToJsx
           options={{
--- src/componentsv2/Markdown/test/Markdown.test.js
+++ src/componentsv2/Markdown/test/Markdown.test.js
@@ -1,17 +1,20 @@
 import React from "react";
-import ReactMarkdown from "react-markdown";
+import MarkdownToJsx from "markdown-to-jsx";
 import { mount } from "enzyme";
-import { customRenderers } from "../helpers";
+import { rootHandler } from "../helpers";
 
 const maliciousBodyText =
   "![clickforxss](javascript:alert('XSS'))  [clickforxss](javascript:alert('XSS'))";
 
 it("filter potencial 'XSS' attackers", () => {
   const wrapper = mount(
-    <ReactMarkdown
-      source={maliciousBodyText}
-      renderers={customRenderers(true)}
-    />
+    <MarkdownToJsx
+      options={{
+        createElement: rootHandler(true)
+      }}
+    >
+      {maliciousBodyText}
+    </MarkdownToJsx>
   );
   wrapper.find("LinkRenderer").forEach(el => {
     expect(["x-javascript:alert('XSS')", "javascript:void(0)"]).toContain(
