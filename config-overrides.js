2e03b6d0e122322ac555ffc7af6c90789bf27dc9
Fernando Abolafio <fernandohsabolafio@gmail.com>
2019-10-21 19:29:23 +0200

    add csp meta to head

--- config-overrides.js
+++ config-overrides.js
@@ -1,4 +1,14 @@
 const path = require("path");
+const { override, addWebpackAlias } = require("customize-cra");
+const cspHtmlWebpackPlugin = require("csp-html-webpack-plugin");
+
+const cspConfigPolicy = {
+  "default-src": "'none'",
+  "base-uri": "'self'",
+  "object-src": "'none'",
+  "script-src": ["'self'"],
+  "style-src": ["'self'"]
+};
 
 module.exports = function override(config) {
   config.resolve = {
@@ -7,4 +17,21 @@ module.exports = function override(config) {
   };
 
   return config;
+};
+
+function addCspHtmlWebpackPlugin(config) {
+  if (process.env.NODE_ENV === "production") {
+    config.plugins.push(new cspHtmlWebpackPlugin(cspConfigPolicy));
+  }
+
+  return config;
+}
+
+module.exports = {
+  webpack: override(
+    addCspHtmlWebpackPlugin,
+    addWebpackAlias({
+      src: path.resolve(__dirname, "src")
+    })
+  )
 };
