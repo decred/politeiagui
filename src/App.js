e713da1a460a74fd0983cffb00573c153b137cf0
Fernando Abolafio <fernandohsabolafio@gmail.com>
2019-11-24 12:08:34 +0100

    fix import paths after master rebase

--- src/App.js
+++ src/App.js
@@ -1,15 +1,17 @@
 import React from "react";
 import { Router } from "src/componentsv2/Router";
-import Config from "src/Config";
+import Config from "src/containers/Config";
 import { defaultLightTheme, useTheme, useFont } from "pi-ui";
 import { ReduxProvider } from "src/redux";
 import Loader from "src/containers/Loader";
-import Routes from "src/pages/Root";
+import Routes from "src/pages";
 import { UserSessionChecker } from "src/containers/User/SessionChecker";
 import { LoginModalProvider } from "src/containers/User/Login";
 import Onboard from "src/containers/User/Onboard";
 import StaticContentProvider from "src/containers/StaticContent";
 import "pi-ui/dist/index.css";
+import "src/style/indexv2.css";
+import "src/validation/setupFormValidators";
 
 import SourceSansProLight from "src/assets/fonts/source-sans-pro/SourceSansPro-Light.ttf";
 import SourceSansProRegular from "src/assets/fonts/source-sans-pro/SourceSansPro-Regular.ttf";
