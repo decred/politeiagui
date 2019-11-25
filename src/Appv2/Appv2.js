447afc41ea900f37a09fd44e64251e63f43c757d
Amir Massarwa <amassarwi@gmail.com>
2019-11-25 23:45:12 +0100

    switch theme

--- src/Appv2/Appv2.js
+++ src/Appv2/Appv2.js
@@ -27,7 +27,7 @@ const App = () => {
   return (
     <ThemeProvider
       themes={{ light: defaultLightTheme, dark: defaultDarkTheme }}
-      defaultThemeName="dark"
+      defaultThemeName="light"
       fontConfig={fontConfig}
     >
       <Config>
