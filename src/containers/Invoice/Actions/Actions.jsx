35e50fe82d7b928af6558781800aea6582b697a4
Fernando Abolafio <fernandohsabolafio@gmail.com>
2019-12-05 14:15:26 +0000

    adjust invoice admin actions for mobile view

--- src/containers/Invoice/Actions/Actions.jsx
+++ src/containers/Invoice/Actions/Actions.jsx
@@ -1,5 +1,11 @@
-import React, { useCallback } from "react";
-import { Button, classNames } from "pi-ui";
+import React, { useCallback, useMemo } from "react";
+import {
+  Button,
+  classNames,
+  useMediaQuery,
+  Dropdown,
+  DropdownItem
+} from "pi-ui";
 import { useAdminInvoiceActions } from "./hooks";
 import AdminContent from "src/componentsv2/AdminContent";
 import { isUnreviewedInvoice } from "../helpers";
@@ -10,7 +16,7 @@ const InvoiceActions = ({ invoice }) => {
       "Admin invoices actions requires an 'AdminActionsProvider' on a higher level of the component tree. "
     );
   }
-
+  const mobile = useMediaQuery("(max-width: 560px)");
   const { onApprove, onReject, onDispute } = useAdminInvoiceActions();
 
   const withInvoice = useCallback(
@@ -20,27 +26,47 @@ const InvoiceActions = ({ invoice }) => {
     [invoice]
   );
 
+  const approveButton = (
+    <Button onClick={withInvoice(onApprove)}>Approve</Button>
+  );
+  const rejectButton = (
+    <Button
+      onClick={withInvoice(onReject)}
+      className={classNames("margin-right-s")}
+      noBorder
+      kind="secondary"
+    >
+      Reject
+    </Button>
+  );
+  const disputeButton = (
+    <Button
+      onClick={withInvoice(onDispute)}
+      className={classNames("margin-right-s")}
+      noBorder
+      kind="secondary"
+    >
+      Dispute
+    </Button>
+  );
+
   return (
     isUnreviewedInvoice(invoice) && (
       <AdminContent>
         <div className="justify-right margin-top-m">
-          <Button
-            onClick={withInvoice(onReject)}
-            className={classNames("margin-right-s")}
-            noBorder
-            kind="secondary"
-          >
-            Reject
-          </Button>
-          <Button
-            onClick={withInvoice(onDispute)}
-            className={classNames("margin-right-s")}
-            noBorder
-            kind="secondary"
-          >
-            Dispute
-          </Button>
-          <Button onClick={withInvoice(onApprove)}>Approve</Button>
+          {!mobile ? (
+            <>
+              {rejectButton}
+              {disputeButton}
+              {approveButton}
+            </>
+          ) : (
+            <Dropdown title="Actions">
+              <DropdownItem>{approveButton}</DropdownItem>
+              <DropdownItem>{rejectButton}</DropdownItem>
+              <DropdownItem>{disputeButton}</DropdownItem>
+            </Dropdown>
+          )}
         </div>
       </AdminContent>
     )
