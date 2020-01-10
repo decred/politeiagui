80a28b23162bb3421343b32149417d5913d15035
Amir Massarwa <amassarwi@gmail.com>
2020-01-10 18:05:49 +0100

    Dark theme tweaks (#1654)

--- src/componentsv2/HelpMessage/HelpMessage.jsx
+++ src/componentsv2/HelpMessage/HelpMessage.jsx
@@ -1,11 +1,13 @@
-import { Text, classNames } from "pi-ui";
 import React from "react";
+import { Text, classNames, useTheme } from "pi-ui";
 import styles from "./HelpMessage.module.css";
 
 const HelpMessage = ({ className, children }) => {
+  const { themeName } = useTheme();
+  const isDarkTheme = themeName === "dark";
   return (
     <div className={classNames("justify-center", styles.container)}>
-      <Text textAlign="center" className={classNames(className, styles.helpMessage)}>{children}</Text>
+      <Text textAlign="center" className={classNames(className, styles.helpMessage, isDarkTheme && styles.dark)}>{children}</Text>
     </div>
   );
 };
