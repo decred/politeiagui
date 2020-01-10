b7298b167590781ac94b6f60fda020e8c7b964aa
Amir Massarwa <amassarwi@gmail.com>
2020-01-10 20:19:48 +0100

    credits tab colors

--- src/containers/User/Detail/Credits/components/ProposalCreditsSection.jsx
+++ src/containers/User/Detail/Credits/components/ProposalCreditsSection.jsx
@@ -1,26 +1,31 @@
 import React from "react";
 import styles from "../Credits.module.css";
-import { Text, P, classNames } from "pi-ui";
+import { Text, P, classNames, useTheme } from "pi-ui";
 
-export default ({ proposalCredits, proposalCreditPrice }) =>
-  <div className={classNames(styles.block, "margin-top-l")}>
-    <div className={styles.blockDetails}>
-      <Text className={styles.title}>Proposal Credits</Text>
-      <Text
-        size="large"
-        className={classNames(
-          styles.status,
-          "margin-top-xs margin-bottom-xs"
-        )}
-      >
-        {proposalCredits}
-      </Text>
-    </div>
-    <div className={styles.description}>
-      <P className={styles.descriptionParagraph}>
-        <b>Proposal credits:</b> each proposal submission requires{" "}
-        <b>1 proposal</b> credit which costs{" "}
-        <b>exactly {proposalCreditPrice || 0.1} DCR</b>.
+export default ({ proposalCredits, proposalCreditPrice }) => {
+  const { themeName } = useTheme();
+  const isDarkTheme = themeName === "dark";
+  return (
+    <div className={classNames(styles.block, "margin-top-l")}>
+      <div className={styles.blockDetails}>
+        <Text className={classNames(styles.title, isDarkTheme && styles.darkTitle)}>Proposal Credits</Text>
+        <Text
+          size="large"
+          className={classNames(
+            styles.status,
+            "margin-top-xs margin-bottom-xs"
+          )}
+        >
+          {proposalCredits}
+        </Text>
+      </div>
+      <div className={styles.description}>
+        <P className={classNames(styles.descriptionParagraph, isDarkTheme && styles.darkDescriptionParagraph)}>
+          <b>Proposal credits:</b> each proposal submission requires{" "}
+          <b>1 proposal</b> credit which costs{" "}
+          <b>exactly {proposalCreditPrice || 0.1} DCR</b>.
       </P>
+      </div>
     </div>
-  </div>;
+  );
+};
