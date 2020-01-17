795d6d24f44ccdb594243a4f630bf46a23e6fc95
Amir Massarwa <amassarwi@gmail.com>
2020-01-17 23:45:18 +0100

    add payouts table

--- src/containers/Invoice/Payouts/PayoutsList.jsx
+++ src/containers/Invoice/Payouts/PayoutsList.jsx
@@ -1,5 +1,5 @@
 import React from "react";
-import { Link } from "pi-ui";
+import { Link, Spinner, Table } from "pi-ui";
 import { useAdminPayouts } from "./hooks";
 import { AdminInvoiceActionsProvider } from "src/containers/Invoice/Actions";
 import styles from "./PayoutsList.module.css";
@@ -7,7 +7,6 @@ import { Row } from "src/componentsv2/layout";
 
 const PayoutsList = ({ TopBanner, PageDetails, Main }) => {
   const { loading, payouts } = useAdminPayouts();
-  console.log(loading, payouts);
   return (
     <AdminInvoiceActionsProvider>
       <TopBanner>
@@ -17,11 +16,13 @@ const PayoutsList = ({ TopBanner, PageDetails, Main }) => {
             <Row justify="space-between" className={styles.actionsWrapper}>
               <div>
                 <Link className="cursor-pointer">
-                  Invite contractor
+                  Export To Csv
                 </Link>
               </div>
               <div>
-                <Link className="cursor-pointer">Generate Payouts</Link>
+                <Link className="cursor-pointer">
+                  Set Invoices To Paid
+                </Link>
               </div>
             </Row>
           }
@@ -29,6 +30,32 @@ const PayoutsList = ({ TopBanner, PageDetails, Main }) => {
         </PageDetails>
       </TopBanner>
       <Main fillScreen>
+        {loading && (
+          <div className={styles.spinnerWrapper}>
+            <Spinner invert />
+          </div>
+        )}
+        {!loading && payouts && (
+          <Table
+            bodyCellClassName={styles.tableBodyCell}
+            data={payouts.map(({ approvedtime, year, month, contractorname, contractorrate, labortotal, expensetotal, total, exchangerate, dcrtotal, address }) => {
+            return {
+              approvedtime: new Date(approvedtime * 1000).toUTCString(),
+              year,
+              month,
+              contractorname,
+              contractorrate: contractorrate / 100,
+              labortotal: labortotal / 100,
+              expensetotal: expensetotal / 100,
+              total: total / 100,
+              exchangerate: exchangerate / 100,
+              dcrtotal: dcrtotal / 100000000,
+              address
+            };
+          })}
+          headers={["Approved Time", "Year", "Month", "Name", "Rate(USD)", "Labor Total(USD)", "Expense Total(USD)", "Combined Total(USD)", "Exchange Rate(USD)", "Total Payment(DCR)", "Address"]}>
+          </Table>
+        )}
       </Main>
     </AdminInvoiceActionsProvider>
   );
