0df549a7f9758284fcd09258361bb8bf74a50cde
Amir Massarwa <amassarwi@gmail.com>
2020-01-25 17:46:10 +0100

    reply section colors (#1719)

--- src/componentsv2/CopyLink.jsx
+++ src/componentsv2/CopyLink.jsx
@@ -5,10 +5,12 @@ import IconButton from "src/componentsv2/IconButton";
 import CopyToClipboard from "src/componentsv2/CopyToClipboard";
 
 const CopyLink = ({ url, className }) => {
-  const { theme } = useTheme();
+  const { theme, themeName } = useTheme();
+  const isDarkTheme = themeName === "dark";
   const hoverColor = getThemeProperty(theme, "icon-hover-color");
+  const darkIconColor = getThemeProperty(theme, "text-color");
   const [ref, isHovered] = useHover();
-  const iconColor = isHovered ? hoverColor : undefined;
+  const iconColor = isHovered ? hoverColor : isDarkTheme ? darkIconColor : undefined;
   return (
     <CopyToClipboard value={url} tooltipText="Copy link">
       {({ onCopyToClipboard }) => (
