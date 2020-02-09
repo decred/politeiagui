505b9773779f1ab1add2318bf0b09774089ec51d
fernandoabolafio <fernandohsabolafio@gmail.com>
2020-02-09 15:46:00 +0100

    add a temporary fix for the tests

--- src/index.js
+++ src/index.js
@@ -2,11 +2,6 @@ import React from "react";
 import ReactDOM from "react-dom";
 import App from "./App.js";
 
-console.log("OASMPDOMASOPDMAPOSMDPO", App);
-
-const targetElement =
-  process.env.NODE_ENV === "test"
-    ? document.body
-    : document.getElementById("root");
+const targetElement = document.getElementById("root");
 
 ReactDOM.render(<App />, targetElement);
