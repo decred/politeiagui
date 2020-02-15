f5e33197a983d54270b537938b85bd72c2dc9373
Fernando Abolafio <fernandohsabolafio@gmail.com>
2020-02-15 15:14:46 +0100

    remove useRedux hook

--- src/redux/hooks.js
+++ src/redux/hooks.js
@@ -1,6 +1,5 @@
-import React, { useState, useMemo, useEffect, useContext } from "react";
+import React, { useMemo, useContext } from "react";
 import { bindActionCreators } from "redux";
-import { selectorMap } from "src/selectors";
 import { createSelectorHook, createDispatchHook } from "react-redux";
 import throttle from "lodash/throttle";
 
@@ -17,38 +16,6 @@ export function useAction(action) {
     dispatch
   ]);
   return boundAction;
-}
-
-const DEFAULT_MAP_DISPATCH = {};
-
-export function useRedux(
-  ownProps,
-  mapStateToProps,
-  mapDispatchToProps = DEFAULT_MAP_DISPATCH
-) {
-  const { store: redux } = useReduxContext();
-
-  const [state, setState] = useState(redux.getState());
-  const dispatch = redux.dispatch;
-  const selector = useMemo(() => selectorMap(mapStateToProps), [
-    mapStateToProps
-  ]);
-  const computedState = useMemo(() => selector(state, ownProps), [
-    selector,
-    state,
-    ownProps
-  ]);
-  const boundActions = useMemo(
-    () => bindActionCreators(mapDispatchToProps, dispatch),
-    [mapDispatchToProps, dispatch]
-  );
-
-  useEffect(() => redux.subscribe(() => setState(redux.getState())));
-
-  return useMemo(() => ({ ...computedState, ...boundActions }), [
-    computedState,
-    boundActions
-  ]);
 }
 
 export const useStoreSubscribe = (callbackFn) => {
