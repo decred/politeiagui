677e083ecdb61d290039198c9946dec2d7f2f0a9
Amir Massarwa <amassarwi@gmail.com>
2020-02-27 15:19:55 +0100

    Format timestamps using (#1796)

--- src/componentsv2/DateTooltip/DateTooltip.jsx
+++ src/componentsv2/DateTooltip/DateTooltip.jsx
@@ -3,12 +3,12 @@ import PropTypes from "prop-types";
 import distance from "date-fns/distance_in_words";
 import { Tooltip, classNames, useTheme } from "pi-ui";
 import styles from "./DateTooltip.module.css";
+import { formatUnixTimestamp } from "src/utilsv2";
 
 const getTimeAgo = timestamp =>
   distance(new Date(), new Date(timestamp * 1000), { addSuffix: true });
 
 const DateTooltip = ({ timestamp, placement, className, children }) => {
-  const date = new Date(timestamp * 1000);
   const timeAgo = useMemo(() => getTimeAgo(timestamp), [timestamp]);
 
   const { themeName } = useTheme();
@@ -17,7 +17,7 @@ const DateTooltip = ({ timestamp, placement, className, children }) => {
   return (
     <Tooltip
       className={classNames(className, styles.dateTooltip, isDarkTheme && styles.darkDateTooltip)}
-      content={date.toLocaleString()}
+      content={formatUnixTimestamp(timestamp)}
       placement={placement}
     >
       {children({ timeAgo })}
