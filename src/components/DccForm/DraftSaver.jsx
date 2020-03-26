1a8c85c3ee158270cf80e6f2e8214331ecfd76ef
Amir Massarwa <amassarwi@gmail.com>
2020-03-26 20:49:51 +0100

    move draftsaver to /components folder & update imports

---
+++ src/components/DccForm/DraftSaver.jsx
@@ -0,0 +1,85 @@
+import React, { useEffect, useState } from "react";
+import { Button } from "pi-ui";
+import { FormikConsumer } from "formik";
+import { useDraftDccs } from "src/containers/DCC/User/hooks";
+import useQueryString from "src/hooks/utils/useQueryString";
+import delay from "lodash/delay";
+
+const DraftSaver = ({ values, setValues, submitSuccess }) => {
+  const [draftId, setDraftId] = useQueryString("draft");
+  const {
+    draftDccs,
+    onDeleteDraftDcc: onDelete,
+    onSaveDraftDcc: onSave
+  } = useDraftDccs();
+  const [saving, setSaving] = useState(false);
+  const [saved, setSaved] = useState(false);
+
+  const canSaveDraft = values && !!values.type && !!values.nomineeid && !saved;
+
+  const executeFakeLoading = () => {
+    setSaving(true);
+    delay(() => {
+      setSaved(true);
+      setSaving(false);
+    }, 300);
+  };
+
+  const handleSave = () => {
+    const id = onSave({ id: draftId, ...values });
+    // first time saving this draft
+    if (!draftId) {
+      setDraftId(id);
+    }
+    executeFakeLoading();
+  };
+
+  useEffect(
+    function resetSaved() {
+      setSaved(false);
+    },
+    [values, setSaved]
+  );
+
+  useEffect(
+    function handleSubmitSuccess() {
+      if (submitSuccess && draftId) {
+        onDelete(draftId);
+      }
+    },
+    [submitSuccess, draftId, onDelete]
+  );
+
+  useEffect(
+    function handleInitializeFormFromDraft() {
+      const foundDraftDcc =
+        draftDccs && draftId && draftDccs[draftId];
+      if (foundDraftDcc) {
+        setValues(foundDraftDcc);
+      }
+    },
+    [draftDccs, draftId, setValues]
+  );
+  return (
+    <Button
+      type="button"
+      width={150}
+      kind={saving || !canSaveDraft ? "disabled" : "secondary"}
+      loading={saving}
+      onClick={handleSave}>
+      {saved ? "Saved ✓" : "Save Draft"}
+    </Button>
+  );
+};
+
+const Wrapper = (props) => {
+  return (
+    <FormikConsumer>
+      {(formikProps) => {
+        return <DraftSaver {...{ ...props, ...formikProps }} />;
+      }}
+    </FormikConsumer>
+  );
+};
+
+export default Wrapper;
--- src/containers/DCC/User/Drafts.jsx
+++ src/containers/DCC/User/Drafts.jsx
@@ -1,10 +1,10 @@
 import React, { useState, useCallback } from "react";
 import { Text } from "pi-ui";
 import { useDraftDccs } from "./hooks";
-import DraftDcc from "src/componentsv2/DCC/DraftDcc";
+import DraftDcc from "src/components/DCC/DraftDcc";
 import useBooleanState from "src/hooks/utils/useBooleanState";
-import ModalConfirm from "src/componentsv2/ModalConfirm";
-import HelpMessage from "src/componentsv2/HelpMessage";
+import ModalConfirm from "src/components/ModalConfirm";
+import HelpMessage from "src/components/HelpMessage";
 
 const Drafts = () => {
   const { draftDccs, onDeleteDraftDcc } = useDraftDccs();
@@ -16,7 +16,7 @@ const Drafts = () => {
   ] = useBooleanState(false);
 
   const handleOpenDeleteDraftModal = useCallback(
-    draftID => {
+    (draftID) => {
       setTargetDraftID(draftID);
       openDeleteModal();
     },
@@ -29,16 +29,15 @@ const Drafts = () => {
   }, [onDeleteDraftDcc, setTargetDraftID, targetDraftID]);
 
   const drafts = draftDccs
-    ? Object
-        .values(draftDccs)
-        .filter(draft => !!draft.id)
+    ? Object.values(draftDccs)
+        .filter((draft) => !!draft.id)
         .sort((a, b) => b.timestamp - a.timestamp)
     : [];
 
   return (
     <div className="margin-top-m">
       {drafts.length ? (
-        drafts.map(draft => (
+        drafts.map((draft) => (
           <DraftDcc
             key={`draft-${draft.id}`}
             onDelete={handleOpenDeleteDraftModal}
