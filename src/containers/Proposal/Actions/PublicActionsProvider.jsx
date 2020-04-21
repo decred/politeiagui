13140907cea98cfa76b4559dc0c0c5041d0a6e32
Amir Massarwa <amassarwi@gmail.com>
2020-04-21 02:55:23 +0200

    useCallback usage

--- src/containers/Proposal/Actions/PublicActionsProvider.jsx
+++ src/containers/Proposal/Actions/PublicActionsProvider.jsx
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { useCallback } from "react";
 import { Text } from "pi-ui";
 import Link from "src/components/Link";
 import { PublicProposalsActionsContext, usePublicActions } from "./hooks";
@@ -20,73 +20,84 @@ const PublicActionsProvider = ({ children }) => {
 
   const [handleOpenModal, handleCloseModal] = useModalContext();
 
-  // TODO: add usecallback for all handlers!
+  const handleOpenAbandonModal = useCallback(
+    (proposal) => {
+      handleOpenModal(ModalConfirmWithReason, {
+        title: `Abandon - ${proposal.name}`,
+        reasonLabel: "Abandon reason",
+        subject: "abandonProposal",
+        onSubmit: onAbandonProposal(proposal),
+        successTitle: "Proposal abandoned",
+        successMessage: (
+          <Text>
+            The proposal has been successfully abandoned! Now it will appear
+            under the <Link to="/?tab=abandoned">Abandoned</Link> tab.
+          </Text>
+        ),
+        onClose: handleCloseModal
+      });
+    },
+    [handleCloseModal, handleOpenModal, onAbandonProposal]
+  );
 
-  const handleOpenAbandonModal = (proposal) => {
-    handleOpenModal(ModalConfirmWithReason, {
-      title: `Abandon - ${proposal.name}`,
-      reasonLabel: "Abandon reason",
-      subject: "abandonProposal",
-      onSubmit: onAbandonProposal(proposal),
-      successTitle: "Proposal abandoned",
-      successMessage: (
-        <Text>
-          The proposal has been successfully abandoned! Now it will appear under
-          the <Link to="/?tab=abandoned">Abandoned</Link> tab.
-        </Text>
-      ),
-      onClose: handleCloseModal
-    });
-  };
+  const handleOpenAuthorizeVoteModal = useCallback(
+    (proposal) => {
+      handleOpenModal(ModalConfirm, {
+        title: `Authorize vote - ${proposal.name}`,
+        message: "Are you sure you want to authorize the vote start?",
+        onSubmit: onAuthorizeVote(proposal),
+        successTitle: "Proposal vote authorized",
+        successMessage: (
+          <Text>
+            The proposal vote has been successfully authorized! Wait for an
+            admin to start the vote for this proposal.
+          </Text>
+        ),
+        onClose: handleCloseModal
+      });
+    },
+    [handleOpenModal, handleCloseModal, onAuthorizeVote]
+  );
 
-  const handleOpenAuthorizeVoteModal = (proposal) => {
-    handleOpenModal(ModalConfirm, {
-      title: `Authorize vote - ${proposal.name}`,
-      message: "Are you sure you want to authorize the vote start?",
-      onSubmit: onAuthorizeVote(proposal),
-      successTitle: "Proposal vote authorized",
-      successMessage: (
-        <Text>
-          The proposal vote has been successfully authorized! Wait for an admin
-          to start the vote for this proposal.
-        </Text>
-      ),
-      onClose: handleCloseModal
-    });
-  };
+  const handleOpenRevokeVoteModal = useCallback(
+    (proposal) => {
+      handleOpenModal(ModalConfirm, {
+        title: `Revoke vote - ${proposal.name}`,
+        message: "Are you sure you want to revoke the vote start?",
+        onSubmit: onRevokeVote(proposal),
+        successTitle: "Proposal vote revoked",
+        successMessage: (
+          <Text>
+            The proposal vote has been successfully revoked! Now the admins
+            cannot start the vote for this proposal until you authorize it
+            again.
+          </Text>
+        ),
+        onClose: handleCloseModal
+      });
+    },
+    [handleCloseModal, handleOpenModal, onRevokeVote]
+  );
 
-  const handleOpenRevokeVoteModal = (proposal) => {
-    handleOpenModal(ModalConfirm, {
-      title: `Revoke vote - ${proposal.name}`,
-      message: "Are you sure you want to revoke the vote start?",
-      onSubmit: onRevokeVote(proposal),
-      successTitle: "Proposal vote revoked",
-      successMessage: (
-        <Text>
-          The proposal vote has been successfully revoked! Now the admins cannot
-          start the vote for this proposal until you authorize it again.
-        </Text>
-      ),
-      onClose: handleCloseModal
-    });
-  };
-
-  const handleStartVoteModal = (proposal) => {
-    handleOpenModal(ModalStartVote, {
-      title: `Start vote - ${proposal.name}`,
-      voteType: VOTE_TYPE_STANDARD,
-      onSubmit: onStartVote(proposal),
-      successTitle: "Proposal vote started",
-      successMessage: (
-        <Text>
-          The proposal vote has been successfully started! Now it will appear
-          under the <Link to="/?tab=voting">Voting</Link> tab.
-        </Text>
-      ),
-      onClose: handleCloseModal,
-      proposal
-    });
-  };
+  const handleStartVoteModal = useCallback(
+    (proposal) => {
+      handleOpenModal(ModalStartVote, {
+        title: `Start vote - ${proposal.name}`,
+        voteType: VOTE_TYPE_STANDARD,
+        onSubmit: onStartVote(proposal),
+        successTitle: "Proposal vote started",
+        successMessage: (
+          <Text>
+            The proposal vote has been successfully started! Now it will appear
+            under the <Link to="/?tab=voting">Voting</Link> tab.
+          </Text>
+        ),
+        onClose: handleCloseModal,
+        proposal
+      });
+    },
+    [handleOpenModal, handleCloseModal, onStartVote]
+  );
 
   const handleStartRunoffVoteModal = async (proposal) => {
     const [submissions] = await onFetchProposalsBatch(proposal.linkedfrom);
