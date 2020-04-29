6d3e8b6b0a37edc3605083defda20f2baa1043f5
Amir Massarwa <amassarwi@gmail.com>
2020-04-29 16:04:26 +0200

    [cms] Improve logo (#1876)

--- src/components/Logo/Logo.jsx
+++ src/components/Logo/Logo.jsx
@@ -1,9 +1,10 @@
 import React from "react";
-import { useTheme } from "pi-ui";
+import { useTheme, classNames } from "pi-ui";
 import PropTypes from "prop-types";
 import { useConfig } from "src/containers/Config";
+import styles from "./Logo.module.css";
 
-const Logo = ({ style }) => {
+const Logo = ({ style, isCMS }) => {
   const { themeName } = useTheme();
   const { logoLight, logoDark } = useConfig();
   const logoSrc = themeName === "dark" ? logoDark : logoLight;
@@ -12,13 +13,15 @@ const Logo = ({ style }) => {
       role="presentation"
       alt="logo"
       style={style}
+      className={classNames(isCMS && styles.cmsLogo)}
       src={require(`src/assets/images/${logoSrc}`)}
     />
   );
 };
 
 Logo.propTypes = {
-  style: PropTypes.object
+  style: PropTypes.object,
+  isCMS: PropTypes.bool
 };
 
 export default Logo;
