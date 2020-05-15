24c68ae88c2efad0fa3fa766365275047947a429
Tiago Alves Dulce <tiagoalvesdulce@outlook.com>
2020-05-15 17:33:53 -0300

    use implicit return

--- src/containers/Config/ConfigFilter.js
+++ src/containers/Config/ConfigFilter.js
@@ -2,13 +2,11 @@ import React from "react";
 import PropTypes from "prop-types";
 import { ConfigConsumer } from "./ConfigProvider";
 
-const ConfigFilter = ({ showIf, children }) => {
-  return (
-    <ConfigConsumer>
-      {(config) => (showIf(config) ? children : null)}
-    </ConfigConsumer>
-  );
-};
+const ConfigFilter = ({ showIf, children }) => (
+  <ConfigConsumer>
+    {(config) => (showIf(config) ? children : null)}
+  </ConfigConsumer>
+);
 
 ConfigFilter.propTypes = {
   showIf: PropTypes.func.isRequired,
