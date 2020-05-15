f6b5d46d376920d607309df86de47ba11b3d9cf6
Amir Massarwa <amassarwi@gmail.com>
2020-05-15 17:08:01 +0200

    prettify

--- src/containers/Invoice/Admin/Admin.jsx
+++ src/containers/Invoice/Admin/Admin.jsx
@@ -8,9 +8,7 @@ import styles from "./Admin.module.css";
 
 const InvoicesAdmin = ({ userID }) => {
   const { loading, invoices } = useAdminInvoices();
-  const userInovices = invoices.filter((inv) =>
-    inv.userid === userID
-  );
+  const userInovices = invoices.filter((inv) => inv.userid === userID);
   const renderInvoice = useCallback(
     (invoice) => (
       <Invoice
