b2b190ef69ab882305e7d429e0b4f13adb3ff32e
Victor Guedes <victorgcramos@gmail.com>
2020-05-28 10:40:41 -0300

    [cms] Fixes supervisorid's list: (#1942)

--- src/containers/User/Detail/ManageContractor/helpers.js
+++ src/containers/User/Detail/ManageContractor/helpers.js
@@ -28,3 +28,13 @@ export const selectDomainOptions = domainOptions.map((op, idx) => ({
   value: idx,
   label: op
 }));
+
+export const getSupervisorsOptions = (supervisors, currentUserID) =>
+  supervisors &&
+  Array.isArray(supervisors) &&
+  supervisors
+    .filter(({ id }) => id !== currentUserID)
+    .map(({ username, id }) => ({
+      label: username,
+      value: id
+    }));
