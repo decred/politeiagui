8bcefc0203bf1427cdc91b49f65377c87d96567d
Victor Guedes <victorgcramos@gmail.com>
2020-06-03 18:35:10 -0300

    fix minor styling issues

--- src/components/RecordWrapper/RecordWrapper.jsx
+++ src/components/RecordWrapper/RecordWrapper.jsx
@@ -128,7 +128,11 @@ export const Header = React.memo(function Header({
     <div className={styles.header}>
       {!mobile || disableMobileView ? (
         <div className={styles.titleWrapper}>
-          <div className={styles.titleEditWrapper}>
+          <div
+            className={classNames(
+              styles.titleEditWrapper,
+              isRfp && styles.rfpTitleWrapper
+            )}>
             {isRfp && <RfpTag />}
             {title}
             {edit}
