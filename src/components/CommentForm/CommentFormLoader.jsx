248119191526e32962540aa69b61ca6f20ed0254
Victor Guedes <victorgcramos@gmail.com>
2020-06-19 09:27:59 -0300

    fix: show dark loaders on darkmode (#2011)

--- src/components/CommentForm/CommentFormLoader.jsx
+++ src/components/CommentForm/CommentFormLoader.jsx
@@ -1,16 +1,22 @@
 import React from "react";
 import ContentLoader from "react-content-loader";
+import { useTheme, getThemeProperty } from "pi-ui";
 
-const ProposalFormLoader = () => (
-  <ContentLoader
-    height={420}
-    width={800}
-    speed={2}
-    primaryColor="#f3f3f3"
-    secondaryColor="#ecebeb">
-    <rect x="0" y="30" width="800" height="200" />
-    <rect x="630" y="250" width="170" height="50" />
-  </ContentLoader>
-);
+const ProposalFormLoader = () => {
+  const { theme } = useTheme();
+  const primaryColor = getThemeProperty(theme, "card-background");
+  const secondaryColor = getThemeProperty(theme, "dimmed-card-background");
+  return (
+    <ContentLoader
+      height={420}
+      width={800}
+      speed={2}
+      primaryColor={primaryColor}
+      secondaryColor={secondaryColor}>
+      <rect x="0" y="30" width="800" height="200" />
+      <rect x="630" y="250" width="170" height="50" />
+    </ContentLoader>
+  );
+};
 
 export default ProposalFormLoader;
--- src/components/ProposalForm/ProposalFormLoader.jsx
+++ src/components/ProposalForm/ProposalFormLoader.jsx
@@ -1,25 +1,31 @@
 import React from "react";
 import ContentLoader from "react-content-loader";
+import { useTheme, getThemeProperty } from "pi-ui";
 
-const ProposalFormLoader = () => (
-  <ContentLoader
-    height={420}
-    width={800}
-    speed={2}
-    primaryColor="#f3f3f3"
-    secondaryColor="#ecebeb">
-    <rect x="0" y="10" width="800" height="40" />
+const ProposalFormLoader = () => {
+  const { theme } = useTheme();
+  const primaryColor = getThemeProperty(theme, "card-background");
+  const secondaryColor = getThemeProperty(theme, "dimmed-card-background");
+  return (
+    <ContentLoader
+      height={420}
+      width={800}
+      speed={2}
+      primaryColor={primaryColor}
+      secondaryColor={secondaryColor}>
+      <rect x="0" y="10" width="800" height="40" />
 
-    <rect x="0" y="75" width="800" height="1" />
+      <rect x="0" y="75" width="800" height="1" />
 
-    <rect x="0" y="100" width="800" height="150" />
+      <rect x="0" y="100" width="800" height="150" />
 
-    <rect x="0" y="280" width="800" height="1" />
-    <rect x="0" y="310" width="130" height="14" />
-    <rect x="150" y="310" width="130" height="14" />
-    <rect x="500" y="360" width="140" height="40" />
-    <rect x="650" y="360" width="140" height="40" />
-  </ContentLoader>
-);
+      <rect x="0" y="280" width="800" height="1" />
+      <rect x="0" y="310" width="130" height="14" />
+      <rect x="150" y="310" width="130" height="14" />
+      <rect x="500" y="360" width="140" height="40" />
+      <rect x="650" y="360" width="140" height="40" />
+    </ContentLoader>
+  );
+};
 
 export default ProposalFormLoader;
