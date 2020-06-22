b2d7fe1cb0aa5d460753ca2064c4ad4c152d7426
Tiago Alves Dulce <tiagoalvesdulce@outlook.com>
2020-06-22 17:41:15 -0300

    fix: add message to DCR nominee invite to CMS (#2029)

--- src/components/DccForm/DccForm.jsx
+++ src/components/DccForm/DccForm.jsx
@@ -118,6 +118,13 @@ const DccForm = React.memo(function DccForm({
         </Message>
       )}
 
+      {isUserValid && (
+        <Message kind="info">
+          Before creating a new DCC the nominee must be invited to CMS by an
+          admin. Please contact an admin on DCR Contractors channel on matrix.
+        </Message>
+      )}
+
       <RadioButtonGroup
         label="DCC Type"
         name="type"
