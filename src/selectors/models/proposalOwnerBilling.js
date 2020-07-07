d53a716b6b3dbc324cecbccbe0074f3098c6309e
Tiago Alves Dulce <tiagoalvesdulce@outlook.com>
2020-07-07 18:12:47 -0300

    fix: cleanup

--- src/selectors/models/proposalOwnerBilling.js
+++ src/selectors/models/proposalOwnerBilling.js
@@ -6,10 +6,5 @@ const proposalOwnerBillingByToken = get([
   "byProposalToken"
 ]);
 
-export const getBillingInfo = (state) => (token) => {
-  console.log(token);
-  console.log(state);
-};
-
 export const makeGetBillingInfoByToken = (token) =>
   createSelector(proposalOwnerBillingByToken, get(token));
