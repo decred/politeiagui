12065149676f042ada5ecbe8f7913cc3218d6616
Tiago Alves Dulce <tiagoalvesdulce@outlook.com>
2020-10-02 12:10:27 -0300

    test(commands): use cy.server and wait for requests instead of using timeouts

--- cypress/support/commands.js
+++ cypress/support/commands.js
@@ -39,7 +39,7 @@ Cypress.Commands.add("assertLoggedInAs", (user) => {
 
 // TODO: add login using cy.request()
 Cypress.Commands.add("typeLogin", (user) => {
-  cy.findByText(/log in/i, { timeout: 10000 }).click();
+  cy.visit("/user/login");
   cy.findByLabelText(/email/i).type(user.email);
   cy.findByLabelText(/password/i).type(user.password);
   cy.findByRole("button", { text: /login/i }).click();
@@ -50,19 +50,35 @@ Cypress.Commands.add("typeLogin", (user) => {
 // Should use after login
 // TODO: add identity using cy.request()
 Cypress.Commands.add("typeIdentity", () => {
+  cy.server();
+  cy.route("GET", "api/v1/user/*").as("getUser");
+  cy.route("POST", "api/v1/user/key").as("newKey");
+  cy.route("POST", "api/v1/user/key/verify").as("keyVerify");
   cy.findByTestId("user-dropdown").click();
   cy.findByText(/account/i).click();
-  cy.findByText(/create new identity/i, { timeout: 20000 }).click();
+  cy.wait("@getUser").its("status").should("eq", 200);
+  cy.findByText(/create new identity/i).click();
   cy.findByText(/confirm/i).click();
-  cy.findByText(/ok/i, { timeout: 20000 }).click();
+  cy.wait("@newKey").its("status").should("eq", 200);
+  cy.findByText(/ok/i).click();
   cy.findByText(/auto verify/i).click();
-  cy.findByText(/go to proposals/i, { timeout: 10000 }).click();
+  cy.wait("@keyVerify").its("status").should("eq", 200);
+  cy.findByText(/go to proposals/i).click();
 });
 
 // TODO: add createProposal using cy.request()
 Cypress.Commands.add("typeCreateProposal", (proposal) => {
-  cy.findByText(/new proposal/i).click();
+  cy.server();
+  cy.route("POST", "/api/v1/proposals/new").as("newProposal");
+  cy.visit("/proposals/new");
   cy.findByTestId("proposal name", { timeout: 10000 }).type(proposal.name);
   cy.findByTestId("text-area").type(proposal.description);
   cy.findByText(/submit/i).click();
+  cy.wait("@newProposal").should((xhr) => {
+    expect(xhr.status).to.equal(200);
+    expect(xhr.response.body)
+      .to.have.property("censorshiprecord")
+      .and.be.a("object")
+      .and.have.all.keys("token", "signature", "merkle");
+  });
 });
