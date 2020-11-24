cf9a89bb4f9eac021cbfbf24e3b04dce65a9a830
thiago <thiagofigueiredo777@gmail.com>
2020-11-24 11:30:07 -0300

    add server pubkey to comments bundle download

--- src/containers/Comments/Download/DownloadComments.jsx
+++ src/containers/Comments/Download/DownloadComments.jsx
@@ -2,16 +2,21 @@ import React from "react";
 import PropTypes from "prop-types";
 import DownloadJSON from "src/components/DownloadJSON";
 import { useDownloadComments } from "./hooks";
+import { useLoader } from "src/containers/Loader";
 
 const DownloadComments = ({ recordToken, className }) => {
   const { comments } = useDownloadComments(recordToken);
+  const { apiInfo } = useLoader();
   return (
     !!comments &&
     !!comments.length && (
       <DownloadJSON
         label="Download Comments Bundle"
         fileName={`${recordToken}-comments`}
-        content={comments}
+        content={comments.map(c => ({
+          ...c, 
+          serverpublickey: apiInfo.pubkey 
+        }))}
         className={className}
       />
     )
