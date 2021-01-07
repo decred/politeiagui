8c52f821e525caa8a46c2cafacb1ec68d12968ca
Tiago Alves Dulce <tiagoalvesdulce@outlook.com>
2021-01-07 17:05:52 -0300

    fix: missing function renaming

--- src/components/InvoiceFilters/InvoiceFilterForm.jsx
+++ src/components/InvoiceFilters/InvoiceFilterForm.jsx
@@ -10,7 +10,7 @@ import styles from "./InvoiceFilterForm.module.css";
 import UserSearchSelect from "src/containers/User/Search/SearchSelector";
 import { Link } from "pi-ui";
 
-const DefaultInitialValues = (isAdminPage) => ({
+const defaultInitialValues = (isAdminPage) => ({
   date: getPreviousMonthsRange(2),
   users: [],
   filters: {
@@ -32,7 +32,7 @@ const InvoiceFilterForm = ({
 }) => {
   return (
     <Formik
-      initialValues={DefaultInitialValues(isAdminPage)}
+      initialValues={defaultInitialValues(isAdminPage)}
       onSubmit={onSubmit}>
       {(formikProps) => {
         const { values, setFieldValue } = formikProps;
@@ -127,7 +127,7 @@ const OnChangeFiltersModifier = ({ formikProps, isAdminPage = false }) => {
 
   useEffect(() => {
     if (all) {
-      setFieldValue("filters", DefaultInitialValues(false).filters);
+      setFieldValue("filters", defaultInitialValues(false).filters);
     }
   }, [all, setFieldValue, isAdminPage]);
   return null;
