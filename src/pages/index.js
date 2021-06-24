28d020ea239c3179cb08c9342bc252d818484e7e
Tiago Alves Dulce <tiagoalvesdulce@outlook.com>
2021-06-24 11:01:02 -0300

    feat: add banner (#2456)

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,15 +1,31 @@
 import React from "react";
 import { useConfig } from "src/containers/Config";
+import { useSelector } from "src/redux";
+import * as sel from "src/selectors";
 import RoutesPoliteia from "./Routes";
 import RoutesCMS from "./RoutesCMS";
+import { classNames, useTheme, DEFAULT_DARK_THEME_NAME } from "pi-ui";
+
 
 const Root = () => {
+  const isTestnet = useSelector(sel.isTestNet);
   const { recordType, constants } = useConfig();
+  const { themeName } = useTheme();
+  const isDarkTheme = themeName === DEFAULT_DARK_THEME_NAME;
   const mapRecordTypeToRoutes = {
     [constants.RECORD_TYPE_INVOICE]: <RoutesCMS />,
     [constants.RECORD_TYPE_PROPOSAL]: <RoutesPoliteia />
   };
-  return mapRecordTypeToRoutes[recordType];
+  const url = `https://${
+    isTestnet ? "test-proposals" : "proposals"
+  }.decred.org`;
+  return (
+  <>
+    <div className={classNames("archive-banner", isDarkTheme && "archive-banner__dark" )}>
+      <p>You are on the archived proposals website. <a href={url} className="archive-banner__link">Click here</a> to go to Politeia.</p>
+    </div>
+    {mapRecordTypeToRoutes[recordType]}
+  </>);
 };
 
 export default Root;
