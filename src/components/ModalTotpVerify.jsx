c987fdb42d3bdc0ebde088c56ba6992b61c620e0
Victor Guedes <victorgcramos@gmail.com>
2021-08-25 17:34:04 -0300

    fix: Display error message on 2fa modal.

--- src/components/ModalTotpVerify.jsx
+++ src/components/ModalTotpVerify.jsx
@@ -1,23 +1,34 @@
-import React from "react";
+import React, { useState } from "react";
 import PropTypes from "prop-types";
-import { Modal } from "pi-ui";
+import { Modal, Message } from "pi-ui";
 import VerifyTotp from "src/containers/User/Totp/Verify";
 import { TOTP_CODE_LENGTH } from "src/constants";
 
 const ModalTotpVerify = ({ show, onClose, onVerify }) => {
+  const [error, setError] = useState();
   const handleChange = (v) => {
     if (v.length === TOTP_CODE_LENGTH) {
-      onVerify(v).then(() => onClose());
+      onVerify(v)
+        .then(() => {
+          onClose();
+          setError();
+        })
+        .catch((e) => {
+          setError(e);
+        });
     }
   };
   return (
     <Modal show={show} onClose={onClose} title="Verify 2FA Code">
-      <VerifyTotp
-        onType={handleChange}
-        extended={false}
-        tabIndex={1}
-        title="Authenticator Code"
-      />
+      <>
+        {error && <Message kind="error">{error.toString()}</Message>}
+        <VerifyTotp
+          onType={handleChange}
+          extended={false}
+          tabIndex={1}
+          title="Authenticator Code"
+        />
+      </>
     </Modal>
   );
 };
