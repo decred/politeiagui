cc91d169af338c40af009e42caaea470eb71dc61
amass <amassarwi@gmail.com>
2021-09-07 00:48:51 +0300

    display a tooltip next to the author update title with info for the author about the feature.

--- src/components/CommentForm/CommentForm.jsx
+++ src/components/CommentForm/CommentForm.jsx
@@ -2,11 +2,19 @@ import React from "react";
 import PropTypes from "prop-types";
 import { Formik } from "formik";
 import FormikPersist from "src/components/FormikPersist";
-import { Button, Message, BoxTextInput } from "pi-ui";
+import {
+  Button,
+  Message,
+  BoxTextInput,
+  Tooltip,
+  Icon,
+  useMediaQuery
+} from "pi-ui";
 import { Row } from "../layout";
 import MarkdownEditor from "src/components/MarkdownEditor";
 import validationSchema from "./validation";
 import { usePolicy } from "src/hooks";
+import styles from "./CommentForm.module.css";
 
 const forbiddenCommentsMdElements = ["h1", "h2", "h3", "h4", "h5", "h6"];
 
@@ -22,6 +30,7 @@ const CommentForm = ({
   const {
     policyPi: { namesupportedchars, namelengthmax, namelengthmin }
   } = usePolicy();
+  const smallTablet = useMediaQuery("(max-width: 685px)");
   async function handleSubmit(
     { comment, title },
     { resetForm, setSubmitting, setFieldError }
@@ -74,14 +83,52 @@ const CommentForm = ({
               <Message kind="error">{errors.global.toString()}</Message>
             )}
             {isAuthorUpdate && (
-              <BoxTextInput
-                placeholder="Update title"
-                name="title"
-                tabIndex={1}
-                value={values.title}
-                onChange={handleTitleChangeWithTouched}
-                error={touched.title && errors.title}
-              />
+              <Row noMargin align="center" wrap={smallTablet}>
+                <BoxTextInput
+                  placeholder="Update title"
+                  name="title"
+                  tabIndex={1}
+                  className={styles.updateTitle}
+                  value={values.title}
+                  onChange={handleTitleChangeWithTouched}
+                  error={touched.title && errors.title}
+                />
+                <Tooltip
+                  contentClassName={styles.updateTitleTooltip}
+                  className={styles.titleTooltipWrapper}
+                  placement="left"
+                  content={
+                    <div>
+                      <p>
+                        Once a proposal vote has finished, all existing comment
+                        threads are locked.
+                      </p>
+                      <br />
+                      <p>
+                        When a proposal author wants to give an update they will
+                        start a new comment thread. The author is the only user
+                        that will have the ability to start a new comment thread
+                        once the voting period has finished. Each update will
+                        have an author provided title.
+                      </p>
+                      <br />
+                      <p>
+                        Anyone can reply to any comments in the thread and can
+                        cast upvotes/downvotes for any comments in the thread.
+                      </p>
+                      <br />
+                      <p>
+                        The comment thread will remain open until either the
+                        author starts a new update thread or an admin marks the
+                        proposal as closed/completed.
+                      </p>
+                    </div>
+                  }>
+                  <div>
+                    <Icon type="info" size={smallTablet ? "md" : "lg"} />
+                  </div>
+                </Tooltip>
+              </Row>
             )}
             <MarkdownEditor
               allowImgs={false}
