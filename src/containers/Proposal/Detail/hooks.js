950247cdd14b174a97c3019ad583bbd06c7b0016
Victor Guedes <victorgcramos@gmail.com>
2021-09-21 18:10:31 -0300

    fix: revert func memoization test

--- src/containers/Proposal/Detail/hooks.js
+++ src/containers/Proposal/Detail/hooks.js
@@ -182,11 +182,11 @@ export function useProposal(token, threadParentID) {
     proposals
   );
 
-  const onReloadProposalDetails = () => send("RETRY", { status: "idle" });
-  // const onReloadProposalDetails = useCallback(
-  //   () => send("RETRY", { status: "idle" }),
-  //   [send]
-  // );
+  const onReloadProposalDetails = useCallback(
+    () => send("RETRY", { status: "idle" }),
+    [send]
+  );
+
   const proposalToken = getProposalToken(proposalWithLinks);
   const proposalState = proposalWithLinks?.state;
 
