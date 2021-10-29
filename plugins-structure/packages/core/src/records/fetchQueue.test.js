4a858d0555509bfd8d4d132f4b735da10cd8853d
Tiago Alves Dulce <tiagoalvesdulce@outlook.com>
2021-10-29 13:24:59 -0300

    test: add fetchQueue tests

---
+++ plugins-structure/packages/core/src/records/fetchQueue.test.js
@@ -0,0 +1,143 @@
+/**
+ * This is the integration test between recordsInventory and records slices.
+ * The goal is to test the recordsFetchQueue.
+ */
+import recordsInventoryReducer, {
+  fetchRecordsInventory,
+} from "./inventory/recordsInventorySlice";
+import apiReducer, { fetchApi } from "../api/apiSlice";
+import recordsReducer, {
+  fetchRecordsNextPage,
+  pushFetchQueue,
+} from "./records/recordsSlice";
+import { configureStore } from "@reduxjs/toolkit";
+import { client } from "../client";
+
+const mockRecord = {
+  state: 2,
+  status: 2,
+  version: 1,
+  timestamp: 1500000000,
+  username: "user1",
+  metadata: [],
+  files: [],
+  censorshiprecord: {
+    token: "fake_token",
+    merkle: "fake_merkle",
+    signature: "fake_signature",
+  },
+};
+
+const mockApiReturn = {
+  version: 1,
+  route: "/v1",
+  pubkey: "fake_pubkey",
+  testnet: true,
+  mode: "piwww",
+  activeusersession: true,
+};
+
+const mockCsrfToken = "fake_csrf";
+
+describe("Given the recordsInventory slice and the records slice", () => {
+  let store;
+  const params = {
+    recordsState: 2,
+    status: 2,
+    page: 1,
+  };
+  let fetchApiSpy;
+  // spy on the fetchRecords client function
+  let fetchRecordsSpy;
+  // spy on the fetchRecordsInventory client function
+  let fetchRecordsInventorySpy;
+  beforeEach(() => {
+    // mock a minimal store with extra argument
+    // re-create the store before each test
+    const staticReducers = {
+      api: apiReducer,
+      recordsInventory: recordsInventoryReducer,
+      records: recordsReducer,
+    };
+    store = configureStore({
+      reducer: staticReducers,
+      middleware: (getDefaultMiddleware) =>
+        getDefaultMiddleware({
+          // This will make the client available in the 'extra' argument
+          // for all our thunks created with createAsyncThunk
+          thunk: {
+            extraArgument: client,
+          },
+        }),
+    });
+    fetchRecordsSpy = jest.spyOn(client, "fetchRecords");
+    fetchApiSpy = jest.spyOn(client, "fetchApi");
+    fetchRecordsInventorySpy = jest.spyOn(client, "fetchRecordsInventory");
+  });
+  afterEach(() => {
+    fetchApiSpy.mockRestore();
+    fetchRecordsSpy.mockRestore();
+    fetchRecordsInventorySpy.mockRestore();
+  });
+  describe("when fetchRecordsInventory is fired", () => {
+    it("should populate the fetchQueue when the fetchRecordsInventory is dispatched", async () => {
+      const resValue = {
+        vetted: { public: ["fake_token", "fake_token2", "fake_token3"] },
+        unvetted: {},
+      };
+      fetchRecordsInventorySpy.mockResolvedValueOnce(resValue);
+      fetchRecordsSpy.mockResolvedValueOnce({
+        fake_token: mockRecord,
+        fake_token2: mockRecord,
+        fake_token3: mockRecord,
+      });
+
+      await store.dispatch(fetchRecordsInventory(params));
+
+      const { recordsInventory, records } = store.getState();
+      expect(recordsInventory.vetted.public.tokens).toEqual([
+        "fake_token",
+        "fake_token2",
+        "fake_token3",
+      ]);
+      expect(recordsInventory.vetted.public.status).toEqual("succeeded/isDone");
+      expect(recordsInventory.vetted.public.lastPage).toEqual(1);
+      expect(records.recordsFetchQueue.vetted.public).toEqual([
+        "fake_token",
+        "fake_token2",
+        "fake_token3",
+      ]);
+      expect(records.status).toEqual("loading");
+    });
+  });
+  describe("when recordsFetchQueue", () => {
+    it("should", async () => {
+      fetchApiSpy.mockResolvedValueOnce({
+        api: mockApiReturn,
+        csrf: mockCsrfToken,
+      });
+      await store.dispatch(fetchApi());
+      await store.dispatch(
+        pushFetchQueue({
+          recordsState: 2,
+          status: 2,
+          records: ["fake_token", "fake_token2", "fake_token3"],
+        })
+      );
+      let state = store.getState();
+      expect(state.records.recordsFetchQueue.vetted.public).toEqual([
+        "fake_token",
+        "fake_token2",
+        "fake_token3",
+      ]);
+      await store.dispatch(
+        fetchRecordsNextPage({
+          recordsState: 2,
+          status: 2,
+        })
+      );
+      state = store.getState();
+      expect(state.records.recordsFetchQueue.vetted.public).toEqual([]);
+    });
+  });
+});
