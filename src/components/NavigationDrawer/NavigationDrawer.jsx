c55764117c1f81c9643645fbaf5c73a78259b2a5
Victor Guedes <victorgcramos@gmail.com>
2021-12-02 08:19:45 -0300

    fix: User menu alignment on mobile.

--- src/components/NavigationDrawer/NavigationDrawer.jsx
+++ src/components/NavigationDrawer/NavigationDrawer.jsx
@@ -4,31 +4,22 @@ import React from "react";
 import HeaderNav from "../HeaderNav";
 import SidebarContent from "../SidebarContent";
 import styles from "./NavigationDrawer.module.css";
-import HamburgerMenu from "src/components/HamburgerMenu";
 
-const NavigationDrawer = ({ show, fullScreen, toggleShowMenu }) => {
+const NavigationDrawer = ({ show, fullScreen }) => {
   useLockBodyScrollOnTrue(show);
   return (
-    <>
-      {/* this div is used when tabIng makes the page scroll down so the user can close the Navigation Drawer */}
-      {show && (
-        <div className={styles.navigationDrawerHeader}>
-          <HamburgerMenu toggleShowMenu={toggleShowMenu} showMenu={show} />
-        </div>
-      )}
-      <div
-        className={classNames(
-          fullScreen
-            ? styles.navigationDrawerFullScreen
-            : styles.navigationDrawer,
-          show && styles.navigationDrawerShow
-        )}>
-        <div className={classNames(styles.navWrapper, "margin-bottom-s")}>
-          <HeaderNav />
-        </div>
-        <SidebarContent />
+    <div
+      className={classNames(
+        fullScreen
+          ? styles.navigationDrawerFullScreen
+          : styles.navigationDrawer,
+        show && styles.navigationDrawerShow
+      )}>
+      <div className={classNames(styles.navWrapper, "margin-bottom-s")}>
+        <HeaderNav />
       </div>
-    </>
+      <SidebarContent />
+    </div>
   );
 };
 
