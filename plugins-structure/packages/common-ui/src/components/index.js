dece5167d8f780dcacc1eba1d3122209546f2d3b
Victor Guedes <victorgcramos@gmail.com>
2022-02-11 17:02:57 -0300

    feat(common-ui): record details components + utils

--- plugins-structure/packages/common-ui/src/components/index.js
+++ plugins-structure/packages/common-ui/src/components/index.js
@@ -1,5 +1,6 @@
-export * from "./Join";
 export * from "./DateTooltip";
 export * from "./Event";
+export * from "./Join";
+export * from "./LabelValueList";
 export * from "./RecordCard";
 export * from "./RecordsList";
