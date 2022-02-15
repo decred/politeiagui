516794c595c49a1583663d880920f23ab67718ff
Victor Guedes <victorgcramos@gmail.com>
2022-02-15 16:42:39 -0300

    feat(comments): improve comments list ui

--- plugins-structure/packages/comments/src/ui/CommentCard/CommentCard.js
+++ plugins-structure/packages/comments/src/ui/CommentCard/CommentCard.js
@@ -1,26 +1,40 @@
 import React from "react";
 import PropTypes from "prop-types";
-import { Card } from "pi-ui";
+import { Card, Link } from "pi-ui";
+import { Event, Join } from "@politeiagui/common-ui";
 import styles from "./styles.module.css";
 
-export const CommentCard = ({ comment }) => {
+export const CommentCard = ({ comment, showCensor, onCensor }) => {
   console.log("comment", comment);
 
   return (
     <Card paddingSize="small">
       <div className={styles.header}>
-        <div className={styles.summary}>Summary</div>
+        <div className={styles.summary}>
+          <Join>
+            <Link href="#comment">{comment.username}</Link>
+            <Event event="" timestamp={comment.timestamp} />
+            {showCensor && (
+              <span className={styles.censor} onClick={onCensor}>
+                Censor
+              </span>
+            )}
+          </Join>
+        </div>
         <div className={styles.actions}>
           <div>Like</div>
           <div>Dislike</div>
         </div>
       </div>
-      <div className={styles.body}>Body</div>
-      <div className={styles.footer}>Footer</div>
+      <div className={styles.body}>{comment.comment}</div>
+      <div className={styles.footer}>
+        <span>Reply</span>
+      </div>
     </Card>
   );
 };
 
 Comment.propTypes = {
   comment: PropTypes.object.isRequired,
+  showCensor: PropTypes.bool,
 };
--- plugins-structure/packages/comments/src/ui/Comments/Comments.js
+++ plugins-structure/packages/comments/src/ui/Comments/Comments.js
@@ -1,13 +1,35 @@
 import React from "react";
 import PropTypes from "prop-types";
 import { CommentCard } from "../CommentCard";
+import { Button, Card, H2 } from "pi-ui";
+import styles from "./styles.module.css";
 
 export const Comments = ({ comments }) => {
   console.log("comments", comments);
 
-  return Object.values(comments).map((c) => <CommentCard comment={c} />);
+  return (
+    <div>
+      <Card paddingSize="small" className={styles.header}>
+        <H2 className={styles.title}>
+          Comments{" "}
+          <span className={styles.count}>({Object.keys(comments).length})</span>
+        </H2>
+        <div className={styles.filters}>
+          <div>
+            Sort by: <select></select>
+          </div>
+          <Button size="sm" kind="secondary">
+            Flat Mode
+          </Button>
+        </div>
+      </Card>
+      {Object.values(comments).map((c) => (
+        <CommentCard comment={c} showCensor={true} />
+      ))}
+    </div>
+  );
 };
 
 Comments.propTypes = {
-  comments: PropTypes.array.isRequired,
+  comments: PropTypes.object.isRequired,
 };
