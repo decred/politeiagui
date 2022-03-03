ba539d98d046a4569f79566fd28fa5dea14ba904
Tiago Alves Dulce <tiagoalvesdulce@outlook.com>
2022-03-03 17:18:08 -0300

    style: prettify

--- src/components/Proposal/VotesCount.jsx
+++ src/components/Proposal/VotesCount.jsx
@@ -1,10 +1,6 @@
 import React from "react";
 import PropTypes from "prop-types";
-import {
-  Text,
-  ButtonIcon,
-  classNames
-} from "pi-ui";
+import { Text, ButtonIcon, classNames } from "pi-ui";
 import styles from "./Proposal.module.css";
 
 const VotesCount = ({
