17b95e20dd67b1fa6cfba5f16914a72054a17e85
Victor Guedes <victorgcramos@gmail.com>
2022-03-30 18:28:04 -0300

    multi: Add Themes on Pi app.

---
+++ plugins-structure/apps/politeia/src/routes/utils.js
@@ -0,0 +1,33 @@
+import React from "react";
+import ReactDOM from "react-dom";
+import { Provider } from "react-redux";
+import { store } from "@politeiagui/core";
+import { connectReducers } from "@politeiagui/core";
+import { UiTheme, themeReducer } from "@politeiagui/common-ui/layout";
+
+export function createAppRoute({
+  path,
+  reducers,
+  Component,
+  rootId = "#root",
+}) {
+  return {
+    path,
+    view: async (params) => {
+      await connectReducers([
+        ...reducers,
+        { key: "uiTheme", reducer: themeReducer },
+      ]);
+      return ReactDOM.render(
+        <Provider store={store}>
+          <UiTheme>
+            <Component {...params} />
+          </UiTheme>
+        </Provider>,
+        document.querySelector(rootId)
+      );
+    },
+    cleanup: () =>
+      ReactDOM.unmountComponentAtNode(document.querySelector(rootId)),
+  };
+}
