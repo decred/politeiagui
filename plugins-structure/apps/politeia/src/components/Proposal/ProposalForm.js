159758ef395861c89cec6fed279b3804c1851fb8
Victor Guedes <victorgcramos@gmail.com>
2022-04-20 12:27:30 -0300

    refactor(pi): isolate proposal form component

---
+++ plugins-structure/apps/politeia/src/components/Proposal/ProposalForm.js
@@ -0,0 +1,67 @@
+import React from "react";
+import { RecordForm } from "@politeiagui/common-ui";
+import { getStartEndDatesRange } from "../../utils/date";
+
+const PROPOSAL_TYPE_OPTIONS = [
+  { label: "Regular Proposal", value: 1 },
+  { label: "RFP Proposal", value: 2 },
+  { label: "RFP Submission", value: 3 },
+];
+
+// TODO: Get domains from pi policy
+const PROPOSAL_DOMAIN_OPTIONS = [
+  { label: "Development", value: "development" },
+  { label: "Marketing", value: "marketing" },
+  { label: "Research", value: "research" },
+  { label: "Design", value: "design" },
+];
+
+// TODO: Get `startdatemin` and `enddatemax` from pi policy
+const DATES_RANGE = {
+  min: 604800,
+  max: 31557600,
+};
+export function ProposalForm({ onSubmit, onSave, initialValues }) {
+  return (
+    <RecordForm onSubmit={onSubmit} initialValues={initialValues}>
+      {({
+        DatePickerInput,
+        TextInput,
+        SelectInput,
+        MarkdownInput,
+        SubmitButton,
+        SaveButton,
+      }) => {
+        return (
+          <div>
+            <SelectInput
+              options={PROPOSAL_TYPE_OPTIONS}
+              name="type"
+              placeholder="Proposal Type"
+            />
+            <TextInput name="name" placeholder="Proposal Name" />
+            <TextInput name="amount" placeholder="Amount (USD)" />
+            <DatePickerInput
+              name="startDate"
+              placeholder="Start Date"
+              years={getStartEndDatesRange(DATES_RANGE.min, DATES_RANGE.max)}
+            />
+            <DatePickerInput
+              name="endDate"
+              placeholder="End Date"
+              years={getStartEndDatesRange(DATES_RANGE.min, DATES_RANGE.max)}
+            />
+            <SelectInput
+              options={PROPOSAL_DOMAIN_OPTIONS}
+              name="domain"
+              placeholder="Domain"
+            />
+            <MarkdownInput name="body" />
+            <SaveButton onSave={onSave} />
+            <SubmitButton />
+          </div>
+        );
+      }}
+    </RecordForm>
+  );
+}
--- plugins-structure/apps/politeia/src/pages/New/New.js
+++ plugins-structure/apps/politeia/src/pages/New/New.js
@@ -1,26 +1,5 @@
 import React from "react";
-import { RecordForm } from "@politeiagui/common-ui";
-import { getStartEndDatesRange } from "../../utils/date";
-
-const PROPOSAL_TYPE_OPTIONS = [
-  { label: "Regular Proposal", value: 1 },
-  { label: "RFP Proposal", value: 2 },
-  { label: "RFP Submission", value: 3 },
-];
-
-// TODO: Get domains from pi policy
-const PROPOSAL_DOMAIN_OPTIONS = [
-  { label: "Development", value: "development" },
-  { label: "Marketing", value: "marketing" },
-  { label: "Research", value: "research" },
-  { label: "Design", value: "design" },
-];
-
-// TODO: Get `startdatemin` and `enddatemax` from pi policy
-const DATES_RANGE = {
-  min: 604800,
-  max: 31557600,
-};
+import { ProposalForm } from "../../components/Proposal/ProposalForm";
 
 function New() {
   function handleSubmit(data) {
@@ -29,51 +8,7 @@ function New() {
   function handleSave(data) {
     console.log("saving...", data);
   }
-  return (
-    <RecordForm
-      onSubmit={handleSubmit}
-      initialValues={{ name: "", amount: "" }}
-    >
-      {({
-        DatePickerInput,
-        TextInput,
-        SelectInput,
-        MarkdownInput,
-        SubmitButton,
-        SaveButton,
-      }) => {
-        return (
-          <div>
-            <SelectInput
-              options={PROPOSAL_TYPE_OPTIONS}
-              name="type"
-              placeholder="Proposal Type"
-            />
-            <TextInput name="name" placeholder="Proposal Name" />
-            <TextInput name="amount" placeholder="Amount (USD)" />
-            <DatePickerInput
-              name="startDate"
-              placeholder="Start Date"
-              years={getStartEndDatesRange(DATES_RANGE.min, DATES_RANGE.max)}
-            />
-            <DatePickerInput
-              name="endDate"
-              placeholder="End Date"
-              years={getStartEndDatesRange(DATES_RANGE.min, DATES_RANGE.max)}
-            />
-            <SelectInput
-              options={PROPOSAL_DOMAIN_OPTIONS}
-              name="domain"
-              placeholder="Domain"
-            />
-            <MarkdownInput name="body" />
-            <SaveButton onSave={handleSave} />
-            <SubmitButton />
-          </div>
-        );
-      }}
-    </RecordForm>
-  );
+  return <ProposalForm onSave={handleSave} onSubmit={handleSubmit} />;
 }
 
 export default New;
