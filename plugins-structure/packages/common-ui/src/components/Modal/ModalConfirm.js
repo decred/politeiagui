38d67475a3d50a928af3d6f47aada5e8c564cdd1
Victor Guedes <victorgcramos@gmail.com>
2022-04-25 16:45:53 -0300

    fix(common-ui): remove memo from modalconfirm

--- plugins-structure/packages/common-ui/src/components/Modal/ModalConfirm.js
+++ plugins-structure/packages/common-ui/src/components/Modal/ModalConfirm.js
@@ -4,78 +4,76 @@ import { Button, Icon, Message, Modal, Text } from "pi-ui";
 import { useModal } from "./ModalProvider";
 import styles from "./styles.module.css";
 
-export const ModalConfirm = React.memo(
-  ({
-    show,
-    onClose,
-    onSubmit,
-    title,
-    message,
-    successTitle,
-    successMessage,
-    onCloseSuccess,
-  }) => {
-    const [success, setSuccess] = useState();
-    const [error, setError] = useState();
-    const [, close] = useModal();
-    const modalTitle = (success && successTitle) || title;
-    async function handleSubmit() {
-      try {
-        await onSubmit();
-        setSuccess(true);
-      } catch (error) {
-        setError(error);
-      }
+export const ModalConfirm = ({
+  show,
+  onClose,
+  onSubmit,
+  title,
+  message,
+  successTitle,
+  successMessage,
+  onCloseSuccess,
+}) => {
+  const [success, setSuccess] = useState();
+  const [error, setError] = useState();
+  const [, close] = useModal();
+  const modalTitle = (success && successTitle) || title;
+  async function handleSubmit() {
+    try {
+      await onSubmit();
+      setSuccess(true);
+    } catch (error) {
+      setError(error);
     }
-    async function handleCloseSuccess() {
-      try {
-        await onCloseSuccess();
-        close();
-      } catch (error) {
-        setError(error);
-      }
-    }
-    async function handleClose() {
-      try {
-        await onClose();
-        close();
-      } catch (error) {
-        setError(error);
-      }
-    }
-    return (
-      <Modal
-        show={show}
-        onClose={handleClose}
-        title={modalTitle}
-        iconComponent={
-          !success ? (
-            <Icon type="info" size={26} />
-          ) : (
-            <Icon type="checkmark" size={26} />
-          )
-        }
-      >
-        {error && <Message kind="error">{error.toString()}</Message>}
-        {!success ? (
-          <div className={styles.modalContent}>
-            <Text>{message}</Text>
-            <div className={styles.modalButtons}>
-              <Button onClick={handleSubmit}>Confirm</Button>
-            </div>
-          </div>
-        ) : (
-          <div className={styles.modalContent}>
-            <Text>{successMessage}</Text>
-            <div className={styles.modalButtons}>
-              <Button onClick={handleCloseSuccess}>Ok</Button>
-            </div>
-          </div>
-        )}
-      </Modal>
-    );
   }
-);
+  async function handleCloseSuccess() {
+    try {
+      await onCloseSuccess();
+      close();
+    } catch (error) {
+      setError(error);
+    }
+  }
+  async function handleClose() {
+    try {
+      await onClose();
+      close();
+    } catch (error) {
+      setError(error);
+    }
+  }
+  return (
+    <Modal
+      show={show}
+      onClose={handleClose}
+      title={modalTitle}
+      iconComponent={
+        !success ? (
+          <Icon type="info" size={26} />
+        ) : (
+          <Icon type="checkmark" size={26} />
+        )
+      }
+    >
+      {error && <Message kind="error">{error.toString()}</Message>}
+      {!success ? (
+        <div className={styles.modalContent}>
+          <Text>{message}</Text>
+          <div className={styles.modalButtons}>
+            <Button onClick={handleSubmit}>Confirm</Button>
+          </div>
+        </div>
+      ) : (
+        <div className={styles.modalContent}>
+          <Text>{successMessage}</Text>
+          <div className={styles.modalButtons}>
+            <Button onClick={handleCloseSuccess}>Ok</Button>
+          </div>
+        </div>
+      )}
+    </Modal>
+  );
+};
 
 ModalConfirm.propTypes = {
   title: PropTypes.string,
