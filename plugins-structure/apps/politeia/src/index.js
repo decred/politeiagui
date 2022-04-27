c9569a082693995d9b21d0563e87121aac9490f0
Victor Guedes <victorgcramos@gmail.com>
2022-04-27 10:31:54 -0300

    style: remove comments

--- plugins-structure/apps/politeia/src/index.js
+++ plugins-structure/apps/politeia/src/index.js
@@ -72,11 +72,6 @@ function initializeApi() {
 }
 
 function handleApi() {
-  // const state = store.getState();
-  // const status = api.selectStatus(state);
-  // if (status === "loading") {
-  //   document.querySelector("#root").innerHTML = "<h1>Loading api...</h1>";
-  // }
   if (!routerInitialized) {
     routerInitialized = true;
     router.init({ routes });
