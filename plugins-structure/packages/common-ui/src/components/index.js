e2ea81765e27e326335ac4fec3c31a9f0185c62c
Victor Guedes <victorgcramos@gmail.com>
2022-04-28 18:04:39 -0300

    feat(comments): render comments as markdown

--- plugins-structure/packages/common-ui/src/components/index.js
+++ plugins-structure/packages/common-ui/src/components/index.js
@@ -1,6 +1,7 @@
 export * from "./Join";
 export * from "./DateTooltip";
 export * from "./Event";
+export * from "./Markdown";
 export * from "./Modal";
 export * from "./RecordCard";
 export * from "./RecordsList";
