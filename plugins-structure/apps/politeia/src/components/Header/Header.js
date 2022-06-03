08ee1c1b18b1f7b0000925929d434430a1ed4bcd
Victor Guedes <victorgcramos@gmail.com>
2022-06-03 16:39:02 -0300

    feat: redirect to home on navbar logo click

--- plugins-structure/apps/politeia/src/components/Header/Header.js
+++ plugins-structure/apps/politeia/src/components/Header/Header.js
@@ -1,17 +1,21 @@
 import React from "react";
+import { router } from "@politeiagui/core/router";
 import { Navbar, ThemeToggle, theme } from "@politeiagui/common-ui/layout";
 import { useSelector } from "react-redux";
 import { DEFAULT_DARK_THEME_NAME, Text } from "pi-ui";
 import LogoLight from "../../public/assets/images/pi-logo-light.svg";
 import LogoDark from "../../public/assets/images/pi-logo-dark.svg";
 import About from "../Static/About";
+import styles from "./styles.module.css";
 
 function PoliteiaLogo() {
   const themeName = useSelector(theme.select);
   return (
-    <Text>
-      {themeName === DEFAULT_DARK_THEME_NAME ? <LogoDark /> : <LogoLight />}
-    </Text>
+    <div onClick={() => router.navigateTo("/")} className={styles.logo}>
+      <Text>
+        {themeName === DEFAULT_DARK_THEME_NAME ? <LogoDark /> : <LogoLight />}
+      </Text>
+    </div>
   );
 }
 
