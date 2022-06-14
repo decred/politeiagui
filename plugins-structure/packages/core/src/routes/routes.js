458b5cb76d3c9a235ce1a64697a1ec176361a04e
Victor Guedes <victorgcramos@gmail.com>
2022-06-14 11:08:49 -0300

    feat(core): add inventory routes + validate params

--- plugins-structure/packages/core/src/routes/routes.js
+++ plugins-structure/packages/core/src/routes/routes.js
@@ -1,5 +1,9 @@
 import { store } from "../storeSetup";
 import { recordsPolicy } from "../records/policy";
+import {
+  validateInventoryPageSize,
+  validateRecordsPageSize,
+} from "../records/validation";
 
 function fetchPolicyIfIdle() {
   if (recordsPolicy.selectStatus(store.getState()) === "idle") {
@@ -12,6 +16,14 @@ export const routes = [
     path: "/records/batch",
     fetch: async () => {
       await fetchPolicyIfIdle();
+      validateRecordsPageSize(store.getState());
+    },
+  },
+  {
+    path: "/records/inventory",
+    fetch: async () => {
+      await fetchPolicyIfIdle();
+      validateInventoryPageSize(store.getState());
     },
   },
 ];
