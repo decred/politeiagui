6f61bf671d160ab160b7e768a2d3ba81b1d47705
Victor Guedes <victorgcramos@gmail.com>
2022-06-15 16:26:45 -0300

    feat(core): record versions handlers

--- plugins-structure/packages/core/src/records/records/index.js
+++ plugins-structure/packages/core/src/records/records/index.js
@@ -1,7 +1,10 @@
 import {
   fetchRecordDetails,
+  fetchRecordVersionDetails,
   fetchRecords,
   selectRecordByToken,
+  selectRecordVersionByToken,
+  selectRecordVersionStatusByToken,
   selectRecords,
   selectRecordsByStateAndStatus,
   selectRecordsByTokensBatch,
@@ -12,9 +15,12 @@ import {
 export const records = {
   fetch: fetchRecords,
   fetchDetails: fetchRecordDetails,
+  fetchVersionDetails: fetchRecordVersionDetails,
   selectStatus: selectRecordsStatus,
   selectError: selectRecordsError,
   selectByToken: selectRecordByToken,
+  selectVersionByToken: selectRecordVersionByToken,
+  selectVersionStatusByToken: selectRecordVersionStatusByToken,
   selectByTokensBatch: selectRecordsByTokensBatch,
   selectAll: selectRecords,
   selectByStateAndStatus: selectRecordsByStateAndStatus,
