c544cff617a379cc2b485464e7524d3aa3408814
Victor Guedes <victorgcramos@gmail.com>
2022-06-28 15:38:04 -0300

    style: cleanup

--- plugins-structure/packages/common-ui/src/components/Diff/RawDiff.js
+++ plugins-structure/packages/common-ui/src/components/Diff/RawDiff.js
@@ -7,18 +7,16 @@ export function RawDiff({ oldText = "", newText = "" }) {
   const linesDiff = diffLines(oldText, newText);
   return (
     <pre className={styles.rawDiff}>
-      {linesDiff.map((curr, key) => {
-        return (
-          <div
-            key={key}
-            className={classNames(
-              curr.added ? styles.added : curr.removed ? styles.removed : ""
-            )}
-          >
-            {curr.value}
-          </div>
-        );
-      })}
+      {linesDiff.map((curr, key) => (
+        <div
+          key={key}
+          className={classNames(
+            curr.added ? styles.added : curr.removed ? styles.removed : ""
+          )}
+        >
+          {curr.value}
+        </div>
+      ))}
     </pre>
   );
 }
