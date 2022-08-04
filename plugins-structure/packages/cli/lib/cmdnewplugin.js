4cfcc095f4c10884257c8e3efa1fde2ff7de7382
Tiago Alves Dulce <tiagoalvesdulce@outlook.com>
2022-08-04 14:48:06 -0300

    style(cli): improve code organization

--- plugins-structure/packages/cli/lib/cmdnewplugin.js
+++ plugins-structure/packages/cli/lib/cmdnewplugin.js
@@ -1,53 +1,42 @@
 const fs = require("fs");
 const path = require("path");
 
-const { replaceFileValuesFromMap, copyFile } = require("./utils");
+const {
+  replaceFileValuesFromMap,
+  validateName,
+  createPackageJsonFile
+} = require("./utils");
 
-const basePluginPackageJSON = require("../plugin/package.json");
-const basePluginPath = path.resolve(__dirname, "../plugin");
-
-module.exports = function newPlugin(pluginName, { port }) {
-  if (!pluginName) {
-    console.error("Please specify the app name");
-    return;
-  }
+function getPluginPath(pluginName) {
   const packagePath = path.resolve(__dirname, "../../../packages/");
   const pluginPath = path.resolve(packagePath, pluginName);
   const pluginExists = fs.existsSync(pluginPath);
   if (pluginExists) {
-    console.error(
-      `Error: Plugin ${pluginName} already exists on ${packagePath}`
-    );
-    return;
+    throw Error(`Plugin ${pluginName} already exists on ${packagePath}`);
   }
+  return pluginPath;
+}
 
-  console.log(`Creating a new plugin: ${pluginName}`);
-  console.log(`Directory: ${pluginPath}`);
-  console.log(`\n\n`);
-  console.log("Creating plugin files...");
+function createNewPlugin(pluginName) {
+  let pluginPath;
+  try {
+    pluginPath = getPluginPath(pluginName);
+    console.log("eieie", pluginPath);
+    console.log(`Directory: ${pluginPath}`);
+    console.log(`\n\n`);
+    console.log("Creating plugin files...");
 
-  // create plugin dir
-  fs.mkdirSync(pluginPath);
-  fs.mkdirSync(`${pluginPath}/src`);
-  fs.mkdirSync(`${pluginPath}/src/dev`);
+    // create plugin dir
+    fs.mkdirSync(pluginPath);
+    fs.mkdirSync(`${pluginPath}/src`);
+    fs.mkdirSync(`${pluginPath}/src/dev`);
+  } catch (e) {
+    throw e;
+  }
+  return pluginPath;
+}
 
-  // create plugin package.json
-  fs.writeFileSync(
-    `${pluginPath}/package.json`,
-    JSON.stringify(
-      {
-        name: `@politeiagui/${pluginName}`,
-        ...basePluginPackageJSON,
-      },
-      null,
-      2
-    )
-  );
-  // create babel config files
-  copyFile("babel.config.js", pluginPath, basePluginPath);
-  // create test config files
-  copyFile("jest.config.js", pluginPath, basePluginPath);
-  // create src files
+function createPluginSrcFile({ basePluginPath, pluginName, pluginPath }) {
   const indexDevHtml = replaceFileValuesFromMap(
     `${basePluginPath}/src/dev/index.html`,
     { __PLUGIN_NAME__: pluginName }
@@ -55,15 +44,33 @@ module.exports = function newPlugin(pluginName, { port }) {
   const indexDevJs = replaceFileValuesFromMap(
     `${basePluginPath}/src/dev/index.js`,
     {
-      __PLUGIN_NAME__: pluginName,
+      __PLUGIN_NAME__: pluginName
     }
   );
   const indexJs = replaceFileValuesFromMap(`${basePluginPath}/src/index.js`, {
-    __PLUGIN_NAME__: pluginName,
+    __PLUGIN_NAME__: pluginName
   });
   fs.writeFileSync(`${pluginPath}/src/dev/index.html`, indexDevHtml);
   fs.writeFileSync(`${pluginPath}/src/dev/index.js`, indexDevJs);
   fs.writeFileSync(`${pluginPath}/src/index.js`, indexJs);
+}
 
+const basePluginPackageJSON = require("../plugin/package.json");
+const basePluginPath = path.resolve(__dirname, "../plugin");
+
+module.exports = function newPlugin(pluginName) {
+  try {
+    validateName(pluginName);
+    const pluginPath = createNewPlugin(pluginName);
+    createPackageJsonFile({
+      name: pluginName,
+      basePackageJSON: basePluginPackageJSON,
+      targetPath: pluginPath
+    });
+    createPluginSrcFile({ pluginPath, basePluginPath, pluginName });
+  } catch (e) {
+    console.error(e);
+    return;
+  }
   console.log("Done!");
 };
