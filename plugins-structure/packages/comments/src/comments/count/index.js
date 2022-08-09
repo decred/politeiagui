bfd547072d7a27a73383534a76951c6face0f6e5
Tiago Alves Dulce <tiagoalvesdulce@outlook.com>
2022-08-09 12:34:13 -0300

    feat(comments): simplify hooks and remove unused votes code

--- plugins-structure/packages/comments/src/comments/count/index.js
+++ plugins-structure/packages/comments/src/comments/count/index.js
@@ -2,14 +2,12 @@ import {
   fetchCommentsCount,
   selectCommentsCountError,
   selectCommentsCountStatus,
-  selectCommentsCounts,
+  selectCommentsCounts
 } from "./countSlice";
-import { useCommentsCount } from "./useCount";
 
 export const commentsCount = {
   fetch: fetchCommentsCount,
   selectAll: selectCommentsCounts,
   selectError: selectCommentsCountError,
-  selectStatus: selectCommentsCountStatus,
-  useFetch: useCommentsCount,
+  selectStatus: selectCommentsCountStatus
 };
