6097cce6e6ba149bda32962338bdc57162bf1227
Victor Guedes <victorgcramos@gmail.com>
2022-08-19 10:59:22 -0300

    fix(app/pi): politeia logo hyperlink

--- plugins-structure/apps/politeia/src/components/Header/Header.js
+++ plugins-structure/apps/politeia/src/components/Header/Header.js
@@ -1,25 +1,17 @@
 import React from "react";
-import { router } from "@politeiagui/core/router";
-import { Navbar, ThemeToggle, theme } from "@politeiagui/common-ui/layout";
 import { useSelector } from "react-redux";
-import { DEFAULT_DARK_THEME_NAME, Text } from "pi-ui";
+import { DEFAULT_DARK_THEME_NAME } from "pi-ui";
+import { Navbar, ThemeToggle, theme } from "@politeiagui/common-ui/layout";
 import LogoLight from "../../public/assets/images/pi-logo-light.svg";
 import LogoDark from "../../public/assets/images/pi-logo-dark.svg";
 import About from "../Static/About";
-import styles from "./styles.module.css";
 
 function PoliteiaLogo() {
   const themeName = useSelector(theme.select);
   return (
-    <div
-      onClick={() => router.navigateTo("/")}
-      className={styles.logo}
-      data-testid="politeia-logo"
-    >
-      <Text>
-        {themeName === DEFAULT_DARK_THEME_NAME ? <LogoDark /> : <LogoLight />}
-      </Text>
-    </div>
+    <a data-link href="/" data-testid="politeia-logo">
+      {themeName === DEFAULT_DARK_THEME_NAME ? <LogoDark /> : <LogoLight />}
+    </a>
   );
 }
 
