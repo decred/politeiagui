b359314609d837bed885e6acb3b752c05636903a
Tiago Alves Dulce <tiagoalvesdulce@outlook.com>
2022-08-25 10:18:37 -0300

    feat: import from pi-ui package

--- plugins-structure/packages/politeiagui-scripts/config/postcss/postcss.config.js
+++ plugins-structure/packages/politeiagui-scripts/config/postcss/postcss.config.js
@@ -7,7 +7,8 @@ module.exports = {
       {
         // When developing with a linked `pi-ui`
         // Point the importFrom to the path of the linked package in your env
-        importFrom: resolveOwn("../../../../pi-ui/dist/exports.css")
+        // importFrom: resolveOwn("../../../../pi-ui/dist/exports.css")
+        importFrom: resolveOwn("../../node_modules/pi-ui/dist/exports.css")
       }
     ]
   ]
