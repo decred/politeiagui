bdafb60cc8509dee8d6fd0a463d92cdad2259938
Victor Guedes <victorgcramos@gmail.com>
2022-09-28 12:16:13 -0300

    test(comments) add unit tests for new utils

--- plugins-structure/packages/comments/src/comments/utils.test.js
+++ plugins-structure/packages/comments/src/comments/utils.test.js
@@ -1,5 +1,5 @@
-import { getCommentsByParent } from "./utils";
-import { random, range } from "lodash";
+import { getCommentsByParent, keyCommentsThreadsBy } from "./utils";
+import { keyBy, random, range } from "lodash";
 
 describe("Given getCommentsByParent util", () => {
   describe("when using a list with 3 comments", () => {
@@ -61,6 +61,60 @@ describe("Given getCommentsByParent util", () => {
       const flatThreadSchema = getCommentsByParent(comments, true);
       const expectedThread = { 0: range(1, 21) };
       expect(flatThreadSchema).toEqual(expectedThread);
+    });
+  });
+});
+
+describe("Given keyCommentsThreadsBy util", () => {
+  describe("when commentsById param is valid and iteratee is default", () => {
+    it("should return comments thread by root parent commentid", () => {
+      const commentsById = keyBy(
+        [
+          { commentid: 1, parentid: 0 },
+          { commentid: 2, parentid: 1 },
+          { commentid: 3, parentid: 0 },
+          { commentid: 4, parentid: 0 },
+          { commentid: 5, parentid: 4 },
+          { commentid: 6, parentid: 0 },
+        ],
+        "commentid"
+      );
+      const commentsByRootParentId = keyCommentsThreadsBy(commentsById);
+      expect(Object.keys(commentsByRootParentId)).toEqual(["1", "3", "4", "6"]);
+    });
+  });
+  describe("when iteratee groups thread parents using custom iteratee", () => {
+    it("should return comments by extradatahint", () => {
+      const commentsById = keyBy(
+        [
+          { commentid: 1, parentid: 0 },
+          { commentid: 2, parentid: 1 },
+          { commentid: 3, parentid: 0 },
+          { commentid: 4, parentid: 0, extradatahint: "hint" },
+          { commentid: 5, parentid: 4 },
+          { commentid: 6, parentid: 0, extradatahint: "hint" },
+        ],
+        "commentid"
+      );
+      const commentsByRootHint = keyCommentsThreadsBy(
+        commentsById,
+        (c) => c.extradatahint || "main"
+      );
+      expect(Object.keys(commentsByRootHint)).toEqual(["main", "hint"]);
+    });
+  });
+  describe("when commentsById param is invalid", () => {
+    it("should return an empty object for falsy values", () => {
+      expect(keyCommentsThreadsBy()).toEqual({});
+      expect(keyCommentsThreadsBy(false)).toEqual({});
+      expect(keyCommentsThreadsBy(null)).toEqual({});
+      expect(keyCommentsThreadsBy(undefined)).toEqual({});
+    });
+    it("should return an empty object when param is array", () => {
+      expect(keyCommentsThreadsBy([])).toEqual({});
+    });
+    it("should return an empty object when param is an empty object", () => {
+      expect(keyCommentsThreadsBy({})).toEqual({});
     });
   });
 });
