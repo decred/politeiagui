0c869d69abba11f192c22656cec401d2439f9ed2
Victor Guedes <victorgcramos@gmail.com>
2022-10-13 12:40:51 -0300

    feat(common-u): add progress service to ui plugin

--- plugins-structure/packages/common-ui/src/plugin.js
+++ plugins-structure/packages/common-ui/src/plugin.js
@@ -1,12 +1,19 @@
 import { pluginSetup } from "@politeiagui/core";
 import { themeReducer } from "./layout";
+import progressReducer, {
+  services as progressServices,
+} from "./services/progress";
 // Apply styles once theme is imported.
 import "pi-ui/dist/index.css";
 
 // UiPlugin Interface
 const UiPlugin = pluginSetup({
-  reducers: [{ key: "uiTheme", reducer: themeReducer }],
+  reducers: [
+    { key: "uiTheme", reducer: themeReducer },
+    { key: "uiProgress", reducer: progressReducer },
+  ],
   name: "common-ui",
+  services: [...progressServices],
 });
 
 export default UiPlugin;
