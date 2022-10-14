9255e543f3c934dafad35ce051c250f329e68d57
Victor Guedes <victorgcramos@gmail.com>
2022-10-14 12:25:47 -0300

    feat(common-ui): improve ProgressBar UI

--- plugins-structure/packages/common-ui/src/components/ProgressBar/ProgressBar.js
+++ plugins-structure/packages/common-ui/src/components/ProgressBar/ProgressBar.js
@@ -5,11 +5,13 @@ import styles from "./styles.module.css";
 
 export const ProgressBar = () => {
   const value = useSelector(progress.select);
-  return (
-    <div
-      data-testid="common-ui-progress-bar"
-      className={styles.progressBar}
-      style={{ width: `${+value * 100}%` }}
-    />
-  );
+  return value ? (
+    <div className={styles.progressBarWrapper}>
+      <div
+        data-testid="common-ui-progress-bar"
+        className={styles.progressBar}
+        style={{ width: `${+value * 100}%` }}
+      />
+    </div>
+  ) : null;
 };
