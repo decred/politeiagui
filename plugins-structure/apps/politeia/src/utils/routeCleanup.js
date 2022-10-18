fff9a21ed0b5b49720c4c3485e5286b90b52d2a5
Victor Guedes <victorgcramos@gmail.com>
2022-10-18 19:38:46 -0300

    feat(app/pi): update nav. history on route cleanup

--- plugins-structure/apps/politeia/src/utils/routeCleanup.js
+++ plugins-structure/apps/politeia/src/utils/routeCleanup.js
@@ -1,5 +1,26 @@
 import ReactDOM from "react-dom";
+import { store } from "@politeiagui/core";
+import {
+  findMatch,
+  getParams,
+  getURLSearchParams,
+  router,
+} from "@politeiagui/core/router";
+import { navigation } from "@politeiagui/common-ui/services";
 
 export function routeCleanup() {
+  // push router match to our ui navigation service.
+  const match = findMatch(router.getRoutes(), window.location.pathname);
+  if (match) {
+    store.dispatch(
+      navigation.push({
+        match: match.result[0],
+        path: match.route.path,
+        params: getParams(match),
+        search: getURLSearchParams(),
+        href: window.location.href,
+      })
+    );
+  }
   return ReactDOM.unmountComponentAtNode(document.querySelector("#root"));
 }
