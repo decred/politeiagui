a90e6a28531a34b3a5d2780ec724ba6ea6ae92f9
Victor Guedes <victorgcramos@gmail.com>
2022-10-19 16:58:15 -0300

    feat(common-ui): improve GoBackLink

--- plugins-structure/packages/common-ui/src/components/GoBackLink/GoBackLink.js
+++ plugins-structure/packages/common-ui/src/components/GoBackLink/GoBackLink.js
@@ -1,21 +1,30 @@
 import React from "react";
+import { classNames } from "pi-ui";
 import { useSelector } from "react-redux";
 import { navigation } from "../../services";
 import startsWith from "lodash/startsWith";
+import findLast from "lodash/findLast";
+import styles from "./styles.module.css";
 
 const backArrow = <>&#8592;</>;
 
-export const GoBackLink = () => {
+export const GoBackLink = ({ className, ...props }) => {
   const history = useSelector(navigation.selectHistory);
 
-  const parent = history.find(
+  const parent = findLast(
+    history,
     (hs) =>
-      startsWith(window.location.pathname, hs.match) &&
-      hs.match !== window.location.pathname
+      startsWith(window.location.pathname, hs.pathname) &&
+      hs.pathname !== window.location.pathname
   );
 
   return parent ? (
-    <a href={parent.href} data-link>
+    <a
+      href={parent.href}
+      data-link
+      className={classNames(styles.link, className)}
+      {...props}
+    >
       {backArrow} Go Back
     </a>
   ) : null;
