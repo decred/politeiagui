9e5590e1eeab53791bff4abcf54125dfa6fde3fa
Victor Guedes <victorgcramos@gmail.com>
2022-11-11 16:16:28 -0300

    test(common-ui): component tests setup

---
+++ plugins-structure/packages/common-ui/cypress.config.js
@@ -0,0 +1,12 @@
+const { defineConfig } = require("cypress");
+const configFactory = require("../politeiagui-scripts/config/webpack/webpack.config");
+
+module.exports = defineConfig({
+  component: {
+    devServer: {
+      framework: "react",
+      bundler: "webpack",
+      webpackConfig: configFactory("development", "plugin"),
+    },
+  },
+});
