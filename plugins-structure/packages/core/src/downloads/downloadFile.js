cad412ab1c4c01f003b557724510173684459e27
Victor Guedes <victorgcramos@gmail.com>
2022-12-15 10:13:03 -0300

    feat(core): add downloadCSV util

--- plugins-structure/packages/core/src/downloads/downloadFile.js
+++ plugins-structure/packages/core/src/downloads/downloadFile.js
@@ -4,3 +4,12 @@ export function downloadJSON(data, filename) {
   const dataToString = JSON.stringify(data, null, 2);
   fileDownload(dataToString, `${filename}.json`);
 }
+
+export function downloadCSV(data, fields, filename) {
+  const titles = `${fields.join(",")}\n`;
+  const csvData = data.reduce((acc, info) => {
+    const row = Object.values(info).join(",");
+    return `${acc}${row}\n`;
+  }, titles);
+  fileDownload(csvData, `${filename}.csv`);
+}
