107a7ca3bf543c244e4230e3132e5f30530ac0a2
Victor Guedes <victorgcramos@gmail.com>
2023-01-12 15:12:57 -0300

    fix(core): update records drafts listeners

--- plugins-structure/packages/core/src/records/drafts/services.js
+++ plugins-structure/packages/core/src/records/drafts/services.js
@@ -1,23 +1,22 @@
 import { createSliceServices } from "../../toolkit/createSliceServices";
 import { deleteRecordDraft, loadRecordDrafts } from "./recordsDraftsSlice";
 
-export const { pluginServices, serviceSetups: serviceListeners } =
-  createSliceServices({
-    name: "recordsDrafts",
-    services: {
-      load: {
-        onSetup: ({ dispatch }) => {
-          // TODO: Use logged in user id
-          const userid = "user-id-test";
-          dispatch(loadRecordDrafts(userid));
-        },
-      },
-      delete: {
-        effect: (_, dispatch, { draftid }) => {
-          // TODO: Use logged in user id
-          const userid = "user-id-test";
-          dispatch(deleteRecordDraft({ userid, draftid }));
-        },
+export const { pluginServices, serviceListeners } = createSliceServices({
+  name: "recordsDrafts",
+  services: {
+    load: {
+      onSetup: ({ dispatch }) => {
+        // TODO: Use logged in user id
+        const userid = "user-id-test";
+        dispatch(loadRecordDrafts(userid));
       },
     },
-  });
+    delete: {
+      effect: (_, dispatch, { draftid }) => {
+        // TODO: Use logged in user id
+        const userid = "user-id-test";
+        dispatch(deleteRecordDraft({ userid, draftid }));
+      },
+    },
+  },
+});
