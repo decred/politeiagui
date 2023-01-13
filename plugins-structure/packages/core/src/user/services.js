169b9aee834c4c646003c1446a62629e987b618e
Victor Guedes <victorgcramos@gmail.com>
2023-01-13 09:48:12 -0300

    fix(core): update user service listeners export

--- plugins-structure/packages/core/src/user/services.js
+++ plugins-structure/packages/core/src/user/services.js
@@ -3,26 +3,24 @@ import { getURLSearchParams } from "../router";
 import { store } from "../storeSetup";
 import { createSliceServices } from "../toolkit";
 
-// TODO: remove `serviceSetups` alias once #2881 gets in
-export const { pluginServices, serviceSetups: serviceListeners } =
-  createSliceServices({
-    name: "userEmail",
-    services: {
-      emailVerify: {
-        onSetup: () => {
-          const { verificationtoken } = getURLSearchParams();
-          if (verificationtoken) {
-            store.dispatch(user.verifyEmail({ verificationtoken }));
-          }
-        },
-      },
-      keyVerify: {
-        onSetup: () => {
-          const { verificationtoken } = getURLSearchParams();
-          if (verificationtoken) {
-            store.dispatch(user.verifyKey({ verificationtoken }));
-          }
-        },
+export const { pluginServices, serviceListeners } = createSliceServices({
+  name: "userEmail",
+  services: {
+    emailVerify: {
+      onSetup: () => {
+        const { verificationtoken } = getURLSearchParams();
+        if (verificationtoken) {
+          store.dispatch(user.verifyEmail({ verificationtoken }));
+        }
       },
     },
-  });
+    keyVerify: {
+      onSetup: () => {
+        const { verificationtoken } = getURLSearchParams();
+        if (verificationtoken) {
+          store.dispatch(user.verifyKey({ verificationtoken }));
+        }
+      },
+    },
+  },
+});
