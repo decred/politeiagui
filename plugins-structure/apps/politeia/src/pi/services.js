3ed979575f81e49fae2fd0c99542f80f515d40fb
Victor Guedes <victorgcramos@gmail.com>
2023-01-18 18:18:16 -0300

    feat(pi): add pi policy service

--- plugins-structure/apps/politeia/src/pi/services.js
+++ plugins-structure/apps/politeia/src/pi/services.js
@@ -1,16 +1,11 @@
-import { fetchPolicyIfIdle } from "./utils";
 import { pluginServices as summariesServices } from "./summaries/services";
 import { pluginServices as billingServices } from "./billing/services";
 import { pluginServices as proposalsServices } from "./proposals/services";
+import { pluginServices as policyServices } from "./policy/services";
 
 export const services = [
   ...billingServices,
   ...summariesServices,
   ...proposalsServices,
-  {
-    id: "pi/new",
-    action: async () => {
-      await fetchPolicyIfIdle();
-    },
-  },
+  ...policyServices,
 ];
