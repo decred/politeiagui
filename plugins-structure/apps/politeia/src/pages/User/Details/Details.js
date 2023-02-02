c28afd036d14fcaba6e75ae4687882ad221a7030
Victor Guedes <victorgcramos@gmail.com>
2023-02-02 15:30:35 -0300

    style(app/pi): user details page readability

--- plugins-structure/apps/politeia/src/pages/User/Details/Details.js
+++ plugins-structure/apps/politeia/src/pages/User/Details/Details.js
@@ -15,14 +15,29 @@ export const TAB_LABELS = {
   "2fa": { title: "Two-Factor Authentication", private: true },
 };
 
-const getTabsToDisplay = (isCurrent, isAdmin) =>
+/**
+ * getTabsToDisplay - Returns the tabs to be displayed based on the user
+ * type. Public tabs are always displayed. Private tabs are displayed
+ * only if the user is the current user. Admin tabs are displayed only
+ * if the user is an admin. Same tab can be either private or admin.
+ * @param {boolean} isCurrentUser - Whether the user is the current user
+ * @param {boolean} isAdmin - Whether the user is an admin
+ * @returns {Array} - Array of tabs indexes to be displayed
+ */
+const getTabsIndexesToDisplay = (isCurrentUser, isAdmin) =>
   Object.keys(TAB_LABELS).filter(
     (tab) =>
       TAB_LABELS[tab].public ||
       (isAdmin && TAB_LABELS[tab].admin) ||
-      (isCurrent && TAB_LABELS[tab].private)
+      (isCurrentUser && TAB_LABELS[tab].private)
   );
 
+/**
+ * formatTabs - Formats the tabs to be displayed in the banner.
+ *  @param {Array} tabs - Array of tabs indexes to be displayed
+ * @param {string} userid - User id
+ * @returns {Array} - Array of formatted tabs
+ */
 const formatTabs = (tabs, userid) =>
   tabs.map((tab) => (
     <a href={`/user/${userid}/${tab}`} data-link key={tab}>
@@ -40,9 +55,9 @@ function UserDetails() {
   const isCurrent = user && user?.id === currentUser?.userid;
   const isAdmin = currentUser?.isadmin;
 
-  const tabsToDisplay = getTabsToDisplay(isCurrent, isAdmin);
-  const tabActive = targetTab ? tabsToDisplay.indexOf(targetTab) : 0;
-  const tabs = formatTabs(tabsToDisplay, userid);
+  const tabsIndexesToDisplay = getTabsIndexesToDisplay(isCurrent, isAdmin);
+  const tabActive = targetTab ? tabsIndexesToDisplay.indexOf(targetTab) : 0;
+  const tabs = formatTabs(tabsIndexesToDisplay, userid);
 
   return (
     <SingleContentPage
